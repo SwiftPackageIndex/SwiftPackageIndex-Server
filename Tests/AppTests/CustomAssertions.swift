eeb5f9966ed8d21200474b21e0dbdc629e2ff73d
Dave Verwer <dave.verwer@gmail.com>
2020-03-18 11:22:19 +0000

    Unused file.

--- Tests/AppTests/CustomAssertions.swift
+++
@@ -1,6 +0,0 @@
-import XCTest
-
-/// Assert that two arrays contain exactly the same elements, regardless of order.
-/// - Parameters:
-///   - firstArray: The first array.
-///   - secondArray: The second array.
