99e54c9d447d9dca8d9f5ebcc3f23d3b44401a8e
Dave Verwer <dave.verwer@gmail.com>
2020-03-24 18:30:40 +0000

    Run both a dev and test server with docker.

--- startLocalDockerDB.sh
+++ startLocalDockerDB.sh
@@ -1,1 +1,2 @@
-docker run --name postgres-swiftpackageindex -e POSTGRES_DB=swiftpackageindex_dev -e POSTGRES_USER=swiftpackageindex -e POSTGRES_PASSWORD=password -p 5432:5432 -d postgres
+docker run --name swiftpackageindex_postgres_dev -e POSTGRES_DB=swiftpackageindex_dev -e POSTGRES_USER=swiftpackageindex -e POSTGRES_PASSWORD=password -p 5432:5432 -d postgres
+docker run --name swiftpackageindex_postgres_test -e POSTGRES_DB=swiftpackageindex_test -e POSTGRES_USER=swiftpackageindex -e POSTGRES_PASSWORD=password -p 5433:5432 -d postgres
