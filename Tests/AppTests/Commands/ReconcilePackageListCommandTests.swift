36887e9570e94e8dd080c09210b567bdcfd41b18
Dave Verwer <dave.verwer@gmail.com>
2020-03-28 11:21:41 +0000

    Added the actual test I was trying to write!

--- Tests/AppTests/Commands/ReconcilePackageListCommandTests.swift
+++ Tests/AppTests/Commands/ReconcilePackageListCommandTests.swift
@@ -35,14 +35,19 @@ final class ReconcilePackageListCommandTests: XCTestCase
     let fakePackageList = [ "https://github.com/vapor/vapor.git", "https://github.com/zntfdr/AStack.git" ]
     fakeClient.registerClientResponse(for: "https://raw.githubusercontent.com/daveverwer/SwiftPMLibrary/master/packages.json", content: fakePackageList)
 
-    let countBeforeReconcile = try Package.query(on: database).count().wait()
-    XCTAssertEqual(countBeforeReconcile, 2)
-
     let command = ReconcilePackageListCommand()
     let commandContext = CommandContext(console: Terminal(), arguments: [:], options: [:], on: app)
     try command.run(using: commandContext).wait()
 
+    // Validate that the database contains exactly the number of packages in the faked package list
     let countAfterReconcile = try Package.query(on: database).count().wait()
-    XCTAssertEqual(countAfterReconcile, 2)
+    XCTAssertEqual(countAfterReconcile, fakePackageList.count)
+
+    // Validate that the database contains every package in the faked package list
+    for packageUrlString in fakePackageList {
+      let packageUrl = URL(string: packageUrlString)!
+      let package = try Package.findByUrl(on: database, url: packageUrl).wait()
+      XCTAssertEqual(package.url, packageUrl)
+    }
   }
 }
