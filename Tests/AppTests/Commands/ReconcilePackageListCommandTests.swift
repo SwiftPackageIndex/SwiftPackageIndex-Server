4e7b6254a32918ca9ea1dd973b760689425a022b
Dave Verwer <dave.verwer@gmail.com>
2020-03-30 13:52:17 +0100

    Revert "Force CI to try the PackageTests rather than the ReconcilePackageListCommandTests."
    This reverts commit 40b8b8fe49a38247010695900e913797f5bb8020.

--- Tests/AppTests/Commands/ReconcilePackageListCommandTests.swift
+++ Tests/AppTests/Commands/ReconcilePackageListCommandTests.swift
@@ -4,56 +4,56 @@ import XCTest
 
 @testable import App
 
-//final class ReconcilePackageListCommandTests: XCTestCase
-//{
-//  var app: Application!
-//  var database: PostgreSQLConnection!
-//
-//  override func setUp()
-//  {
-//    super .setUp()
-//
-//    try! Application.reset()
-//    app = try! Application.testable()
-//    database = try! app.newConnection(to: .psql).wait()
-//  }
-//
-//  override func tearDown()
-//  {
-//    super .tearDown()
-//
-//    database.close()
-//    try? app.syncShutdownGracefully()
-//  }
-//
-//  func testAddingAndDeletingPackages() throws
-//  {
-//    TestData.createPackage(on:database, urlString: "https://github.com/Alamofire/Alamofire.git")
-//    TestData.createPackage(on:database, urlString: "https://github.com/vapor/vapor.git")
-//
-//    let fakeClient = try app.make(FakeClient.self)
-//    let fakePackageList = [ "https://github.com/vapor/vapor.git", "https://github.com/zntfdr/AStack.git" ]
-//    fakeClient.registerClientResponse(for: "https://raw.githubusercontent.com/daveverwer/SwiftPMLibrary/master/packages.json", content: fakePackageList)
-//
-//    let command = ReconcilePackageListCommand()
-//    let commandContext = CommandContext(console: Terminal(), arguments: [:], options: [:], on: app)
-//    try command.run(using: commandContext).wait()
-//
-//    // Validate that the database contains exactly the number of packages in the faked package list
-//    let countAfterReconcile = try Package.query(on: database).count().wait()
-//    XCTAssertEqual(countAfterReconcile, fakePackageList.count)
-//
-//    // Validate that the database contains every package in the faked package list
-//    for packageUrlString in fakePackageList {
-//      let packageUrl = URL(string: packageUrlString)!
-//      let package = try Package.findByUrl(on: database, url: packageUrl).wait()
-//      XCTAssertEqual(package.url, packageUrl)
-//    }
-//
-//    // Validate that the database does not contain the deleted package
-//    let deletedPackageUrl = URL(string: "https://github.com/Alamofire/Alamofire.git")!
-//    XCTAssertThrowsError(try Package.findByUrl(on: database, url: deletedPackageUrl).wait()) { error in
-//      XCTAssertEqual(error as? PackageError, PackageError.recordNotFound)
-//    }
-//  }
-//}
+final class ReconcilePackageListCommandTests: XCTestCase
+{
+  var app: Application!
+  var database: PostgreSQLConnection!
+
+  override func setUp()
+  {
+    super .setUp()
+
+    try! Application.reset()
+    app = try! Application.testable()
+    database = try! app.newConnection(to: .psql).wait()
+  }
+
+  override func tearDown()
+  {
+    super .tearDown()
+
+    database.close()
+    try? app.syncShutdownGracefully()
+  }
+
+  func testAddingAndDeletingPackages() throws
+  {
+    TestData.createPackage(on:database, urlString: "https://github.com/Alamofire/Alamofire.git")
+    TestData.createPackage(on:database, urlString: "https://github.com/vapor/vapor.git")
+
+    let fakeClient = try app.make(FakeClient.self)
+    let fakePackageList = [ "https://github.com/vapor/vapor.git", "https://github.com/zntfdr/AStack.git" ]
+    fakeClient.registerClientResponse(for: "https://raw.githubusercontent.com/daveverwer/SwiftPMLibrary/master/packages.json", content: fakePackageList)
+
+    let command = ReconcilePackageListCommand()
+    let commandContext = CommandContext(console: Terminal(), arguments: [:], options: [:], on: app)
+    try command.run(using: commandContext).wait()
+
+    // Validate that the database contains exactly the number of packages in the faked package list
+    let countAfterReconcile = try Package.query(on: database).count().wait()
+    XCTAssertEqual(countAfterReconcile, fakePackageList.count)
+
+    // Validate that the database contains every package in the faked package list
+    for packageUrlString in fakePackageList {
+      let packageUrl = URL(string: packageUrlString)!
+      let package = try Package.findByUrl(on: database, url: packageUrl).wait()
+      XCTAssertEqual(package.url, packageUrl)
+    }
+
+    // Validate that the database does not contain the deleted package
+    let deletedPackageUrl = URL(string: "https://github.com/Alamofire/Alamofire.git")!
+    XCTAssertThrowsError(try Package.findByUrl(on: database, url: deletedPackageUrl).wait()) { error in
+      XCTAssertEqual(error as? PackageError, PackageError.recordNotFound)
+    }
+  }
+}
