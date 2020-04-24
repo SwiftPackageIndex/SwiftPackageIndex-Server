e320c1472e33fed15c07d1d01d3bc1775a89421c
Sven A. Schmidt <sas@finestructure.co>
2020-04-24 12:05:40 +0200

    Configure db, add test.restfile

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
 run:
 	vapor-beta run
+
+db-up:
+	docker run --name spmidx_dev -e POSTGRES_DB=spmidx_dev -e POSTGRES_USER=spmidx_dev -e POSTGRES_PASSWORD=xxx -p 5432:5432 -d postgres
+
+db-down:
+	docker rm -f spmidx_dev
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -9,9 +9,9 @@ public func configure(_ app: Application) throws {
 
     app.databases.use(.postgres(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+        username: Environment.get("DATABASE_USERNAME") ?? "spmidx_dev",
+        password: Environment.get("DATABASE_PASSWORD") ?? "xxx",
+        database: Environment.get("DATABASE_NAME") ?? "spmidx_dev"
     ), as: .psql)
 
     app.migrations.add(CreateTodo())
