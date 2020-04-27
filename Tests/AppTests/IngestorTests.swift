23034616b8231481b4c9902dc4cafc833dca8d8e
Sven A. Schmidt <sas@finestructure.co>
2020-04-27 17:32:45 +0200

    Clean up

--- Tests/AppTests/IngestorTests.swift
+++ Tests/AppTests/IngestorTests.swift
@@ -59,45 +59,6 @@ class IngestorTests: XCTestCase {
         }
     }
 
-    func test_insertOrUpdateRepository_bulk_simpler() throws {
-        // test flattening of many updates
-        let packages = try savePackages(on: app.db, testUrls100)
-        let req = try packages
-            .map { (Github.Metadata.mock(for: $0), $0) }
-            .map { try insertOrUpdateRepository(on: self.app.db, for: $0.1, metadata: $0.0) }
-            .flatten(on: app.db.eventLoop)
-
-        try req.wait()
-
-        let repos = try Repository.query(on: app.db).all().wait()
-        XCTAssertEqual(repos.count, testUrls100.count)
-        XCTAssertEqual(repos.map(\.$package.id.uuidString).sorted(),
-                       packages.map(\.id).compactMap { $0?.uuidString }.sorted())
-    }
-
-    func test_bulk_metadata() throws {
-        let input = app.db.eventLoop.future(
-            testUrls100.map { Package(url: $0) } )  // EventLoopFuture<[URL]>
-            .flatMapEachThrowing {
-                EventLoopFuture<Github.Metadata>.just(value: Github.Metadata.mock(for: $0))
-                    .and(value: $0)
-        }
-        .flatMap { $0.flatten(on: self.app.db.eventLoop) }
-        let res = try input.wait()
-        XCTAssertEqual(res.count, testUrls100.count)
-    }
-
-    func test_bulk_save_issue() throws {
-        let input = app.db.eventLoop.future(
-            testUrls100.map { Package(url: $0) } )  // EventLoopFuture<[URL]>
-        let inserts = input
-            .flatMapEachThrowing { try $0.create(on: self.app.db) }
-            .flatMap { $0.flatten(on: self.app.db.eventLoop) }
-        try inserts.wait()
-        let packages = try Package.query(on: app.db).all().wait()
-        XCTAssertEqual(packages.count, testUrls100.count)
-    }
-
     func test_partial_save_issue() throws {
         // setup
         Current.fetchMasterPackageList = { _ in .just(value: testUrls) }
@@ -116,22 +77,11 @@ class IngestorTests: XCTestCase {
     func test_insertOrUpdateRepository_bulk() throws {
         // test flattening of many updates
         let packages = try savePackages(on: app.db, testUrls100)
-        let metadata = Package.query(on: app.db)
-            .all()
-            .flatMapEachThrowing {
-                EventLoopFuture<Github.Metadata>.just(value: Github.Metadata.mock(for: $0))
-                    .and(value: $0)
-        }
-        .flatMap { $0.flatten(on: self.app.db.eventLoop) }
+        let req = try packages
+            .map { ($0, Github.Metadata.mock(for: $0)) }
+            .map { try insertOrUpdateRepository(on: self.app.db, for: $0.0, metadata: $0.1) }
+            .flatten(on: app.db.eventLoop)
 
-        let resolvedMetadata = try metadata.wait()
-        XCTAssertEqual(resolvedMetadata.count, 100)
-
-        let req = app.db.eventLoop.future(resolvedMetadata)
-            .flatMapThrowing { (arr) -> ([EventLoopFuture<Void>]) in
-                let a = try arr.map { try insertOrUpdateRepository(on: self.app.db, for: $0.1, metadata: $0.0) }
-                return a  //.flatten(on: self.app.db.eventLoop)
-        }.flatMap { $0.flatten(on: self.app.db.eventLoop) }
         try req.wait()
 
         let repos = try Repository.query(on: app.db).all().wait()
@@ -140,100 +90,4 @@ class IngestorTests: XCTestCase {
                        packages.map(\.id).compactMap { $0?.uuidString }.sorted())
     }
 
-    func test_flatten() throws {
-        // setup
-        try savePackages(on: app.db, testUrls100)
-
-        // test
-        let foos = getFoos(app.client, app.db)
-        let flattenedFoos = foos.flatMap { $0.flatten(on: self.app.db.eventLoop) }
-        let inserts = flattenedFoos
-            .flatMapEachThrowing { try doBar(on: self.app.db, with: $0) }
-            .flatMap { $0.flatten(on: self.app.db.eventLoop) }
-
-        try inserts.wait()
-
-        XCTAssertEqual(try Bar.query(on: app.db).count().wait(), 100)
-    }
-
-    func test_flatten_upsert() throws {
-        // setup
-        try savePackages(on: app.db, testUrls100)
-
-        // test
-        let foos = getFoos(app.client, app.db)
-        let flattenedFoos = foos.flatMap { $0.flatten(on: self.app.db.eventLoop) }
-        let inserts = flattenedFoos
-            .flatMapEachThrowing { try doBarUpsert(on: self.app.db, with: $0) }
-            .flatMap { $0.flatten(on: self.app.db.eventLoop) }
-
-        try inserts.wait()
-
-        XCTAssertEqual(try Bar.query(on: app.db).count().wait(), 100)
-    }
-
-    func test_flatten_upsertNoBang() throws {
-        // setup
-        try savePackages(on: app.db, testUrls100)
-
-        // test
-        let foos = getFoos(app.client, app.db)
-        let flattenedFoos = foos.flatMap { $0.flatten(on: self.app.db.eventLoop) }
-        let inserts = flattenedFoos
-            .flatMapEachThrowing { try doBarUpsertNoBang(on: self.app.db, with: $0) }
-            .flatMap { $0.flatten(on: self.app.db.eventLoop) }
-
-        try inserts.wait()
-
-        XCTAssertEqual(try Bar.query(on: app.db).count().wait(), 100)
-    }
 }
-
-
-typealias Foo = (Github.Metadata, Package)
-typealias Bar = Repository
-
-func getFoos(_ client: Client, _ database: Database) -> EventLoopFuture<[EventLoopFuture<Foo>]> {
-    Package.query(on: database)
-        .all()
-        .flatMapEachThrowing { try Current.fetchRepository(client, $0).and(value: $0) }
-}
-
-func doBar(on database: Database, with foo: Foo) throws -> EventLoopFuture<Void> {
-    try Repository(package: foo.1, metadata: foo.0).save(on: database)
-}
-
-func doBarUpsert(on database: Database, with foo: Foo) throws -> EventLoopFuture<Void> {
-    let req = Repository.query(on: database)
-        .filter(try \.$package.$id == foo.1.requireID())
-        .first()
-        .flatMap { (repo) -> EventLoopFuture<Void> in
-            if let repo = repo {
-                // update fields
-                return repo.save(on: database)
-            } else {
-                return try! Repository(package: foo.1, metadata: foo.0).save(on: database)
-            }
-    }
-    return req
-}
-
-func doBarUpsertNoBang(on database: Database, with foo: Foo) throws -> EventLoopFuture<Void> {
-    let req = Repository.query(on: database)
-        .filter(try \.$package.$id == foo.1.requireID())
-        .first()
-        .flatMap { (repo) -> EventLoopFuture<Void> in
-            if let repo = repo {
-                // update fields
-                return repo.save(on: database)
-            } else {
-                do {
-                    return try Repository(package: foo.1, metadata: foo.0).save(on: database)
-                } catch {
-                    return database.eventLoop.makeFailedFuture(Abort(.internalServerError))
-                }
-            }
-    }
-    return req
-}
-
