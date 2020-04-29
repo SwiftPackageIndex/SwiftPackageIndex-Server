7c6787754c1537517322471274198963d167a96b
Sven A. Schmidt <sas@finestructure.co>
2020-04-29 15:40:52 +0200

    Add "Ingest" button

--- Sources/App/Pages/Home.swift
+++ Sources/App/Pages/Home.swift
@@ -24,6 +24,7 @@ func homePage() -> HTML.Node {
                     .h2([.text(title)]),
                     ]),
                 reconcileButton(),
+                ingestButton(),
             ]),
         ])
     ])
@@ -33,10 +34,22 @@ func homePage() -> HTML.Node {
 func reconcileButton() -> HTML.Node {
     return .form([.action => "/packages/run/reconcile", .method => "get"], [
         row([
-            .label(["Reconcile the master package list with the Package Index."]),
-            ]),
-        row([
             .input([.class => "btn btn-primary", .type => "submit", .value => "Reconcile"])
-            ]),
-        ])
+        ]),
+        row([
+            .label(["Reconcile the master package list with the Package Index."]),
+        ]),
+    ])
+}
+
+
+func ingestButton() -> HTML.Node {
+    return .form([.action => "/packages/run/ingest", .method => "get"], [
+        row([
+            .input([.class => "btn btn-primary", .type => "submit", .value => "Ingest"])
+        ]),
+        row([
+            .label(["Ingest metadata for a batch of packages."]),
+        ]),
+    ])
 }
