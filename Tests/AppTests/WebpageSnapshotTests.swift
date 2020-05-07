c3a1e014df4f462525abb1e7858374cf3bbfc750
Sven A. Schmidt <sas@finestructure.co>
2020-05-07 12:18:33 +0200

    html snapshot

--- Tests/AppTests/WebpageSnapshotTests.swift
+++ Tests/AppTests/WebpageSnapshotTests.swift
@@ -1,32 +1,14 @@
-//
-//  WebpageSnapshotTests.swift
-//  AppTests
-//
-//  Created by Sven A. Schmidt on 07/05/2020.
-//
+@testable import App
 
+import SnapshotTesting
 import XCTest
+
 
 class WebpageSnapshotTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_home() throws {
+        let home = HTML.render(homePage())
+        assertSnapshot(matching: home, as: .lines)
     }
 
 }
