bbc8878f9bde48e4bd91ba08d5a69113c092cb85
Sven A. Schmidt <sas@finestructure.co>
2020-05-07 14:48:10 +0200

    Add Vapor integration for our html dsl

--- Sources/App/Pages/Base.swift
+++ Sources/App/Pages/Base.swift
@@ -8,11 +8,11 @@
 import Vapor
 
 
-func render(page: HTML.Node) -> Response {
-    let res = Response(status: .ok, body: .init(string: HTML.render(page)))
-    res.headers.add(name: "Content-Type", value: "text/html")
-    return res
-}
+//func render(page: HTML.Node) -> Response {
+//    let res = Response(status: .ok, body: .init(string: HTML.render(page)))
+//    res.headers.add(name: "Content-Type", value: "text/html")
+//    return res
+//}
 
 
 func myhead(title: String) -> HTML.Node {
