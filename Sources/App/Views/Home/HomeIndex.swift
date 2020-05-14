ac85491592aa263b86f09ac0abe177bd3cce4df1
Dave Verwer <dave.verwer@gmail.com>
2020-05-14 19:23:30 +0100

    Corrected the markup in the home page.

--- Sources/App/Views/Home/HomeIndex.swift
+++ Sources/App/Views/Home/HomeIndex.swift
@@ -4,15 +4,25 @@ import Plot
 class HomeIndex: PublicPage {
 
     override func content() -> Node<HTML.BodyContext> {
-        .main(
-            .p("Home page")
-        )
+        .p("Home page")
     }
 
     override func navItems() -> [Node<HTML.ListContext>] {
         // The default navigation menu, without search.
-        [.li(.a(.href("#"), "Add a Package")),
-         .li(.a(.href("#"), "About"))]
+        [
+            .li(
+                .a(
+                    .href("#"),
+                    "Add a Package"
+                )
+            ),
+            .li(
+                .a(
+                    .href("#"),
+                    "About"
+                )
+            )
+        ]
     }
 
 }
