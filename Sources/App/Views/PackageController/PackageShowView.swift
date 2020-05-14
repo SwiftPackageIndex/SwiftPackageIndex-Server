eeb8e4264edaf9fe1189debe25dda4c2ad769cce
Dave Verwer <dave.verwer@gmail.com>
2020-05-14 21:26:46 +0100

    New markup for having the URL above the HR.

--- Sources/App/Views/PackageController/PackageShowView.swift
+++ Sources/App/Views/PackageController/PackageShowView.swift
@@ -17,13 +17,22 @@ class PackageShowView: PublicPage {
         .group(
             .div(
                 .class("split"),
-                .h2("Alamofire"),
+                .div(
+                    .h2("Alamofire"),
+                    .element(named: "small", nodes: [ // TODO: Fix after Plot update
+                        .a(
+                            .href("https://github.com/Alamofire/Alamofire.git"),
+                            "https://github.com/Alamofire/Alamofire.git"
+                        )
+                    ])
+                ),
                 .div(
                     .class("license"),
-                    .attribute(named: "title", value: "MIT License"), // TODO: Suggest that Plot is able to add `title` attributes on div elements (and on any other element!)
+                    .attribute(named: "title", value: "MIT License"), // TODO: Fix after Plot update
                     "MIT"
                 )
-            )
+            ),
+            .hr()
         )
     }
 
