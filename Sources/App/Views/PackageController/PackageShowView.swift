32556df312a2f631d028cd9c051d776f15e8cf78
Sven A. Schmidt <sas@finestructure.co>
2020-05-17 14:05:02 +0200

    Add authors and history

--- Sources/App/Views/PackageController/PackageShowView.swift
+++ Sources/App/Views/PackageController/PackageShowView.swift
@@ -42,46 +42,12 @@ class PackageShowView: PublicPage {
                 .class("metadata"),
                 .li(
                     .class("authors"),
-                    "By ",
-                    .a(
-                        .href("https://github.com/cnoon"),
-                        "Christian Noon"
-                    ),
-                    ", ",
-                    .a(
-                        .href("https://github.com/mattt"),
-                        "Mattt"
-                    ),
-                    ", ",
-                    .a(
-                        .href("https://github.com/jshier"),
-                        "Jon Shier"
-                    ),
-                    ", ",
-                    .a(
-                        .href("https://github.com/kcharwood"),
-                        "Kevin Harwood"
-                    ),
-                    ", and ",
-                    .a(
-                        .href("https://github.com/Alamofire/Alamofire/graphs/contributors"),
-                        "186 other contributors"
-                    ),
-                    "."
-                ),
+                    .group(model.authorsClause())
+                )
+                ,
                 .li(
                     .class("history"),
-                    "In development for over 5 years, with ",
-                    .a(
-                        .href("https://github.com/Alamofire/Alamofire/commits/master"),
-                        "1,433 commits"
-                    ),
-                    " and ",
-                    .a(
-                        .href("https://github.com/Alamofire/Alamofire/releases"),
-                        "79 releases"
-                    ),
-                    "."
+                    .group(model.historyClause())
                 ),
                 .li(
                     .class("activity"),
@@ -200,16 +166,21 @@ extension PackageShowView {
         let url: URL
         let license: License
         let summary: String
-//        let authors: [Author]
+        let authors: [Link]
+        let history: History
 
-        struct Author {
+        struct Link {
             let name: String
             let url: URL
         }
+
+        struct History {
+            let since: String  // TODO: use Date and derive on the fly
+            let commits: Link
+            let releases: Link
+        }
     }
 }
-
-
 
 
 // FIXME: temporary compile fix
@@ -217,7 +188,64 @@ func packateToModel(_ package: Package) -> PackageShowView.Model {
     .init(title: "Alamofire",
           url: URL(string: "https://github.com/Alamofire/Alamofire.git")!,
           license: .mit,
-          summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quis porttitor erat. Vivamus porttitor mi odio, quis imperdiet velit blandit id. Vivamus vehicula urna eget ipsum laoreet, sed porttitor sapien malesuada. Mauris faucibus tellus at augue vehicula, vitae aliquet felis ullamcorper. Praesent vitae leo rhoncus, egestas elit id, porttitor lacus. Cras ac bibendum mauris. Praesent luctus quis nulla sit amet tempus. Ut pharetra non augue sed pellentesque."
-
+          summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quis porttitor erat. Vivamus porttitor mi odio, quis imperdiet velit blandit id. Vivamus vehicula urna eget ipsum laoreet, sed porttitor sapien malesuada. Mauris faucibus tellus at augue vehicula, vitae aliquet felis ullamcorper. Praesent vitae leo rhoncus, egestas elit id, porttitor lacus. Cras ac bibendum mauris. Praesent luctus quis nulla sit amet tempus. Ut pharetra non augue sed pellentesque.",
+          authors: [
+            .init(name: "Christian Noon", url: URL(string: "https://github.com/cnoon")!),
+            .init(name: "Mattt", url: URL(string: "https://github.com/mattt")!),
+            .init(name: "Jon Shier", url: URL(string: "https://github.com/jshier")!),
+            .init(name: "Kevin Harwood", url: URL(string: "https://github.com/kcharwood")!),
+            .init(name: "186 other contributors", url: URL(string: "https://github.com/Alamofire/Alamofire/graphs/contributors")!),
+        ],
+          history: .init(
+            since: "over 5 years",
+            commits: .init(name: "1,433 commits",
+                           url: URL(string: "https://github.com/Alamofire/Alamofire/commits/master")!),
+            releases: .init(name: "79 releases",
+                            url: URL(string: "https://github.com/Alamofire/Alamofire/releases")!)
+        )
     )
 }
+
+
+extension PackageShowView.Model {
+    func authorsClause() -> [Node<HTML.BodyContext>] {
+        switch authors.count {
+            case 0:
+                return ["–"]
+            case 1:
+                let author = authors.first!
+                return ["By ", .a(.href(author.url), .text(author.name)), "."]
+            case 2:
+                let author1 = authors[0]
+                let author2 = authors[1]
+                return ["By ", .a(.href(author1.url), .text(author1.name)),
+                        " and ", .a(.href(author2.url), .text(author2.name)), "."]
+            default:
+                let start: [Node<HTML.BodyContext>]
+                    = ["By ", .a(.href(authors.first!.url), .text(authors.first!.name))]
+                let middle: [[Node<HTML.BodyContext>]] = authors[1..<(authors.count - 1)].map {
+                        [", ", .a(.href($0.url), .text($0.name))]
+                }
+                let end: [Node<HTML.BodyContext>] =
+                    [", and ", .a(.href(authors.last!.url), .text(authors.last!.name)), "."]
+                return middle.reduce(start) { $0 + $1 } + end
+        }
+    }
+
+    func historyClause() -> [Node<HTML.BodyContext>] {
+        [
+            "In development for \(history.since), with ",
+            .a(
+                .href(history.commits.url),
+                .text(history.commits.name)
+            ),
+            " and ",
+            .a(
+                .href(history.releases.url),
+                .text(history.releases.name)
+            ),
+            "."
+        ]
+    }
+}
+
