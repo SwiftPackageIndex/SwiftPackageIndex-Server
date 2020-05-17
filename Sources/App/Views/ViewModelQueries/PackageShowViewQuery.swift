90e90613207992d73e078ec28ba208a21c224bb2
Sven A. Schmidt <sas@finestructure.co>
2020-05-17 18:25:09 +0200

    Show package name and some version info

--- Sources/App/Views/ViewModelQueries/PackageShowViewQuery.swift
+++ Sources/App/Views/ViewModelQueries/PackageShowViewQuery.swift
@@ -38,13 +38,16 @@ extension PackageShowView.Model {
     static func query(database: Database, packageId: Package.Id) -> EventLoopFuture<Self> {
         Package.query(on: database)
             .with(\.$repositories)
-            .with(\.$versions)
+            .with(\.$versions) { $0.with(\.$products) }
+            .filter(\.$id == packageId)
             .first()
             .unwrap(or: Abort(.notFound))
             .map { p in
                 // TODO: consider which missing values take defaults and which ones error out
-                // or make fields optional and let display deal with it?
-                Self.init(title: "title",
+                // Or do we make view model fields optional and let display deal with it?
+                // If for instance we have just a package entry with no repository, we should
+                // probably error out (because too many fields would be blank).
+                Self.init(title: p.name ?? "–",
                           url: p.url,
                           license: p.repository?.license ?? .other,
                           summary: p.repository?.summary ?? "",
@@ -59,4 +62,16 @@ extension PackageShowView.Model {
                           products: (libraries: 17, executables: 42))
         }
     }
+}
+
+
+private extension Package {
+    // keep this private, because it requires relationships to be eagerly loaded
+    // we do this above but in order to ensure this not being called from elsewhere
+    // where this isn't guaranteed, we keep this extension off limits
+    var defaultVersion: Version? {
+        versions.first(where: { $0.reference?.isBranch ?? false })
+    }
+
+    var name: String? { defaultVersion?.packageName }
 }
