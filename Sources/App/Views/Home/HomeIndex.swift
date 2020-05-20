a1a56c3a5ff47bcccbbc6883fa337cf1bd0f837e
Dave Verwer <dave.verwer@gmail.com>
2020-05-20 14:51:57 +0100

    Basic home page markup.

--- Sources/App/Views/Home/HomeIndex.swift
+++ Sources/App/Views/Home/HomeIndex.swift
@@ -4,7 +4,90 @@ import Plot
 class HomeIndex: PublicPage {
 
     override func content() -> Node<HTML.BodyContext> {
-        .p("Home page")
+        .group(
+            .div(
+                .class("search"),
+                .p(
+                    "The place to find Swift packages."
+                ),
+                .form(
+                    .input(
+                        .type(.text),
+                        .attribute(named: "spellcheck", value: "false"), // TODO: Fix after Plot update
+                        .placeholder("Search")
+                    )
+                )
+            ),
+            .div(
+                .class("split"),
+                .section(
+                    .class("recent_packages"),
+                    .ul(
+                        .li(
+                            "Package",
+                            .element(named: "small", text: "2 hours ago.") // TODO: Fix after Plot update
+                        ),
+                        .li(
+                            "Package",
+                            .element(named: "small", text: "2 hours ago.") // TODO: Fix after Plot update
+                        ),
+                        .li(
+                            "Package",
+                            .element(named: "small", text: "2 hours ago.") // TODO: Fix after Plot update
+                        ),
+                        .li(
+                            "Package",
+                            .element(named: "small", text: "2 hours ago.") // TODO: Fix after Plot update
+                        ),
+                        .li(
+                            "Package",
+                            .element(named: "small", text: "2 hours ago.") // TODO: Fix after Plot update
+                        ),
+                        .li(
+                            "Package",
+                            .element(named: "small", text: "2 hours ago.") // TODO: Fix after Plot update
+                        ),
+                        .li(
+                            "Package",
+                            .element(named: "small", text: "2 hours ago.") // TODO: Fix after Plot update
+                        )
+                    )
+                ),
+                .section(
+                    .class("recent_releases"),
+                    .ul(
+                        .li(
+                            "Package",
+                            .element(named: "small", text: "2 hours ago.") // TODO: Fix after Plot update
+                        ),
+                        .li(
+                            "Package",
+                            .element(named: "small", text: "2 hours ago.") // TODO: Fix after Plot update
+                        ),
+                        .li(
+                            "Package",
+                            .element(named: "small", text: "2 hours ago.") // TODO: Fix after Plot update
+                        ),
+                        .li(
+                            "Package",
+                            .element(named: "small", text: "2 hours ago.") // TODO: Fix after Plot update
+                        ),
+                        .li(
+                            "Package",
+                            .element(named: "small", text: "2 hours ago.") // TODO: Fix after Plot update
+                        ),
+                        .li(
+                            "Package",
+                            .element(named: "small", text: "2 hours ago.") // TODO: Fix after Plot update
+                        ),
+                        .li(
+                            "Package",
+                            .element(named: "small", text: "2 hours ago.") // TODO: Fix after Plot update
+                        )
+                    )
+                )
+            )
+        )
     }
 
     override func navItems() -> [Node<HTML.ListContext>] {
