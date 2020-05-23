84edfe8a93e13fb309c398ca018874ecf758de6d
Dave Verwer <dave.verwer@gmail.com>
2020-05-23 15:59:55 +0100

    Display a proper error message when something goes wrong in the fetch of the search JSON.

--- Public/javascripts/main.js
+++ Public/javascripts/main.js
@@ -31,11 +31,19 @@ document.addEventListener('DOMContentLoaded', function(event) {
 window.performSearch = _.debounce(function(searchQuery) {
   const searchUrl = '/api/search?query=' + searchQuery
 
+  // Clear out any existing content. Errors, the loading indicator, or previous results.
+  clearSearchResults()
+
   axios.get(searchUrl).then(function(response) {
     displaySearchResults(response.data)
   }).catch(function(error) {
-    console.error(error) // TODO: Display the error.
+    console.error(error) // At the very least, always log to the console.
+    displayErrorMessage(error)
   })
+
+  // Doesn't matter if there was an error, or valid results, always show the results area.
+  const resultsElement = document.getElementById('results')
+  if (!!resultsElement) { resultsElement.hidden = false }
 }, 200)
 
 window.clearSearchResults = function() {
@@ -50,9 +58,6 @@ window.clearSearchResults = function() {
 window.displaySearchResults = function(searchResults) {
   const resultsElement = document.getElementById('results')
   if (resultsElement == null) { return }
-
-  // Clear out any existing content. Either the loading indicator, or previous results.
-  clearSearchResults()
 
   // Are there any results?
   const numResults = searchResults.results.length
@@ -77,9 +82,43 @@ window.displaySearchResults = function(searchResults) {
       resultsElement.appendChild(moreResultsElement)
     }
   }
+}
 
-  // At the end of this process, the element should *always* be visible.
-  resultsElement.hidden = false
+window.displayErrorMessage = function(error) {
+  const resultsElement = document.getElementById('results')
+  if (!resultsElement == null) { return }
+
+  // Container for the error message.
+  const errorContainerElement = document.createElement('div')
+  errorContainerElement.classList.add('error')
+  resultsElement.appendChild(errorContainerElement)
+
+  // Start with an icon.
+  const errorIconElement = document.createElement('i')
+  errorIconElement.classList.add('icon')
+  errorIconElement.classList.add('warning')
+  errorContainerElement.appendChild(errorIconElement)
+
+  // Header, with a quick apology.
+  const errorHeaderElement = document.createElement('h4')
+  errorHeaderElement.textContent = 'Something went wrong. Sorry!'
+  errorContainerElement.appendChild(errorHeaderElement)
+
+  // Then, what actually happened.
+  const errorMessageElement = document.createElement('p')
+  errorContainerElement.appendChild(errorMessageElement)
+
+  // Finally, what was the error?
+  if (!!error.response) {
+    errorMessageElement.textContent = error.response.status + ' – ' + error.response.statusText
+
+    // Is there any extra information in the "reason" that might be useful?
+    if (!!error.response.data && !!error.response.data.reason && error.response.data.reason != error.response.statusText) {
+      errorMessageElement.textContent +=  ' – ' + error.response.data.reason
+    }
+  } else {
+    errorMessageElement.textContent = 'Unexpected Error.'
+  }
 }
 
 // Helpers
