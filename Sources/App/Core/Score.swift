5805891979ea9e213ac8dab9ed3de09317e69680
Sven A. Schmidt <sas@finestructure.co>
2020-05-24 17:36:56 +0200

    Add Score.compute

---
+++ Sources/App/Core/Score.swift
@@ -0,0 +1,33 @@
+enum Score {
+    struct Input {
+        var supportsLatestSwiftVersion: Bool
+        var releaseCount: Int
+        var likeCount: Int
+    }
+
+    static func compute(_ candidate: Input) -> Int {
+        var score = 0
+
+        // Swift major version support
+        if candidate.supportsLatestSwiftVersion { score += 10 }
+
+        // Number of releases
+        switch candidate.releaseCount {
+            case  ..<5 :   break
+            case 5..<20:   score += 10
+            default    :   score += 20
+        }
+
+        // Stars count
+        switch candidate.likeCount {
+            case      ..<25    :  break
+            case    25..<100   :  score += 10
+            case   100..<500   :  score += 20
+            case   500..<5_000 :  score += 30
+            case 5_000..<10_000:  score += 40
+            default:              score += 50
+        }
+
+        return score
+    }
+}
---
+++ Tests/AppTests/ScoreTests.swift
@@ -0,0 +1,32 @@
+@testable import App
+
+import XCTest
+
+
+class ScoreTests: XCTestCase {
+
+    func test_computeScore() throws {
+        XCTAssertEqual(Score.compute(.init(supportsLatestSwiftVersion: false,
+                                           releaseCount: 0,
+                                           likeCount: 0)),
+                       0)
+        XCTAssertEqual(Score.compute(.init(supportsLatestSwiftVersion: true,
+                                           releaseCount: 0,
+                                           likeCount: 0)),
+                       10)
+        XCTAssertEqual(Score.compute(.init(supportsLatestSwiftVersion: true,
+                                           releaseCount: 10,
+                                           likeCount: 0)),
+                       20)
+        XCTAssertEqual(Score.compute(.init(supportsLatestSwiftVersion: true,
+                                           releaseCount: 10,
+                                           likeCount: 50)),
+                       30)
+        // current maximum value
+        XCTAssertEqual(Score.compute(.init(supportsLatestSwiftVersion: true,
+                                           releaseCount: 20,
+                                           likeCount: 20_000)),
+                       80)
+    }
+
+}
