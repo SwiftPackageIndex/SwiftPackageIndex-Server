3ab0b22cd782b2afeb84c7fc084fdcb409a3eafa
Dave Verwer <dave.verwer@gmail.com>
2020-05-26 17:50:03 +0100

    Final refactoring of the external link retargeter.

---
+++ Public/javascripts/external_link_retargeter.js
@@ -0,0 +1,21 @@
+export class ExternalLinkRetargeter {
+  constructor() {
+    document.addEventListener('DOMContentLoaded', () => {
+      this.installDocumentEventHandlers()
+    })
+  }
+
+  installDocumentEventHandlers() {
+    document.addEventListener('click', (event) => {
+      var target = event.target
+      do {
+        // Force a blank target for anything that's a link, with a destination that's on a different host.
+        if (target.nodeName.toLowerCase() == 'a' && target.hostname != window.location.hostname) {
+          target.setAttribute('target', '_blank')
+        }
+      // Move up the DOM, in case the click was on a nested element but the link is further up in the hierarchy.
+      // If there's no links in the hierarchy from here up, this will eventually hit the document root and finish.
+      } while ((target = target.parentElement))
+    })
+  }
+}
--- Public/javascripts/main.js
+++ Public/javascripts/main.js
@@ -1,7 +1,7 @@
-import { OpenExternalLinksInBlankTarget } from './links.js'
+import { ExternalLinkRetargeter } from './external_link_retargeter.js'
 import { SPISearchCore } from './search_core.js'
 import { SPISearchKeyboardNavigation } from './search_keyboard_navigation.js'
 
-window.externalLinkRetargeter = new OpenExternalLinksInBlankTarget()
+window.externalLinkRetargeter = new ExternalLinkRetargeter()
 window.spiSearchCore = new SPISearchCore()
 window.spiSearchKeyboardNavigation = new SPISearchKeyboardNavigation()
