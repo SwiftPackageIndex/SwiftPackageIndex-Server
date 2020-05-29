c5d69712140d3c605793f251a417a174e000beaf
Sven A. Schmidt <sas@finestructure.co>
2020-05-29 15:34:51 +0200

    Add search view

--- Sources/App/Migrations/CreateMaterializedViews.swift
+++ Sources/App/Migrations/CreateMaterializedViews.swift
@@ -63,3 +63,34 @@ struct CreateRecentReleases: Migration {
         return db.raw("DROP MATERIALIZED VIEW recent_releases").run()
     }
 }
+
+
+struct CreateSearch: Migration {
+    func prepare(on database: Database) -> EventLoopFuture<Void> {
+        guard let db = database as? SQLDatabase else {
+            fatalError("Database must be an SQLDatabase ('as? SQLDatabase' must succeed)")
+        }
+        return db.raw(
+            """
+            CREATE MATERIALIZED VIEW search AS
+            SELECT
+              p.id,
+              v.package_name,
+              r.name,
+              r.owner,
+              r.summary
+            FROM packages p
+              JOIN repositories r ON r.package_id = p.id
+              JOIN versions v ON v.package_id = p.id
+            WHERE v.reference ->> 'branch' = r.default_branch
+            """
+        ).run()
+    }
+
+    func revert(on database: Database) -> EventLoopFuture<Void> {
+        guard let db = database as? SQLDatabase else {
+            fatalError("Database must be an SQLDatabase ('as? SQLDatabase' must succeed)")
+        }
+        return db.raw("DROP MATERIALIZED VIEW search").run()
+    }
+}
