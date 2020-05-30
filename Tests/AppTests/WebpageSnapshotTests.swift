4180d90ae69eadde5da8bb424b7faec4e056aba4
Dave Verwer <dave.verwer@gmail.com>
2020-05-30 20:50:30 +0100

    Added a snapshot test for the error page.

--- Tests/AppTests/WebpageSnapshotTests.swift
+++ Tests/AppTests/WebpageSnapshotTests.swift
@@ -170,6 +170,27 @@ class WebpageSnapshotTests: XCTestCase {
         #endif
     }
 
+    func test_ErrorPageView() throws {
+        let model = ErrorPage.Model(status: .notFound, error: nil)
+        let page = ErrorPage.View(model).document()
+
+        let recordSnapshotForThisTest = false
+        record = recordSnapshotForThisTest || recordSnapshotForAllTests
+
+        assertSnapshot(matching: page.render(indentedBy: .spaces(2)), as: .lines)
+
+        #if os(macOS)
+        if !isRunningInCI {
+            // FIXME: css and image loading broken, despite setting correct base url
+            // permission issue? In a macOS app project this required setting
+            // com.apple.security.network.client permissions but I don't see how to do
+            // that with SPM - nor would I expect to need that for tests?
+            assertSnapshot(matching: page, as: .image(size: .init(width: 800, height: 800),
+                                                      baseURL: baseURL()))
+        }
+        #endif
+    }
+
 }
 
 
