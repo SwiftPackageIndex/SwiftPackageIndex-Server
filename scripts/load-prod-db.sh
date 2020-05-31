0997a5c8cee782e81fc76ee2c92996917d579f05
Sven A. Schmidt <sas@finestructure.co>
2020-05-31 11:56:34 +0200

    Add db export/import scripts

---
+++ scripts/load-prod-db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eu
+IMPORT_FILE=$1
+docker rm -f spi_dev
+docker run --name spi_dev -e POSTGRES_DB=spi_dev -e POSTGRES_USER=spi_dev -e POSTGRES_PASSWORD=xxx -p 6432:5432 -d postgres:12.1-alpine
+echo "Giving Postgres a moment to launch ..."
+sleep 5
+echo "Importing"
+PASSWORD=xxx psql -h localhost -p 6432 -U spi_dev spi_dev < $IMPORT_FILE
