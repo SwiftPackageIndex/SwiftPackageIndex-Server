cd22615ed004fffda3d53c053d582679b70d603b
Sven A. Schmidt <sas@finestructure.co>
2020-06-03 11:48:24 +0200

    Fix indent

--- Sources/App/Views/Home/HomeIndex+Query.swift
+++ Sources/App/Views/Home/HomeIndex+Query.swift
@@ -19,9 +19,9 @@ extension HomeIndex.Model {
     }
 
     static func makeLink(_ recent: RecentRelease) -> Link {
-        return .init(label: recent.packageName, url:
-            SiteURL.package(.value(recent.repositoryOwner),
-                            .value(recent.repositoryName)).relativeURL)
+        return .init(label: recent.packageName,
+                     url: SiteURL.package(.value(recent.repositoryOwner),
+                                          .value(recent.repositoryName)).relativeURL)
     }
 
     static func makeDatedLink(_ recent: RecentPackage) -> DatedLink {
