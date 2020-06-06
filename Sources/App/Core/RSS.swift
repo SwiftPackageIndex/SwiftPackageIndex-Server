c89d863b05fa95817912e7c30fac3ddac8af15f3
Sven A. Schmidt <sas@finestructure.co>
2020-06-06 13:48:49 +0200

    Add test_render_feed

--- Sources/App/Core/RSS.swift
+++ Sources/App/Core/RSS.swift
@@ -1,3 +1,4 @@
+import Foundation
 import Plot
 
 
@@ -14,5 +15,26 @@ struct RSSFeed {
                 .link(link)
             )
         }
+    }
+
+    var title: String
+    var description: String
+    var link: URL
+    var maxItemCount: Int
+    var items: [Item]
+
+    var rss: RSS {
+        RSS(
+            .title(title),
+            .description(description),
+            .link(link),
+            //  .language(language),
+            //  .lastBuildDate(date, timeZone: context.dateFormatter.timeZone),
+            //  .pubDate(date, timeZone: context.dateFormatter.timeZone),
+            //  .ttl(Int(config.ttlInterval)),
+            //  .atomLink(context.site.url(for: config.targetPath)),
+            .forEach(items.prefix(maxItemCount), \.node)
+        )
+
     }
 }
--- Tests/AppTests/RSSTests.swift
+++ Tests/AppTests/RSSTests.swift
@@ -8,8 +8,25 @@ class RSSTests: XCTestCase {
 
     func test_render_item() throws {
         let item = RSSFeed.Item(title: "title", link: "link", content: "content")
-        assertSnapshot(matching: item.node.render(indentedBy: .spaces(2)),
-                       as: .lines)
+        assertSnapshot(matching: item.node.render(indentedBy: .spaces(2)), as: .lines)
+    }
+
+    func test_render_feed() throws {
+        // Test generated feed. The result should validate successfully
+        // on https://validator.w3.org/feed/check.cgi
+        let feed = RSSFeed(title: "feed title", description: "feed description",
+                           link: "https://SwiftPackageIndex.com",
+                           maxItemCount: 100,
+                           items: [
+                            RSSFeed.Item(title: "title",
+                                         link: "https://SwiftPackageIndex.com/foo/bar",
+                                         content: "content"),
+                            RSSFeed.Item(title: "title",
+                                         link: "https://SwiftPackageIndex.com/bar/baz",
+                                         content: "content")]
+        )
+        assertSnapshot(matching: feed.rss.render(indentedBy: .spaces(2)),
+                       as: .init(pathExtension: "xml", diffing: .lines))
     }
 
 }
