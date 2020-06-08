604b7dcbb51ca1dc432781434286f9ef6d7fb550
Sven A. Schmidt <sas@finestructure.co>
2020-06-08 15:09:43 +0200

    Add page-title attribute to markdown files

--- Sources/App/Views/MarkdownPage.swift
+++ Sources/App/Views/MarkdownPage.swift
@@ -2,16 +2,31 @@ import Foundation
 import Plot
 import Ink
 
+
 class MarkdownPage: PublicPage {
 
-    let pathToMarkdownFile: String
+    enum Metadata: String {
+        case pageTitle = "page-title"
+    }
+
+    let metadata: [String: String]
+    let html: String?
 
     init(_ markdownFilename: String) {
         assert(markdownFilename.split(separator: "/").count < 2, "Filename should not include path separators.")
 
-        pathToMarkdownFile = Current.fileManager.workingDirectory()
+        let pathToMarkdownFile = Current.fileManager.workingDirectory()
             .appending("Resources/Markdown/")
             .appending(markdownFilename)
+
+        let markdown = try? String(contentsOfFile: pathToMarkdownFile)
+        let result = markdown.map(MarkdownParser().parse)
+        metadata = result?.metadata ?? [:]
+        html = result?.html
+    }
+
+    override func pageTitle() -> String? {
+        metadata[Metadata.pageTitle]
     }
 
     override func bodyClass() -> String? {
@@ -19,13 +34,17 @@ class MarkdownPage: PublicPage {
     }
 
     override func content() -> Node<HTML.BodyContext> {
-        do {
-            let markdown = try String(contentsOfFile: pathToMarkdownFile)
-            let rawHTML = MarkdownParser().html(from: markdown)
-            return .raw(rawHTML)
-        } catch {
+        guard let html = html else {
             return .p("Markdown file not found!")
         }
+        return .raw(html)
     }
 
+}
+
+
+private extension Dictionary where Key == String, Value == String {
+    subscript(_ key: MarkdownPage.Metadata) -> String? {
+        self[key.rawValue]
+    }
 }
