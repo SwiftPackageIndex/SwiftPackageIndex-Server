c6eecfb79e66c2d8655a202c77083c7cfc1b792d
Dave Verwer <dave.verwer@gmail.com>
2020-06-09 01:00:15 +0100

    Added JavaScript for the copy button.

--- Public/javascripts/main.js
+++ Public/javascripts/main.js
@@ -1,7 +1,9 @@
 import { ExternalLinkRetargeter } from './external_link_retargeter.js'
 import { SPISearchCore } from './search_core.js'
 import { SPISearchKeyboardNavigation } from './search_keyboard_navigation.js'
+import { SPICopyPackageURL } from './copy_package_url.js'
 
 window.externalLinkRetargeter = new ExternalLinkRetargeter()
 window.spiSearchCore = new SPISearchCore()
 window.spiSearchKeyboardNavigation = new SPISearchKeyboardNavigation()
+window.spiCopyPackageURL = new SPICopyPackageURL()
