6745143da9c6cc68859a39184f64dadb304c4e30
Dave Verwer <dave.verwer@gmail.com>
2020-06-10 09:13:46 +0100

    Added summary to the RSS tests.

--- Tests/AppTests/RSSTests.swift
+++ Tests/AppTests/RSSTests.swift
@@ -58,7 +58,7 @@ class RSSTests: AppTestCase {
             // re-write creation date to something stable for snapshotting
             pkg.createdAt = Date(timeIntervalSince1970: TimeInterval(100*$0))
             try pkg.save(on: app.db).wait()
-            try Repository(package: pkg, name: "pkg-\($0)", owner: "owner-\($0)").create(on: app.db).wait()
+            try Repository(package: pkg, summary: "Summary", name: "pkg-\($0)", owner: "owner-\($0)").create(on: app.db).wait()
             try Version(package: pkg, packageName: "pkg-\($0)").save(on: app.db).wait()
         }
         // make sure to refresh the materialized view
@@ -77,7 +77,7 @@ class RSSTests: AppTestCase {
         try (1...10).forEach {
             let pkg = Package(id: UUID(), url: "\($0)".asGithubUrl.url)
             try pkg.save(on: app.db).wait()
-            try Repository(package: pkg, name: "pkg-\($0)", owner: "owner-\($0)").create(on: app.db).wait()
+            try Repository(package: pkg, summary: "Summary", name: "pkg-\($0)", owner: "owner-\($0)").create(on: app.db).wait()
             try Version(package: pkg,
                         reference: .tag(.init($0, 0, 0), "\($0).0.0"),
                         packageName: "pkg-\($0)",
