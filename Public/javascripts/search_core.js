da2cd95229e6525ebeb2278c1853dd1d3203316f
Dave Verwer <dave.verwer@gmail.com>
2020-06-15 21:15:30 +0100

    Fixed up the debounce and re-minified.

--- Public/javascripts/search_core.js
+++ Public/javascripts/search_core.js
@@ -39,7 +39,7 @@ export class SPISearchCore {
     // When any input is received by the query field, perform the search.
     queryFieldElement.addEventListener('input', debounce(() => {
       this.performSearch()
-    }, 200), 300)
+    }, 300))
   }
 
   performSearch() {
