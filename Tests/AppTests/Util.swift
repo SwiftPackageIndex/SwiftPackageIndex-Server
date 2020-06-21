b5115146baefa1543f71d240e33cc5732fdb2846
Sven A. Schmidt <sas@finestructure.co>
2020-06-21 16:10:40 +0200

    Ensure schema is created and tables loaded

--- Tests/AppTests/Util.swift
+++ Tests/AppTests/Util.swift
@@ -8,6 +8,7 @@ import XCTest
 
 // MARK: - Test helpers
 
+private var _schemaCreated = false
 
 func setup(_ environment: Environment) throws -> Application {
     // Always start with a baseline mock environment to avoid hitting live resources
@@ -16,19 +17,39 @@ func setup(_ environment: Environment) throws -> Application {
     let app = Application(.testing)
     app.logger.logLevel = Environment.get("LOG_LEVEL").flatMap(Logger.Level.init(rawValue:)) ?? .warning
     try configure(app)
+    if !_schemaCreated {
+        // ensure we create the schema when running the first test
+        try app.autoMigrate().wait()
+        _schemaCreated = true
+    }
     return app
 }
 
+
+private var _tables: [String]?
 
 func resetDb(_ app: Application) throws {
     guard let db = app.db as? SQLDatabase else {
         fatalError("Database must be an SQLDatabase ('as? SQLDatabase' must succeed)")
     }
 
-    let tables = ["packages",
-                  "products",
-                  "repositories",
-                  "versions"]
+    guard let tables = _tables else {
+        struct Row: Decodable { var table_name: String }
+        _tables = try db.raw("""
+                SELECT table_name FROM
+                information_schema.tables
+                WHERE
+                  table_schema NOT IN ('pg_catalog', 'information_schema', 'public._fluent_migrations')
+                  AND table_schema NOT LIKE 'pg_toast%'
+                  AND table_name NOT LIKE '_fluent_%'
+                """)
+            .all(decoding: Row.self)
+            .wait()
+            .map(\.table_name)
+        if _tables != nil { try resetDb(app) }
+        return
+    }
+
     for table in tables {
         try db.raw("TRUNCATE TABLE \(table) CASCADE").run().wait()
     }
