f53c19258499084f224389dd8353957986bd1e0f
Dave Verwer <dave.verwer@gmail.com>
2020-06-29 12:08:54 +0100

    Whitespace on merged file.

--- Tests/AppTests/WebpageSnapshotTests.swift
+++ Tests/AppTests/WebpageSnapshotTests.swift
@@ -9,19 +9,19 @@ let defaultSize = CGSize(width: 800, height: 600)
 let recordSnapshotForAllTests = false
 
 class WebpageSnapshotTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         Current.date = { Date(timeIntervalSince1970: 0) }
     }
-
+    
     func test_home() throws {
         let page = PublicPage.admin()
-
+        
         let recordSnapshotForThisTest = false
         SnapshotTesting.record = recordSnapshotForThisTest || recordSnapshotForAllTests
-
+        
         assertSnapshot(matching: page.render(indentedBy: .spaces(2)), as: .lines)
-
+        
         // Snapshot renders slightly differently on macOS 11 (swift 5.3) - exclude it for now
         #if os(macOS) && swift(<5.3)
         if !isRunningInCI {
@@ -29,15 +29,15 @@ class WebpageSnapshotTests: XCTestCase {
         }
         #endif
     }
-
+    
     func test_HomeIndexView() throws {
         let page = HomeIndex.View(path: "/", model: .mock).document()
-
+        
         let recordSnapshotForThisTest = false
         SnapshotTesting.record = recordSnapshotForThisTest || recordSnapshotForAllTests
-
+        
         assertSnapshot(matching: page.render(indentedBy: .spaces(2)), as: .lines)
-
+        
         #if os(macOS)
         if !isRunningInCI {
             // FIXME: css and image loading broken, despite setting correct base url
@@ -49,15 +49,15 @@ class WebpageSnapshotTests: XCTestCase {
         }
         #endif
     }
-
+    
     func test_PackageShowView() throws {
         let page = PackageShow.View(path: "", model: .mock).document()
-
+        
         let recordSnapshotForThisTest = false
         SnapshotTesting.record = recordSnapshotForThisTest || recordSnapshotForAllTests
-
+        
         assertSnapshot(matching: page.render(indentedBy: .spaces(2)), as: .lines)
-
+        
         #if os(macOS)
         if !isRunningInCI {
             // FIXME: css and image loading broken, despite setting correct base url
@@ -75,12 +75,12 @@ class WebpageSnapshotTests: XCTestCase {
         model.summary = ":package: Nothing but Cache. :octocat:"
         
         let page = PackageShow.View(path: "", model: model).document()
-
+        
         let recordSnapshotForThisTest = false
         SnapshotTesting.record = recordSnapshotForThisTest || recordSnapshotForAllTests
-
+        
         assertSnapshot(matching: page.render(indentedBy: .spaces(2)), as: .lines)
-
+        
         #if os(macOS)
         if !isRunningInCI {
             // FIXME: css and image loading broken, despite setting correct base url
@@ -92,17 +92,17 @@ class WebpageSnapshotTests: XCTestCase {
         }
         #endif
     }
-
+    
     func test_PackageShowView_unknown_license() throws {
         var model = PackageShow.Model.mock
         model.license = License.none
         let page = PackageShow.View(path: "", model: model).document()
-
+        
         let recordSnapshotForThisTest = false
         SnapshotTesting.record = recordSnapshotForThisTest || recordSnapshotForAllTests
-
+        
         assertSnapshot(matching: page.render(indentedBy: .spaces(2)), as: .lines)
-
+        
         #if os(macOS)
         if !isRunningInCI {
             // FIXME: css and image loading broken, despite setting correct base url
@@ -114,17 +114,17 @@ class WebpageSnapshotTests: XCTestCase {
         }
         #endif
     }
-
+    
     func test_PackageShowView_incompatible_license() throws {
         var model = PackageShow.Model.mock
         model.license = License.gpl_3_0
         let page = PackageShow.View(path: "", model: model).document()
-
+        
         let recordSnapshotForThisTest = false
         SnapshotTesting.record = recordSnapshotForThisTest || recordSnapshotForAllTests
-
+        
         assertSnapshot(matching: page.render(indentedBy: .spaces(2)), as: .lines)
-
+        
         #if os(macOS)
         if !isRunningInCI {
             // FIXME: css and image loading broken, despite setting correct base url
@@ -136,7 +136,7 @@ class WebpageSnapshotTests: XCTestCase {
         }
         #endif
     }
-
+    
     func test_PackageShowView_no_authors_activity() throws {
         // Test to ensure we don't display empty bullet points when there is
         // no author or activity info
@@ -144,12 +144,12 @@ class WebpageSnapshotTests: XCTestCase {
         model.authors = nil
         model.activity = nil
         let page = PackageShow.View(path: "", model: model).document()
-
+        
         let recordSnapshotForThisTest = false
         SnapshotTesting.record = recordSnapshotForThisTest || recordSnapshotForAllTests
-
+        
         assertSnapshot(matching: page.render(indentedBy: .spaces(2)), as: .lines)
-
+        
         #if os(macOS)
         if !isRunningInCI {
             // FIXME: css and image loading broken, despite setting correct base url
@@ -161,19 +161,19 @@ class WebpageSnapshotTests: XCTestCase {
         }
         #endif
     }
-
+    
     func test_PackageShowView_no_lpInfo() throws {
         // Test display when there is no L&P info at all
         // no author or activity info
         var model = PackageShow.Model.mock
         model.languagePlatforms = .init(stable: nil, beta: nil, latest: nil)
         let page = PackageShow.View(path: "", model: model).document()
-
+        
         let recordSnapshotForThisTest = false
         SnapshotTesting.record = recordSnapshotForThisTest || recordSnapshotForAllTests
-
+        
         assertSnapshot(matching: page.render(indentedBy: .spaces(2)), as: .lines)
-
+        
         #if os(macOS)
         if !isRunningInCI {
             // FIXME: css and image loading broken, despite setting correct base url
@@ -185,7 +185,7 @@ class WebpageSnapshotTests: XCTestCase {
         }
         #endif
     }
-
+    
     func test_PackageShowView_no_platforms() throws {
         // Test display when there is no platform info
         // see: https://github.com/SwiftPackageIndex/SwiftPackageIndex-Server/pull/195#issue-424606548
@@ -194,12 +194,12 @@ class WebpageSnapshotTests: XCTestCase {
         model.languagePlatforms.beta?.platforms = []
         model.languagePlatforms.latest?.platforms = []
         let page = PackageShow.View(path: "", model: model).document()
-
+        
         let recordSnapshotForThisTest = false
         SnapshotTesting.record = recordSnapshotForThisTest || recordSnapshotForAllTests
-
+        
         assertSnapshot(matching: page.render(indentedBy: .spaces(2)), as: .lines)
-
+        
         #if os(macOS)
         if !isRunningInCI {
             // FIXME: css and image loading broken, despite setting correct base url
@@ -211,7 +211,7 @@ class WebpageSnapshotTests: XCTestCase {
         }
         #endif
     }
-
+    
     func test_PackageShowView_no_versions() throws {
         // Test display when there is no version info
         // see: https://github.com/SwiftPackageIndex/SwiftPackageIndex-Server/pull/195#issue-424606548
@@ -220,12 +220,12 @@ class WebpageSnapshotTests: XCTestCase {
         model.languagePlatforms.beta?.swiftVersions = []
         model.languagePlatforms.latest?.swiftVersions = []
         let page = PackageShow.View(path: "", model: model).document()
-
+        
         let recordSnapshotForThisTest = false
         SnapshotTesting.record = recordSnapshotForThisTest || recordSnapshotForAllTests
-
+        
         assertSnapshot(matching: page.render(indentedBy: .spaces(2)), as: .lines)
-
+        
         #if os(macOS)
         if !isRunningInCI {
             // FIXME: css and image loading broken, despite setting correct base url
@@ -237,16 +237,16 @@ class WebpageSnapshotTests: XCTestCase {
         }
         #endif
     }
-
+    
     func test_ErrorPageView() throws {
         let model = ErrorPage.Model(Abort(.notFound))
         let page = ErrorPage.View(path: "", model: model).document()
-
+        
         let recordSnapshotForThisTest = false
         SnapshotTesting.record = recordSnapshotForThisTest || recordSnapshotForAllTests
-
+        
         assertSnapshot(matching: page.render(indentedBy: .spaces(2)), as: .lines)
-
+        
         #if os(macOS)
         if !isRunningInCI {
             // FIXME: css and image loading broken, despite setting correct base url
@@ -258,15 +258,15 @@ class WebpageSnapshotTests: XCTestCase {
         }
         #endif
     }
-
+    
     func test_MarkdownPage() throws {
         let page = MarkdownPage(path: "", "privacy.md").document()
-
+        
         let recordSnapshotForThisTest = false
         SnapshotTesting.record = recordSnapshotForThisTest || recordSnapshotForAllTests
-
+        
         assertSnapshot(matching: page.render(indentedBy: .spaces(2)), as: .lines)
-
+        
         #if os(macOS)
         if !isRunningInCI {
             // FIXME: css and image loading broken, despite setting correct base url
@@ -278,7 +278,7 @@ class WebpageSnapshotTests: XCTestCase {
         }
         #endif
     }
-
+    
 }
 
 
