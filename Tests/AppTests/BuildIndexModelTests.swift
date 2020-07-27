d647b1c118cb737f5d4237cae71db636e32b7574
Sven A. Schmidt <sas@finestructure.co>
2020-07-27 12:33:54 +0200

    Pull out Model to own file, add test class

---
+++ Tests/AppTests/BuildIndexModelTests.swift
@@ -0,0 +1,28 @@
+@testable import App
+
+import XCTVapor
+
+
+class BuildIndexModelTests: AppTestCase {
+
+    func test_init_no_name() throws {
+        // Tests behaviour when we're lacking data
+        // setup package without package name
+        let pkg = try savePackage(on: app.db, "1".url)
+        try Repository(package: pkg,
+                       summary: "summary",
+                       defaultBranch: "main",
+                       license: .mit,
+                       name: "bar",
+                       owner: "foo",
+                       stars: 17,
+                       forks: 42).save(on: app.db).wait()
+
+        // MUT
+        let m = BuildIndex.Model(package: pkg)
+
+        // validate
+        XCTAssertNil(m)
+    }
+
+}
