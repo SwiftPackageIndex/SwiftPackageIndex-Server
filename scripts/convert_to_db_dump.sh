54fe3d075433df9e0f74009b95114121a3e1e8e2
Sven A. Schmidt <sas@finestructure.co>
2020-08-29 14:52:17 +0200

    Well, of course we need docker, just not the login, and we need the path

--- scripts/convert_to_db_dump.sh
+++ scripts/convert_to_db_dump.sh
@@ -11,12 +11,12 @@ DUMPFILE=$(basename $TARFILE).dump
 
 tar xvfz $TARFILE
 
-docker run --rm --name backup-db -d -v "$PWD/db_data:/var/lib/postgresql/data" -p 9432:5432 postgres:12.1-alpine
+/usr/local/bin/docker run --rm --name backup-db -d -v "$PWD/db_data:/var/lib/postgresql/data" -p 9432:5432 postgres:12.1-alpine
 
 echo "Exporting ..."
 
 RETRIES=20
-until env PGPASSWORD=${DATABASE_PASSWORD} pg_dump --no-owner -Fc -h localhost -p 9432 -U spi_${ENV} spi_${ENV} > $DUMPFILE || [ $RETRIES -eq 0 ]; do
+until env PGPASSWORD=${DATABASE_PASSWORD} /usr/local/bin/pg_dump --no-owner -Fc -h localhost -p 9432 -U spi_${ENV} spi_${ENV} > $DUMPFILE || [ $RETRIES -eq 0 ]; do
     echo "Waiting for postgres server, $((RETRIES-=1)) remaining attempts..."
     sleep 2
 done
