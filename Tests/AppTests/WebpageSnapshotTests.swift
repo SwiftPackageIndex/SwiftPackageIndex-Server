95db3c9900a2080fa50b5612ac4f6514f60e9148
Sven A. Schmidt <sas@finestructure.co>
2020-08-31 16:32:43 +0200

    Add single row tables test

--- Tests/AppTests/WebpageSnapshotTests.swift
+++ Tests/AppTests/WebpageSnapshotTests.swift
@@ -179,6 +179,56 @@ class WebpageSnapshotTests: XCTestCase {
         #endif
     }
     
+    func test_PackageShowView_single_row_tables() throws {
+        // Test display when all three significant version collapse to a single row
+        var model = PackageShow.Model.mock
+        do {
+            let compatible = PackageShow.Model.SwiftVersionResults(
+                status4_2: .compatible,
+                status5_0: .compatible,
+                status5_1: .compatible,
+                status5_2: .compatible,
+                status5_3: .compatible
+            )
+            model.swiftVersionBuildInfo = .init(
+                stable: .init(referenceName: "5.2.5", results: compatible),
+                beta: .init(referenceName: "6.0.0-b1", results: compatible),
+                latest: .init(referenceName: "main", results: compatible)
+            )
+        }
+        do {
+            let compatible = PackageShow.Model.PlatformResults(
+                iosStatus: .compatible,
+                linuxStatus: .compatible,
+                macosStatus: .compatible,
+                macosArmStatus: .compatible,
+                tvosStatus: .compatible,
+                watchosStatus: .compatible
+            )
+            model.platformBuildInfo = .init(
+                stable: .init(referenceName: "5.2.5", results: compatible),
+                beta: .init(referenceName: "6.0.0-b1", results: compatible),
+                latest: .init(referenceName: "main", results: compatible)
+            )
+        }
+        let page = { PackageShow.View(path: "", model: model).document() }
+        
+        let recordSnapshotForThisTest = false
+        SnapshotTesting.isRecording = recordSnapshotForThisTest || recordSnapshotForAllTests
+        
+        assertSnapshot(matching: page, as: .html)
+        
+        #if os(macOS)
+        if !isRunningInCI {
+            configs.forEach {
+                assertSnapshot(matching: page,
+                               as: .image(size: $0.size, baseURL: TempWebRoot.baseURL),
+                               named: $0.name)
+            }
+        }
+        #endif
+    }
+    
     func test_ErrorPageView() throws {
         let model = ErrorPage.Model(Abort(.notFound))
         let page = { ErrorPage.View(path: "", model: model).document() }
