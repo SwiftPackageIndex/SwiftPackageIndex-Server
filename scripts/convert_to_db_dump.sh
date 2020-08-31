ed5502abb0840b0c29559c161e938d0b4305229b
Sven A. Schmidt <sas@finestructure.co>
2020-08-31 14:48:37 +0200

    Use docker to pg_dump, don't expose db port publically

--- scripts/convert_to_db_dump.sh
+++ scripts/convert_to_db_dump.sh
@@ -4,10 +4,15 @@ set -eu
 
 TARFILE=$1
 DUMPFILE=$(basename $TARFILE).dump
+PG_IMAGE=postgres:12.1-alpine
 
 [[ -n $DATABASE_PASSWORD ]]  || (echo "DATABASE_PASSWORD not set" && exit 1)
 [[ -n $DB_BACKUP_DIR ]]      || (echo "DB_BACKUP_DIR not set" && exit 1)
 [[ -n $ENV ]]                || (echo "ENV not set" && exit 1)
+
+# Create docker network (ignoring "already exists") in order to
+# discover database at hostname "backup-db"
+docker network create backup || true
 
 echo "Unpacking ..."
 
@@ -15,14 +20,25 @@ tar xfz $TARFILE
 
 echo "Launching backup db ..."
 
-docker run --rm --name backup-db -d -v "$PWD/db_data:/var/lib/postgresql/data" -p 9432:5432 postgres:12.1-alpine
+docker run --rm --name backup-db -d \
+    -v "$PWD/db_data:/var/lib/postgresql/data" \
+    --network backup \
+    $PG_IMAGE
 
 echo "Exporting to $DUMPFILE ..."
 
 RETRIES=30
-until env PGPASSWORD=${DATABASE_PASSWORD} pg_dump --no-owner -Fc -h localhost -p 9432 -U spi_${ENV} spi_${ENV} > $DUMPFILE || [ $RETRIES -eq 0 ]; do
+until docker run --rm --name pg_dump \
+    -v "$PWD":/host \
+    --network backup \
+    --env PGPASSWORD=${DATABASE_PASSWORD} \
+    $PG_IMAGE \
+    pg_dump --no-owner -Fc -f /host/$DUMPFILE \
+        -h backup-db -U spi_${ENV} spi_${ENV} \
+    || [ $RETRIES -eq 0 ]; do
     echo "Waiting for postgres server, $((RETRIES-=1)) remaining attempts..."
     sleep 5
+    echo "Retrying ..."
 done
 
 echo "Moving file"
