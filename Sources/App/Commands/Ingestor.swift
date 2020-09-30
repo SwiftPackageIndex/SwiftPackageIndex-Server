a29c91c5f312cb1448ab9a958651c49ae17a92de
Sven A. Schmidt <sas@finestructure.co>
2020-09-30 11:13:38 +0200

    Improve error message for packages without repository metadata

--- Sources/App/Commands/Ingestor.swift
+++ Sources/App/Commands/Ingestor.swift
@@ -111,7 +111,7 @@ func updateRepositories(on database: Database, metadata: [Result<(Package, Githu
 /// - Returns: future
 func insertOrUpdateRepository(on database: Database, for package: Package, metadata: Github.Metadata) -> EventLoopFuture<Void> {
     guard let pkgId = try? package.requireID() else {
-        return database.eventLoop.makeFailedFuture(AppError.genericError(nil, "package id not found"))
+        return database.eventLoop.future(error: AppError.genericError(nil, "package id not found"))
     }
 
     return Repository.query(on: database)
@@ -119,17 +119,21 @@ func insertOrUpdateRepository(on database: Database, for package: Package, metad
         .first()
         .flatMap { repo -> EventLoopFuture<Void> in
             let repo = repo ?? Repository(packageId: pkgId)
-            repo.defaultBranch = metadata.repository.defaultBranch
-            repo.forks = metadata.repository.forkCount
-            repo.lastIssueClosedAt = metadata.repository.lastIssueClosedAt
-            repo.lastPullRequestClosedAt = metadata.repository.lastPullRequestClosedAt
-            repo.license = .init(from: metadata.repository.licenseInfo)
-            repo.name = metadata.repository.name
-            repo.openIssues = metadata.repository.openIssues.totalCount
-            repo.openPullRequests = metadata.repository.openPullRequests.totalCount
-            repo.owner = metadata.repository.owner.login
-            repo.stars = metadata.repository.stargazerCount
-            repo.summary = metadata.repository.description
+            guard let repository = metadata.repository else {
+                return database.eventLoop.future(
+                    error: AppError.genericError(pkgId, "repository is nil for package \(package.url)"))
+            }
+            repo.defaultBranch = repository.defaultBranch
+            repo.forks = repository.forkCount
+            repo.lastIssueClosedAt = repository.lastIssueClosedAt
+            repo.lastPullRequestClosedAt = repository.lastPullRequestClosedAt
+            repo.license = .init(from: repository.licenseInfo)
+            repo.name = repository.name
+            repo.openIssues = repository.openIssues.totalCount
+            repo.openPullRequests = repository.openPullRequests.totalCount
+            repo.owner = repository.owner.login
+            repo.stars = repository.stargazerCount
+            repo.summary = repository.description
             // TODO: find and assign parent repo
             return repo.save(on: database)
         }
