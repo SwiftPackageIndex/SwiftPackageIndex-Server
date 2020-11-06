edf8dc6e43379e346955027f80751837425cf65c
Sven A. Schmidt <sas@finestructure.co>
2020-11-06 16:22:45 +0100

    Fix latest/beta labels

--- Sources/App/Views/PackageController/SwiftVersion+BuildResultParameter.swift
+++ Sources/App/Views/PackageController/SwiftVersion+BuildResultParameter.swift
@@ -9,6 +9,6 @@ extension SwiftVersion: BuildResultParameter {
         return nil
     }
 
-    var isLatest: Bool { isCompatible(with: .v5_2) }
-    var isBeta: Bool { isCompatible(with: .v5_3) }
+    var isLatest: Bool { isCompatible(with: .v5_3) }
+    var isBeta: Bool { false }
 }
