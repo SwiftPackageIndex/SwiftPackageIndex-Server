0f2bb2409c9cac9ecfe9e74bc585bd73bce80d27
Sven A. Schmidt <sas@finestructure.co>
2020-11-17 10:41:00 +0100

    Clean up

--- Sources/App/Core/Twitter.swift
+++ Sources/App/Core/Twitter.swift
@@ -9,34 +9,6 @@ enum Twitter {
         case missingCredentials
     }
 
-    static func buildFirehosePost(package: Package) -> String? {
-        guard let repo = package.repository,
-              let version = Package.findRelease(package.versions),
-              let semVer = version.reference?.semVer,
-              let repoName = repo.name
-        else { return nil }
-        
-        // summary = '- repo summary goes here' or ''
-        let summary = repo.summary?.isEmpty != false ? "" : "- " + (repo.summary ?? "")
-        let url = SiteURL.package(.value(repo.owner ?? ""), .value(repoName), .none).absoluteURL()
-        
-        return "\(repoName) just released v\(semVer.description) \(summary)\n\n\(url)"
-    }
-    
-    static func postToFirehose(client: Client, package: Package) -> EventLoopFuture<Void> {
-        guard let message = buildFirehosePost(package: package) else {
-            return client.eventLoop.future(error: Error.invalidMessage)
-        }
-
-        return Current.twitterPostTweet(client, message)
-    }
-}
-
-
-// MARK:- POST request
-
-extension Twitter {
-
     private static let apiUrl: String = "https://api.twitter.com/1.1/statuses/update.json"
 
     struct Credentials {
@@ -44,9 +16,7 @@ extension Twitter {
         var user: (key: String, secret: String)
     }
 
-    // FIXME: add to AppEnvironment
-    static func post(client: Client,
-                     tweet: String) -> EventLoopFuture<Void> {
+    static func post(client: Client, tweet: String) -> EventLoopFuture<Void> {
         guard let credentials = Current.twitterCredentials() else {
             return client.eventLoop.future(error: Error.missingCredentials)
         }
@@ -64,6 +34,34 @@ extension Twitter {
         headers.add(name: "Content-Type", value: "application/x-www-form-urlencoded")
         return client.post(URI(string: url.absoluteString), headers: headers)
             .transform(to: ())
+    }
+
+}
+
+
+// MARK:- Helpers to post package to firehose
+
+extension Twitter {
+
+    static func buildFirehosePost(package: Package) -> String? {
+        guard let repo = package.repository,
+              let version = Package.findRelease(package.versions),
+              let semVer = version.reference?.semVer,
+              let repoName = repo.name
+        else { return nil }
+
+        // summary = '- repo summary goes here' or ''
+        let summary = repo.summary?.isEmpty != false ? "" : "- " + (repo.summary ?? "")
+        let url = SiteURL.package(.value(repo.owner ?? ""), .value(repoName), .none).absoluteURL()
+
+        return "\(repoName) just released v\(semVer.description) \(summary)\n\n\(url)"
+    }
+
+    static func postToFirehose(client: Client, package: Package) -> EventLoopFuture<Void> {
+        guard let message = buildFirehosePost(package: package) else {
+            return client.eventLoop.future(error: Error.invalidMessage)
+        }
+        return Current.twitterPostTweet(client, message)
     }
 
 }
