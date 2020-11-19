5ac9395bfbe224f48794b5b4f92d5841b15c48c4
Dave Verwer <dave.verwer@gmail.com>
2020-11-19 09:35:23 +0000

    Allow local port numbers to be customised when exporting database backups.

--- scripts/dump-prod-db.sh
+++ scripts/dump-prod-db.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # This command assumes the prod db to be available on port 7432 on localhost, via an ssh tunnel:
 # ssh -i <your private key> -L 7432:db:5432 -p 2222 root@173.255.229.82
-pg_dump --no-owner -Fc -h localhost -p 7432 -U spi_prod spi_prod > spi_prod_$(date +%Y-%m-%d).dump
+PORT=${SPI_PRODUCTION_DB_PORT:-'7432'}
+pg_dump --no-owner -Fc -h localhost -p $PORT -U spi_prod spi_prod > spi_prod_$(date +%Y-%m-%d).dump
