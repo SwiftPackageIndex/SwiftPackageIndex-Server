b8b5e6e27e54bc49713b217c8a6bb5d292904653
Sven A. Schmidt <sas@finestructure.co>
2020-12-15 14:13:33 +0100

    Add generate by owner

--- Sources/App/Core/PackageCollection+generate.swift
+++ Sources/App/Core/PackageCollection+generate.swift
@@ -30,4 +30,32 @@ extension PackageCollection {
                     createdBy: createdBy)
             }
     }
+
+    static func generate(db: Database,
+                         name: String,
+                         overview: String? = nil,
+                         keywords: [String]? = nil,
+                         owner: String,
+                         createdBy: Author? = nil) -> EventLoopFuture<PackageCollection> {
+        Repository.query(on: db)
+            .with(\.$package)
+            .filter(\.$owner == owner)
+            .all()
+            .mapEach { repository in
+                Package.init(url: repository.package.url,
+                             summary: repository.summary,
+                             keywords: nil,
+                             readmeURL: nil,
+                             versions: [])
+            }
+            .map { packages in
+                PackageCollection.init(
+                    name: name,
+                    overview: overview,
+                    keywords: keywords,
+                    packages: packages,
+                    createdAt: Date(),
+                    createdBy: createdBy)
+            }
+    }
 }
--- Tests/AppTests/PackageCollectionTests.swift
+++ Tests/AppTests/PackageCollectionTests.swift
@@ -4,7 +4,7 @@ import XCTest
 
 class PackageCollectionTests: AppTestCase {
 
-    func test_generate_basic() throws {
+    func test_generate_from_urls() throws {
         // setup
         let pkg = try savePackage(on: app.db, "1")
         try Repository(package: pkg, summary: "summary").create(on: app.db).wait()
@@ -16,10 +16,44 @@ class PackageCollectionTests: AppTestCase {
                                                  keywords: ["key", "word"],
                                                  packageURLs: ["1"],
                                                  createdBy: .init(name: "Foo", url: nil)).wait()
+
+        // validate
         XCTAssertEqual(res.name, "Foo")
         XCTAssertEqual(res.packages,[
                         .init(url: "1",
                               summary: "summary",
+                              keywords: nil,
+                              readmeURL: nil,
+                              versions: [])
+        ])
+    }
+
+    func test_generate_for_owner() throws {
+        // setup
+        let p1 = try savePackage(on: app.db, "https://github.com/foo/1")
+        let p2 = try savePackage(on: app.db, "https://github.com/foo/2")
+        _ = try savePackage(on: app.db, "https://github.com/bar/1")
+        try Repository(package: p1, summary: "summary 1", owner: "foo").create(on: app.db).wait()
+        try Repository(package: p2, summary: "summary 2", owner: "foo").create(on: app.db).wait()
+
+        // MUT
+        let res = try PackageCollection.generate(db: self.app.db,
+                                                 name: "Foo",
+                                                 overview: "overview",
+                                                 keywords: ["key", "word"],
+                                                 owner: "foo",
+                                                 createdBy: .init(name: "Foo", url: nil)).wait()
+
+        // validate
+        XCTAssertEqual(res.name, "Foo")
+        XCTAssertEqual(res.packages,[
+                        .init(url: "https://github.com/foo/1",
+                              summary: "summary 1",
+                              keywords: nil,
+                              readmeURL: nil,
+                              versions: []),
+                        .init(url: "https://github.com/foo/2",
+                              summary: "summary 2",
                               keywords: nil,
                               readmeURL: nil,
                               versions: [])
