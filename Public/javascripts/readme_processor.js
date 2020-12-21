05fc51cc9b7e76efa1eba6708780321427cb6284
Dave Verwer <dave.verwer@gmail.com>
2020-12-21 09:19:09 +0000

    This should have been a URL from the beginning, but it needs to be now.

--- Public/javascripts/readme_processor.js
+++ Public/javascripts/readme_processor.js
@@ -4,7 +4,7 @@ export class SPIReadmeProcessor {
       const readmeNode = document.querySelector('article.readme')
       if (!readmeNode) { return }
 
-      const readmeBaseUrl = readmeNode.getAttribute('data-readme-base-url')
+      const readmeBaseUrl = new URL(readmeNode.getAttribute('data-readme-base-url'))
       const readmeImages = readmeNode.querySelectorAll('img')
       readmeImages.forEach((image) => {
         const imageSource = image.getAttribute('src')
