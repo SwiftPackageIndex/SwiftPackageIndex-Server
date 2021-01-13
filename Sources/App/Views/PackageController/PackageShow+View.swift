1da0565a1f6f1569fd9b0a478bb428b27f712a5f
Dave Verwer <dave.verwer@gmail.com>
2021-01-13 10:32:33 +0000

    Removed <hr> elements in the sidebar.

--- Sources/App/Views/PackageController/PackageShow+View.swift
+++ Sources/App/Views/PackageController/PackageShow+View.swift
@@ -104,9 +104,7 @@ enum PackageShow {
         func sidebar() -> Node<HTML.BodyContext> {
             .group(
                 sidebarMetadata(),
-                .hr(),
                 sidebarMenu(),
-                .hr(),
                 sidebarReleases(),
                 sidebarGitHubSupport()
             )
