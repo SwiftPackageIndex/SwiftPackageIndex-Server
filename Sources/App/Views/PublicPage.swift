9e678fdc68914ab4d22ece67de165d0d2ee6dd2b
Dave Verwer <dave.verwer@gmail.com>
2021-01-14 11:20:15 +0000

    Switched to using the system fonts for every operating system.

--- Sources/App/Views/PublicPage.swift
+++ Sources/App/Views/PublicPage.swift
@@ -40,10 +40,6 @@ class PublicPage {
             ),
             .link(
                 .rel(.stylesheet),
-                .href("https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap")
-            ),
-            .link(
-                .rel(.stylesheet),
                 .href(SiteURL.stylesheets("main.min.css").relativeURL()
                         + "?\(resourceReloadQueryString())")
             ),
