f2ad66cacd5ab0e3a5bf9abbe99aaeb5bc32ead0
Sven A. Schmidt <sas@finestructure.co>
2021-01-21 12:11:05 +0100

    Move Date extension

--- Sources/App/Commands/VersionReAnalyzer.swift
+++ Sources/App/Commands/VersionReAnalyzer.swift
@@ -56,27 +56,6 @@ struct ReAnalyzeVersionsCommand: Command {
 }
 
 
-// Conformance required by @Option command line argument conversion
-extension Date: LosslessStringConvertible {
-    private static let ymd: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd"
-        formatter.calendar = Calendar(identifier: .iso8601)
-        formatter.timeZone = TimeZone(secondsFromGMT: 0)
-        return formatter
-    }()
-
-    public init?(yyyyMMdd: String) {
-        guard let date = Self.ymd.date(from: yyyyMMdd) else { return nil }
-        self = date
-    }
-
-    public init?(_ string: String) {
-        self.init(yyyyMMdd: string)
-    }
-}
-
-
 /// Re-analyze outdated versions for a given `Package`, identified by its `Id`.
 /// - Parameters:
 ///   - client: `Client` object
