594e1ac0da89331fed494486a5ed14067b1cb88b
Dave Verwer <dave.verwer@gmail.com>
2021-02-12 16:55:03 +0000

    Refactored all of the copy code to make it more generic. There’s more that could be done, but this is an improvement.

--- Public/javascripts/main.js
+++ Public/javascripts/main.js
@@ -5,9 +5,8 @@ import { ExternalLinkRetargeter } from './external_link_retargeter.js'
 import { SPISearchCore } from './search_core.js'
 import { SPISearchFocusHandler } from './search_focus_handler.js'
 import { SPISearchKeyboardNavigation } from './search_keyboard_navigation.js'
-import { SPICopyPackageURL } from './copy_package_url.js'
-import { SPICopySwiftVersionBadge } from './copy_package_url.js'
-import { SPICopyPlatformBadge } from './copy_package_url.js'
+import { SPICopyPackageURLButton } from './copy_buttons.js'
+import { SPICopyBadgeMarkdownButtons } from './copy_buttons.js'
 import { SPIBuildLogNavigation } from './build_log_navigation.js'
 import { SPIReadmeProcessor } from './readme_processor.js'
 
@@ -15,8 +14,7 @@ window.externalLinkRetargeter = new ExternalLinkRetargeter()
 window.spiSearchCore = new SPISearchCore()
 window.spiSearchFocusHandler = new SPISearchFocusHandler()
 window.spiSearchKeyboardNavigation = new SPISearchKeyboardNavigation()
-window.spiCopyPackageURL = new SPICopyPackageURL()
-window.spiCopySwiftVersionBadge = new SPICopySwiftVersionBadge()
-window.spiCopyPlatformBadge = new SPICopyPlatformBadge()
+window.spiCopyPackageURLButton = new SPICopyPackageURLButton()
+window.spiCopyBadgeMarkdownButtons = new SPICopyBadgeMarkdownButtons()
 window.buildLogNavigation = new SPIBuildLogNavigation()
 window.spiReadmeProcessor = new SPIReadmeProcessor()
