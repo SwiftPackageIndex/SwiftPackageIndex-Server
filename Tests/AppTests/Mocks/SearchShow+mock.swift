d6f6ba11af667fc47531fece136022a085ba6f76
Sven A. Schmidt <sas@finestructure.co>
2021-02-18 14:49:26 +0100

    Add snapshot

---
+++ Tests/AppTests/Mocks/SearchShow+mock.swift
@@ -0,0 +1,18 @@
+@testable import App
+
+import Foundation
+
+
+extension SearchShow.Model {
+    static var mock: Self {
+        let results = (0..<10).map { idx in
+            Search.Record.init(
+                packageId: .mockId(at: idx),
+                packageName: "Package \(idx)",
+                packageURL: "\(idx)".asGithubUrl,
+                repositoryName: "bar-\(idx)",
+                repositoryOwner: "foo",
+                summary: "Package number \(idx)") }
+        return .init(query: "search query", results: results)
+    }
+}
