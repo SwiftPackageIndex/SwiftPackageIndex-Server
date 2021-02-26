5e3dc6b7231823d72dbaa8d0027483c63fe5d087
Dave Verwer <dave.verwer@gmail.com>
2021-02-26 13:43:08 +0000

    Temporarily disable search and keyboard JavaScript.

--- Resources/Scripts/main.js
+++ Resources/Scripts/main.js
@@ -4,18 +4,18 @@ import './find_parent_matching.js'
 import './highlighting.js'
 
 import { ExternalLinkRetargeter } from './external_link_retargeter.js'
-import { SPISearchCore } from './search_core.js'
-import { SPISearchFocusHandler } from './search_focus_handler.js'
-import { SPISearchKeyboardNavigation } from './search_keyboard_navigation.js'
+// import { SPISearchCore } from './search_core.js'
+// import { SPISearchFocusHandler } from './search_focus_handler.js'
+// import { SPISearchKeyboardNavigation } from './search_keyboard_navigation.js'
 import { SPICopyPackageURLButton } from './copy_buttons.js'
 import { SPICopyBadgeMarkdownButtons } from './copy_buttons.js'
 import { SPIBuildLogNavigation } from './build_log_navigation.js'
 import { SPIReadmeProcessor } from './readme_processor.js'
 
 window.externalLinkRetargeter = new ExternalLinkRetargeter()
-window.spiSearchCore = new SPISearchCore()
-window.spiSearchFocusHandler = new SPISearchFocusHandler()
-window.spiSearchKeyboardNavigation = new SPISearchKeyboardNavigation()
+// window.spiSearchCore = new SPISearchCore()
+// window.spiSearchFocusHandler = new SPISearchFocusHandler()
+// window.spiSearchKeyboardNavigation = new SPISearchKeyboardNavigation()
 window.spiCopyPackageURLButton = new SPICopyPackageURLButton()
 window.spiCopyBadgeMarkdownButtons = new SPICopyBadgeMarkdownButtons()
 window.buildLogNavigation = new SPIBuildLogNavigation()
