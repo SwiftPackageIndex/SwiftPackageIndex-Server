2265e6abbf84e85bfa598912ec38d50f80590fd0
Dave Verwer <dave.verwer@gmail.com>
2021-02-26 18:46:01 +0000

    Added a new Components extension file for HTML snippets shared across pages.

---
+++ Sources/App/Views/Plot+Components.swift
@@ -0,0 +1,25 @@
+import Plot
+
+extension Node where Context == HTML.BodyContext {
+    static func searchForm(query: String = "") -> Self {
+        .form(
+            .action(SiteURL.search.relativeURL()),
+            .searchField(query: query),
+            .submit(text: "Search")
+        )
+    }
+}
+
+extension Node where Context == HTML.FormContext {
+    static func searchField(query: String = "") -> Self {
+        .input(
+            .id("query"),
+            .name("query"),
+            .type(.search),
+            .attribute(named: "placeholder", value: "Search"), // TODO: Fix after Plot update
+            .attribute(named: "spellcheck", value: "false"), // TODO: Fix after Plot update
+            .attribute(named: "data-gramm", value: "false"),
+            .value(query)
+        )
+    }
+}
