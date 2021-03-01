e33b48be74618d85f53900b0d9555969c7a21c1a
Dave Verwer <dave.verwer@gmail.com>
2021-03-01 10:48:49 +0000

    Stop the query string being sorted.

--- Sources/App/Core/Extensions/Dictionary+queryString.swift
+++ Sources/App/Core/Extensions/Dictionary+queryString.swift
@@ -2,13 +2,12 @@
 extension Dictionary where Key == String, Value == String {
     func queryString(includeSeparator: Bool = true) -> String {
         guard !isEmpty else { return "" }
-        let query: String = keys.sorted()
-            .map { key in
-                let encodedKey = key.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? key
-                let value = self[key]!
-                let encodedValue = value.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? value
-                return "\(encodedKey)=\(encodedValue)"
-            }.joined(separator: "&")
+        let query: String = keys.map { key in
+            let encodedKey = key.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? key
+            let value = self[key]!
+            let encodedValue = value.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? value
+            return "\(encodedKey)=\(encodedValue)"
+        }.joined(separator: "&")
         return includeSeparator ? "?" + query : query
     }
 }
