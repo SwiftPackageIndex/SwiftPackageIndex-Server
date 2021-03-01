413139dc151e3d9e908c1b7f46db63f8c1db57dc
Dave Verwer <dave.verwer@gmail.com>
2021-03-01 16:45:37 +0000

    Removed the as-you-type search JavaScript.

--- Resources/Scripts/main.js
+++ Resources/Scripts/main.js
@@ -4,8 +4,6 @@ import './find_parent_matching.js'
 import './highlighting.js'
 
 import { ExternalLinkRetargeter } from './external_link_retargeter.js'
-// import { SPISearchCore } from './search_core.js'
-// import { SPISearchFocusHandler } from './search_focus_handler.js'
 // import { SPISearchKeyboardNavigation } from './search_keyboard_navigation.js'
 import { SPICopyPackageURLButton } from './copy_buttons.js'
 import { SPICopyBadgeMarkdownButtons } from './copy_buttons.js'
@@ -13,8 +11,6 @@ import { SPIBuildLogNavigation } from './build_log_navigation.js'
 import { SPIReadmeProcessor } from './readme_processor.js'
 
 window.externalLinkRetargeter = new ExternalLinkRetargeter()
-// window.spiSearchCore = new SPISearchCore()
-// window.spiSearchFocusHandler = new SPISearchFocusHandler()
 // window.spiSearchKeyboardNavigation = new SPISearchKeyboardNavigation()
 window.spiCopyPackageURLButton = new SPICopyPackageURLButton()
 window.spiCopyBadgeMarkdownButtons = new SPICopyBadgeMarkdownButtons()
