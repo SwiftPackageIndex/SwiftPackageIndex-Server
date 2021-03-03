c588e4ab4c5739ea1d9413e6ab5b3fc41e0e1eae
Dave Verwer <dave.verwer@gmail.com>
2021-03-03 19:01:30 +0000

    Submit forms that use GET requests using Turbolinks.

--- Resources/Scripts/turbolink_forms.js
+++ Resources/Scripts/turbolink_forms.js
@@ -1,5 +1,21 @@
+import Turbolinks from 'turbolinks'
+
 export class SPITurbolinkForms {
   constructor() {
-    document.addEventListener('DOMContentLoaded', () => {})
+    // Bind to ALL submit events, regardless of where they come from.
+    document.addEventListener('submit', (event) => {
+      // Submit forms that use GET requests using Turbolinks.
+      const formElement = event.target
+      if (formElement.matches('form') && formElement.method === 'get') {
+        const formData = new FormData(formElement)
+        const params = new URLSearchParams(formData)
+        const url = new URL(formElement.action)
+        url.search = params.toString()
+        Turbolinks.visit(url.toString())
+
+        // Stop the form submission.
+        event.preventDefault()
+      }
+    })
   }
 }
