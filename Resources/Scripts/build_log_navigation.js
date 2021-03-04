8f6d6bf88433bd70b1d2a6411eb7422d534b3350
Dave Verwer <dave.verwer@gmail.com>
2021-03-04 14:23:35 +0000

    Slight cleanup.

--- Resources/Scripts/build_log_navigation.js
+++ Resources/Scripts/build_log_navigation.js
@@ -2,9 +2,7 @@ export class SPIBuildLogNavigation {
   constructor() {
     document.addEventListener('turbolinks:load', () => {
       const buildLogElement = document.getElementById('build_log')
-      if (!buildLogElement) {
-        return
-      }
+      if (!buildLogElement) return
 
       // Scroll to the bottom of the log.
       buildLogElement.scrollTop = buildLogElement.scrollHeight
