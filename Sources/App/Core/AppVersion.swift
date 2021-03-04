7908879a902ec7f4119bc3c8f7be5a1bcdfc88e1
Dave Verwer <dave.verwer@gmail.com>
2021-03-04 13:10:36 +0000

    Fixed up the generation of AppVersion.swift and added a comment so future generations won’t make the same mistake!

--- Sources/App/Core/AppVersion.swift
+++ Sources/App/Core/AppVersion.swift
@@ -1,4 +1,8 @@
 // In a local development environment, appVersion will remain nil (as set here).
 // In the staging development environment, appVersion is set to the commit hash of the deployed version.
 // In production, appVersion is set either to released tag name, or to the commit hash if that does not exist.
+
+// Note: If the definition of appVersion ever changes, the `gitlab-ci.yml` file also
+// needs updating as this file is re-generated during the deployment process.
+
 let appVersion: String? = nil
