4a303b3378fcb1ebe9bf380a18ff0d645e0537d7
Sven A. Schmidt <sas@finestructure.co>
2021-03-29 09:47:41 +0200

    Set component for server, for monitoring visibility

--- Sources/App/Core/Extensions/Logger+ext.swift
+++ Sources/App/Core/Extensions/Logger+ext.swift
@@ -4,6 +4,14 @@ import Logging
 extension Logger {
     init(component: String) {
         self.init(label: component)
-        self[metadataKey: "component"] = .string(component)
+        self.component = component
+    }
+
+    var component: String? {
+        get { self[metadataKey: "component"].map { "\($0)" } }
+        set {
+            guard let newValue = newValue else { return }
+            self[metadataKey: "component"] = .string(newValue)
+        }
     }
 }
