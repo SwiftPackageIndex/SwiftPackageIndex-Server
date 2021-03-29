8adfe8d718cf28bee4278509e992c486ffa53e22
Dave Verwer <dave.verwer@gmail.com>
2021-03-29 11:16:26 +0100

    Updated database dump scripts for the new Azure database machines.

--- scripts/dump-prod-db.sh
+++ scripts/dump-prod-db.sh
@@ -7,5 +7,5 @@ then
   # This command assumes the prod db to be available on port 7432 on localhost, via an ssh tunnel:
   # ssh -i <your private key> -L 7432:db:5432 -p 2222 root@173.255.229.82
   PORT=${SPI_PRODUCTION_DB_PORT:-'7432'}
-  pg_dump --no-owner -Fc -h localhost -p $PORT -U spi_prod spi_prod > spi_prod_$(date +%Y-%m-%d).dump
+  pg_dump --no-owner -Fc -h localhost -p $PORT -U spi_prod@spi-prod-db-1 spi_prod > spi_prod_$(date +%Y-%m-%d).dump
 fi
