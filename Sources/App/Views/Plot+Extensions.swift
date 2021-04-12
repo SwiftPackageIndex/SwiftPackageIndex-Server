a3282d80df2b242ca3fe3d498dde506639e91bf6
Dave Verwer <dave.verwer@gmail.com>
2021-04-12 19:02:29 +0100

    Added a <turbo-frame> Plot extension.

--- Sources/App/Views/Plot+Extensions.swift
+++ Sources/App/Views/Plot+Extensions.swift
@@ -8,6 +8,10 @@ extension Node where Context == HTML.BodyContext {
     static func hr(_ attributes: Attribute<HTML.BodyContext>...) -> Node {
         .selfClosedElement(named: "hr", attributes: attributes)
     }
+
+    static func turboFrame(_ nodes: Node<HTML.BodyContext>...) -> Self {
+        .element(named: "turbo-frame", nodes: nodes)
+    }
 }
 
 extension Node where Context == HTML.AnchorContext {
