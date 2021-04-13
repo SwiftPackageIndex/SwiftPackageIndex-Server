0c0ae27213d7ed5bf80abe79027837590eb6319f
Dave Verwer <dave.verwer@gmail.com>
2021-04-13 08:21:58 +0100

    Made Node elements renderable as responses as well as HTML elements.

--- Sources/App/Core/Extensions/HTML+ResponseEncodable.swift
+++ Sources/App/Core/Extensions/HTML+ResponseEncodable.swift
@@ -1,15 +1,24 @@
 import Plot
 import Vapor
 
+protocol Renderable {
+    func render() -> String
+}
 
-extension HTML: ResponseEncodable {
+extension Renderable {
     public func encodeResponse(for request: Request) -> EventLoopFuture<Response> {
         encodeResponse(for: request, status: .ok)
     }
-    
+
     public func encodeResponse(for request: Request, status: HTTPResponseStatus) -> EventLoopFuture<Response> {
         let res = Response(status: status, body: .init(string: self.render()))
         res.headers.add(name: "Content-Type", value: "text/html; charset=utf-8")
         return request.eventLoop.makeSucceededFuture(res)
     }
+}
+
+extension HTML: Renderable, ResponseEncodable  {
+}
+
+extension Node: Renderable, ResponseEncodable {
 }
