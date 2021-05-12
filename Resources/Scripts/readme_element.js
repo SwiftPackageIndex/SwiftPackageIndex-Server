d465ebed2859b56be26c09afb1692e5f7cd3932d
Dave Verwer <dave.verwer@gmail.com>
2021-05-12 19:49:49 +0100

    Fix up anchor elements *without* the Turbo fix.

--- Resources/Scripts/readme_element.js
+++ Resources/Scripts/readme_element.js
@@ -18,16 +18,29 @@ export class SPIReadmeElement extends HTMLElement {
       }
     })
 
-    // Find all relative links and point them to the formatted version on GitHub.
+    // Fix up relative and anchor URLs.
     const links = this.querySelectorAll('a')
     links.forEach((linkElement) => {
+      // Remove turbo from *all* links inside the readme.
+      linkElement.setAttribute('data-turbo', 'false')
+
       const linkTarget = linkElement.getAttribute('href')
-      try {
-        // Relative URLs will *fail* this initialisation.
-        new URL(linkTarget)
-      } catch (error) {
-        console.log(`linkTarget=${linkTarget}`)
-        linkElement.href = `https://github.com${linkTarget}`
+      if (linkTarget.charAt(0) === '#') {
+        // Fix up anchor URLs by faking the navigation.
+        linkElement.addEventListener('click', (event) => {
+          const targetAnchor = `user-content-${linkTarget.substring(1)}`
+          const destinationElement = document.getElementById(targetAnchor)
+          destinationElement.scrollIntoView()
+          event.preventDefault()
+        })
+      } else {
+        // Fix up relative URLs to go to GitHub.
+        try {
+          // Relative URLs will *fail* this initialisation.
+          new URL(linkTarget)
+        } catch (error) {
+          linkElement.href = `https://github.com${linkTarget}`
+        }
       }
     })
   }
