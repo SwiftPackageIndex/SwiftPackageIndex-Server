0c6b7fa402176eae0e680d44b87809fd08d584df
Dave Verwer <dave.verwer@gmail.com>
2021-05-12 16:01:24 +0100

    Started to create the spi-readme class.

--- Resources/Scripts/main.js
+++ Resources/Scripts/main.js
@@ -7,18 +7,19 @@ import { SPIPackageListNavigation } from './package_list_navigation.js'
 import { SPICopyPackageURLButton } from './copy_buttons.js'
 import { SPICopyBadgeMarkdownButtons } from './copy_buttons.js'
 import { SPIBuildLogNavigation } from './build_log_navigation.js'
-import { SPIReadmeProcessor } from './readme_processor.js'
 import { SPIAutofocus } from './autofocus.js'
 import { SPIPlaygroundsAppLinkFallback } from './playgrounds_app_link.js'
+import { SPIReadmeElement } from './readme_element.js'
 
 window.externalLinkRetargeter = new ExternalLinkRetargeter()
 window.spiPackageListNavigation = new SPIPackageListNavigation()
 window.spiCopyPackageURLButton = new SPICopyPackageURLButton()
 window.spiCopyBadgeMarkdownButtons = new SPICopyBadgeMarkdownButtons()
 window.buildLogNavigation = new SPIBuildLogNavigation()
-window.spiReadmeProcessor = new SPIReadmeProcessor()
 new SPIAutofocus()
 new SPIPlaygroundsAppLinkFallback()
+
+customElements.define('spi-readme', SPIReadmeElement, { extends: 'article' })
 
 import 'normalize.css'
 import '../Styles/main.scss'
