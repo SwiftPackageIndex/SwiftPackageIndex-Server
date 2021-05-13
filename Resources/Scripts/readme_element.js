15287c938af3f4dde6a434e51d9096af63d4153d
Dave Verwer <dave.verwer@gmail.com>
2021-05-13 14:14:36 +0100

    Removed client side code for correcting links and images.

--- Resources/Scripts/readme_element.js
+++ Resources/Scripts/readme_element.js
@@ -2,26 +2,9 @@ export class SPIReadmeElement extends HTMLElement {
   constructor() {
     super()
 
-    // -----------------------------------------------------------------------------------
-    // Note: This is very brittle code that relies on everything being hosted with GitHub.
-    // -----------------------------------------------------------------------------------
-
-    // Find all relative image URLs and point them at the raw image sources.
-    const readmeImages = this.querySelectorAll('img')
-    readmeImages.forEach((imageElement) => {
-      const imageSource = imageElement.getAttribute('src')
-      try {
-        // Relative URLs will *fail* this initialisation.
-        new URL(imageSource)
-      } catch (error) {
-        imageElement.src = `https://github.com${imageSource}`
-      }
-    })
-
-    // Fix up relative and anchor URLs.
-    const links = this.querySelectorAll('a')
-    links.forEach((linkElement) => {
-      // Remove turbo from *all* links inside the readme.
+    const linkElements = this.querySelectorAll('a')
+    linkElements.forEach((linkElement) => {
+      // Remove turbo from *all* links inside the README.
       linkElement.setAttribute('data-turbo', 'false')
 
       const linkTarget = linkElement.getAttribute('href')
@@ -33,14 +16,6 @@ export class SPIReadmeElement extends HTMLElement {
           if (destinationElement) destinationElement.scrollIntoView()
           event.preventDefault()
         })
-      } else {
-        // Fix up relative URLs to go to GitHub.
-        try {
-          // Relative URLs will *fail* this initialisation.
-          new URL(linkTarget)
-        } catch (error) {
-          linkElement.href = `https://github.com${linkTarget}`
-        }
       }
     })
   }
