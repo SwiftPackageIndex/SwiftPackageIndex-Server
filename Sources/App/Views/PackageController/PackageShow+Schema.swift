6a0fc8a130b6fd802cdd8e1cab06f88433373e54
James Sherlock <15193942+Sherlouk@users.noreply.github.com>
2021-05-20 19:53:01 +0100

    Add more information to package schema

--- Sources/App/Views/PackageController/PackageShow+Schema.swift
+++ Sources/App/Views/PackageController/PackageShow+Schema.swift
@@ -4,18 +4,27 @@ extension PackageShow {
     
     struct PackageSchema: Schema {
         enum CodingKeys: String, CodingKey {
-            case context = "@context"
-            case type = "@type"
-            case codeRepository
+            case context = "@context", type = "@type"
+            case identifier, name, description, license, version, codeRepository
         }
         
         var context: String = "https://schema.org"
         var type: String = "SoftwareSourceCode"
         
+        let identifier: String
+        let name: String
+        let description: String?
+        let license: String?
+        let version: String?
         let codeRepository: String
         
         init(model: Model) {
-            codeRepository = model.url
+            identifier = "\(model.repositoryOwner)/\(model.repositoryName)"
+            name = model.repositoryName
+            description = model.summary
+            license = model.licenseUrl
+            version = model.releases.stable?.link.label
+            codeRepository = model.url.droppingGitExtension
         }
     }
     
