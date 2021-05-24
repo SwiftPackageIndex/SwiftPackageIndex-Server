3226a7c2caf576dc907aaee5f6d0b7de7ee10c89
Dave Verwer <dave.verwer@gmail.com>
2021-05-24 12:36:48 +0100

    Allow optional additional parameters to be sent to plausible.

--- Resources/Scripts/plausible_analytics.js
+++ Resources/Scripts/plausible_analytics.js
@@ -1,6 +1,6 @@
-export function measurePlausibleEvent(event) {
+export function measurePlausibleEvent(event, options = {}) {
   if (typeof plausible === 'function') {
     // eslint-disable-next-line no-undef
-    plausible(event)
+    plausible(event, options)
   }
 }
