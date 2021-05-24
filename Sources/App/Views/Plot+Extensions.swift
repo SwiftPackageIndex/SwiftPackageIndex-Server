8f4e914fa522431370284b948f8ce2d0e7ac696a
Dave Verwer <dave.verwer@gmail.com>
2021-05-24 15:33:22 +0100

    Small extension is no longer needed!

--- Sources/App/Views/Plot+Extensions.swift
+++ Sources/App/Views/Plot+Extensions.swift
@@ -1,10 +1,6 @@
 import Plot
 
 extension Node where Context: HTML.BodyContext {
-    static func small(_ nodes: Node<HTML.BodyContext>...) -> Self {
-        .element(named: "small", nodes: nodes)
-    }
-
     static func hr(_ attributes: Attribute<HTML.BodyContext>...) -> Node {
         .selfClosedElement(named: "hr", attributes: attributes)
     }
