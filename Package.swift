cb6e707ebdb1c625326b27339bbff4bded193c0c
Sven A. Schmidt <sas@finestructure.co>
2021-06-11 15:26:15 +0200

    Revert "Add swift-tools-support-core"
    This reverts commit 622e9e1248aa992c916175670e2c554fe63c1819.
    
    We don't want to include this dependency, because it requires sqlite as a build dependency.

--- Package.swift
+++ Package.swift
@@ -22,10 +22,7 @@ let package = Package(
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.3.2"),
         .package(name: "SwiftPM",
                  url: "https://github.com/apple/swift-package-manager.git",
-                 .revision("swift-DEVELOPMENT-SNAPSHOT-2021-05-04-a")),
-        .package(name: "swift-tools-support-core",
-                 url: "https://github.com/apple/swift-tools-support-core.git", from:"0.2.2")
-
+                 .revision("swift-DEVELOPMENT-SNAPSHOT-2021-05-04-a"))
     ],
     targets: [
         .target(name: "App", dependencies: [
@@ -39,8 +36,7 @@ let package = Package(
             "SwiftPrometheus",
             "OhhAuth",
             "SwiftSoup",
-            .product(name: "PackageCollectionsModel", package: "SwiftPM"),
-            .product(name: "SwiftToolsSupport", package: "swift-tools-support-core")
+            .product(name: "PackageCollectionsModel", package: "SwiftPM")
         ]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: [
