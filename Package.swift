725a90913003aa4f67504d212c86ef5b04830dc9
Dave Verwer <dave.verwer@gmail.com>
2021-06-21 16:02:18 +0100

    Plot package update to version with upstreamed changes.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-rc"),
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0-rc"),
         .package(url: "https://github.com/JohnSundell/Ink.git", from: "0.5.1"),
-        .package(url: "https://github.com/JohnSundell/Plot.git", from: "0.7.0"),
+        .package(url: "https://github.com/JohnSundell/Plot.git", from: "0.10.0"),
         .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0"),
         .package(url: "https://github.com/MrLotU/SwiftPrometheus.git", from: "1.0.0-alpha"),
         .package(name: "SnapshotTesting",
