300487dcaaaebb810a82200b670c2aa7169721e7
Sven A. Schmidt <sas@finestructure.co>
2021-06-27 16:30:00 +0200

    Add KeywordShow+[Model|View]

--- Tests/AppTests/Mocks/AuthorShow+mock.swift
+++ Tests/AppTests/Mocks/AuthorShow+mock.swift
@@ -5,7 +5,7 @@ import Foundation
 
 extension AuthorShow.Model {
     static var mock: AuthorShow.Model {
-        let packages = (1...10).map { AuthorShow.PackageInfo(
+        let packages = (1...10).map { PackageInfo(
             title: "vapor-\($0)",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec orci scelerisque, interdum purus a, tempus turpis.",
             url: ""
