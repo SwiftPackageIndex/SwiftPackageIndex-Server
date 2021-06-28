2efa930b5b11515c82befb58e202d5f837980ffc
Sven A. Schmidt <sas@finestructure.co>
2021-06-28 07:52:33 +0200

    Add KeywordShow snapshot

---
+++ Tests/AppTests/Mocks/KeywordShow+mock.swift
@@ -0,0 +1,15 @@
+@testable import App
+
+import Foundation
+
+
+extension KeywordShow.Model {
+    static var mock: Self {
+        let packages = (1...10).map { PackageInfo(
+            title: "Networking Package \($0)",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec orci scelerisque, interdum purus a, tempus turpis.",
+            url: ""
+        ) }
+        return .init(keyword: "networking", packages: packages)
+    }
+}
