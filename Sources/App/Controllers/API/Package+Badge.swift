4f2f1e6cf3ae55c3b2634acd7c8be24d6a762c85
Sven A. Schmidt <sas@finestructure.co>
2021-07-06 07:36:33 +0200

    Fix Xcode 13 warnings

--- Sources/App/Controllers/API/Package+Badge.swift
+++ Sources/App/Controllers/API/Package+Badge.swift
@@ -21,7 +21,7 @@ extension Package {
     /// - Returns: A `CompatibilityResult` of `SwiftVersion`
     func swiftVersionCompatibility() -> CompatibilityResult<SwiftVersion> {
         let allBuilds = allSignificantBuilds()
-        if allBuilds.allSatisfy { $0.status == .pending } { return .pending }
+        if allBuilds.allSatisfy({ $0.status == .pending }) { return .pending }
         
         let builds = allBuilds
             .filter { $0.status == .ok }
@@ -45,7 +45,7 @@ extension Package {
     /// - Returns: A `CompatibilityResult` of `Platform`
     func platformCompatibility() -> CompatibilityResult<Build.Platform> {
         let allBuilds = allSignificantBuilds()
-        if allBuilds.allSatisfy { $0.status == .pending } { return .pending }
+        if allBuilds.allSatisfy({ $0.status == .pending }) { return .pending }
         
         let builds = allBuilds
             .filter { $0.status == .ok }
