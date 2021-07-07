2ae8bf6bf76768d8be41a6cddbb1a2f7a813852b
Sven A. Schmidt <sas@finestructure.co>
2021-07-07 08:50:16 +0200

    Keep count on single results page

--- Sources/App/Views/Keyword/KeywordShow+View.swift
+++ Sources/App/Views/Keyword/KeywordShow+View.swift
@@ -41,9 +41,11 @@ enum KeywordShow {
                         }
                     )
                 ),
-//                .p(
-//                    .strong("\(model.packages.count) \("package".pluralized(for: model.packages.count)).")
-//                ),
+                .if(model.page == 1 && !model.hasMoreResults,
+                    .p(
+                        .strong("\(model.packages.count) \("package".pluralized(for: model.packages.count)).")
+                    )
+                ),
                 .ul(
                     .class("pagination"),
                     .if(model.page > 1, .previousPage(model: model)),
