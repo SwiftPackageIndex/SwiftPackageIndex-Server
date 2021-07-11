71e3b90a1fe7b649769da4c3771d7f1e14012d03
James Sherlock <15193942+Sherlouk@users.noreply.github.com>
2021-07-11 17:12:46 +0100

    Add some documentation

--- Sources/App/Core/SearchFilter.swift
+++ Sources/App/Core/SearchFilter.swift
@@ -3,21 +3,37 @@ import SQLKit
 import Vapor
 
 protocol SearchFilter {
-    // <Key>:<Op?><Value>
+    /// The key or selector used to formulate the first part of the search syntax.
+    ///
+    /// `<key>:<operator?><value>`
     static var key: String { get }
     
+    /// Create an instance of a search filter, using a given string value and comparison operator.
+    ///
+    /// An error should be thrown if the value cannot be converted to the appropriate type, or if the comparison method is not supported for that filter.
     init(value: String, comparison: SearchFilterComparison) throws
+    
+    /// Add a SQLKit `where` clause to the "SELECT" expression, using the filter's stored value and provided comparison method for context.
     func query(_ builder: SQLPredicateGroupBuilder) -> SQLPredicateGroupBuilder
 }
 
 struct SearchFilterParser {
     
+    /// A list of all currently supported search filters.
     static var allSearchFilters: [SearchFilter.Type] = [
         StarsSearchFilter.self,
         LicenseSearchFilter.self,
         UpdatedSearchFilter.self,
     ]
     
+    
+    /// Separates search terms from filter syntax.
+    ///
+    /// A "filter syntax" is a part of the user input which is a set of instructions to the search controller to filter the results by. "Search terms" is anything which is not
+    /// a valid filter syntax.
+    ///
+    /// In this example: `["test", "stars:>500"]` - `"test"` is a search term, and `"stars:>500"` is filter syntax (instructing the search controller to
+    /// only return results with more than 500 stars.)
     func separate(terms: [String]) -> (terms: [String], filters: [SearchFilter]) {
         terms.reduce(into: (terms: [], filters: [])) { builder, term in
             if let filter = parse(term: term) {
@@ -28,6 +44,8 @@ struct SearchFilterParser {
         }
     }
     
+    /// Attempts to identify the appropriate `SearchFilter` for the provided term. If it does not match our filter syntax, then this will return `nil` and it should
+    /// be treated as a search term.
     func parse(term: String, allFilters: [SearchFilter.Type] = SearchFilterParser.allSearchFilters) -> SearchFilter? {
         let components = term
             .components(separatedBy: ":")
@@ -88,6 +106,15 @@ enum SearchFilterError: Error {
 
 // MARK: Stars
 
+/// Filters by the number of stars the package has.
+///
+/// Examples:
+/// ```
+/// stars:5  - Exactly 5 stars
+/// stars:>5 - Any number of stars more than 5
+/// stars:<5 - Any number of stars less than 5
+/// stars:!5 - Any number of stars except 5
+/// ```
 struct StarsSearchFilter: SearchFilter {
     static var key: String = "stars"
     
@@ -114,6 +141,13 @@ struct StarsSearchFilter: SearchFilter {
 
 // MARK: License
 
+/// Filters by the license of the package.
+///
+/// Examples:
+/// ```
+/// license:compatible   - The license is compatible with the app store
+/// license:incompatible - The license is unknown, none is provided, or the one provided is not compatible with the app store
+/// ```
 struct LicenseSearchFilter: SearchFilter {
     enum FilterType: String, Equatable {
         case compatible
@@ -158,6 +192,17 @@ struct LicenseSearchFilter: SearchFilter {
 
 // MARK: Last Commit Date
 
+/// Filters by the date in which the package's main branch was last updated.
+///
+/// Dates must be provided in the `YYYY-MM-DD` format.
+///
+/// Examples:
+/// ```
+/// updated:2020-07-01  - Updated on exactly July 1st 2020
+/// updated:!2020-07-01 - Updated on any day other than July 1st 2020
+/// updated:>2020-07-01 - Updated on any day more recent than July 1st 2020
+/// updated:<2020-07-01 - Updated on any day older than July 1st 2020
+/// ```
 struct UpdatedSearchFilter: SearchFilter {
     static var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
