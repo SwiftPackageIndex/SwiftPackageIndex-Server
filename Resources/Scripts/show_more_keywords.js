e90523c67e5b5b59d7f8de6b943fa3dc68ab1631
Dave Verwer <dave.verwer@gmail.com>
2021-09-22 23:05:28 +0100

    Dynamically hide tags over two lines and add a button to re-show them.

--- Resources/Scripts/show_more_keywords.js
+++ Resources/Scripts/show_more_keywords.js
@@ -14,6 +14,47 @@
 
 export class SPIShowMoreKeywords {
   constructor() {
-    super()
+    document.addEventListener('turbo:load', () => {
+      // Immediately collapse the overflowing keyword list.
+      const keywordsListElement = document.querySelector('article.details ul.keywords')
+      keywordsListElement.classList.add('collapsed')
+
+      // If the collapsing hid any content, add a "show more" that expands it.
+      if (this.isOverflowing(keywordsListElement)) {
+        const totalKeywords = keywordsListElement.children.length
+
+        // Either
+        const showMoreElement = document.createElement('a')
+        showMoreElement.innerText = `Show all ${totalKeywords} tags`
+        showMoreElement.href = '#'
+
+        // Or
+        // const showMoreElement = document.createElement('button')
+        // showMoreElement.innerText = `Show all ${totalKeywords} tags`
+
+        showMoreElement.addEventListener('click', (event) => {
+          keywordsListElement.classList.remove('collapsed')
+          showMoreElement.remove()
+          event.preventDefault()
+        })
+
+        // Put the link in a container so it may be justified. (Remove if unneeded before merging)
+        const buttonContainerElement = document.createElement('div')
+        buttonContainerElement.classList.add('show_more')
+        buttonContainerElement.appendChild(showMoreElement)
+
+        const keywordsListParentElement = keywordsListElement.parentElement
+        keywordsListParentElement.appendChild(buttonContainerElement)
+      }
+    })
+  }
+
+  // Adapted from https://stackoverflow.com/a/143889
+  isOverflowing(element) {
+    var currentOverflow = element.style.overflow
+    if (!currentOverflow || currentOverflow === 'visible') element.style.overflow = 'hidden'
+    var isOverflowing = element.clientWidth < element.scrollWidth || element.clientHeight < element.scrollHeight
+    element.style.overflow = currentOverflow
+    return isOverflowing
   }
 }
