227d539be193a519da71bdadbdda34db16f21e5d
Dave Verwer <dave.verwer@gmail.com>
2021-09-23 11:47:08 +0100

    Added two new snapshot tests for a small and large number of keywords.

--- Tests/AppTests/WebpageSnapshotTests.swift
+++ Tests/AppTests/WebpageSnapshotTests.swift
@@ -70,7 +70,50 @@ class WebpageSnapshotTests: WebpageSnapshotTestCase {
         }
         #endif
     }
-    
+
+    func test_PackageShowView_few_keywords() throws {
+        var model = PackageShow.Model.mock
+        model.keywords = ["tag1", "tag2", "tag3", "tag4", "tag5", "tag6", "tag7", "tag8", "tag9", "tag10"]
+        let page = { PackageShow.View(path: "", model: model, packageSchema: .mock).document() }
+
+        assertSnapshot(matching: page, as: .html)
+
+        #if os(macOS)
+        if !isRunningInCI {
+            configs.forEach {
+                assertSnapshot(matching: page,
+                               as: .image(precision: defaultPrecision,
+                                          size: $0.size,
+                                          baseURL: TempWebRoot.baseURL),
+                               named: $0.name)
+            }
+        }
+        #endif
+    }
+
+    func test_PackageShowView_many_keywords() throws {
+        var model = PackageShow.Model.mock
+        model.keywords = ["tag1", "tag2", "tag3", "tag4", "tag5", "tag6", "tag7", "tag8", "tag9", "tag10",
+                          "tag11", "tag12", "tag13", "tag14", "tag15", "tag16", "tag17", "tag18", "tag19", "tag20",
+                          "tag21", "tag22", "tag23", "tag24", "tag25", "tag26", "tag27", "tag28", "tag29", "tag30",
+                          "tag31", "tag32", "tag33", "tag34", "tag35", "tag36", "tag37", "tag38", "tag39", "tag40"]
+        let page = { PackageShow.View(path: "", model: model, packageSchema: .mock).document() }
+
+        assertSnapshot(matching: page, as: .html)
+
+        #if os(macOS)
+        if !isRunningInCI {
+            configs.forEach {
+                assertSnapshot(matching: page,
+                               as: .image(precision: defaultPrecision,
+                                          size: $0.size,
+                                          baseURL: TempWebRoot.baseURL),
+                               named: $0.name)
+            }
+        }
+        #endif
+    }
+
     func test_PackageShowView_emoji_summary() throws {
         var model = PackageShow.Model.mock
         model.summary = ":package: Nothing but Cache. :octocat:"
