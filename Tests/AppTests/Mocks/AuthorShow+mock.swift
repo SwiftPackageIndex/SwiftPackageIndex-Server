f07b43aa9fd1b5e92448e43034142a2817482c69
Dave Verwer <dave.verwer@gmail.com>
2021-10-19 14:04:23 +0100

    Added `repositoryOwner ` and `repositoryName ` to `PackageInfo`.

--- Tests/AppTests/Mocks/AuthorShow+mock.swift
+++ Tests/AppTests/Mocks/AuthorShow+mock.swift
@@ -22,7 +22,9 @@ extension AuthorShow.Model {
         let packages = (1...10).map { PackageInfo(
             title: "vapor-\($0)",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec orci scelerisque, interdum purus a, tempus turpis.",
-            url: "",
+            repositoryOwner: "owner",
+            repositoryName: "name",
+            url: "https://example.com/owner/name.git",
             stars: 3
         ) }
         return .init(owner: "test-author", ownerName: "Test Author", packages: packages)
--- Tests/AppTests/Mocks/KeywordShow+mock.swift
+++ Tests/AppTests/Mocks/KeywordShow+mock.swift
@@ -22,7 +22,9 @@ extension KeywordShow.Model {
         let packages = (1...10).map { PackageInfo(
             title: "Networking Package \($0)",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec orci scelerisque, interdum purus a, tempus turpis.",
-            url: "",
+            repositoryOwner: "owner",
+            repositoryName: "name",
+            url: "https://example.com/owner/name.git",
             stars: 4
         ) }
         return .init(keyword: "networking", packages: packages, page: 1, hasMoreResults: false)
