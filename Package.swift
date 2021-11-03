bec1392c9731e32dea92cda7339a9df04c7d9f16
Sven A. Schmidt <sas@finestructure.co>
2021-11-03 09:35:34 +0100

    Switch to SwiftPM/main

--- Package.swift
+++ Package.swift
@@ -39,10 +39,7 @@ let package = Package(
         .package(url: "https://github.com/handya/OhhAuth.git", from: "1.4.0"),
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.3.2"),
         .package(name: "SwiftPM",
-                 url: "https://github.com/finestructure/swift-package-manager.git",
-//                 .branch("signer-default-observability")
-                 .revision("7e36dcf37efd18fb2aa8d416429d3fa3b3f92eba")
-                )
+                 url: "https://github.com/apple/swift-package-manager.git", .branch("main"))
     ],
     targets: [
         .target(name: "App", dependencies: [
