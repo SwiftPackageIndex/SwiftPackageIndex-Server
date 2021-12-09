1be880ca44f1bdeb0f4256f46488e8d44b6d4ca0
Dave Verwer <dave.verwer@gmail.com>
2021-12-09 12:19:16 +0000

    Don't show filter suggestions on production.

--- Sources/App/Views/Search/SearchShow+View.swift
+++ Sources/App/Views/Search/SearchShow+View.swift
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import Vapor
 import Plot
 
 
@@ -39,18 +40,19 @@ extension SearchShow {
                 .section(
                     .class("search"),
                     .searchForm(query: model.query),
-                    .div(
-                        .class("filter_suggestions"),
-                        .text("Add filters to narrow search results. "),
-                        .span(
-                            .class("learn_more"),
-                            .a(
-                                .href(SiteURL.faq.relativeURL(anchor: "search-filters")),
-                                .text("Learn more")
-                            ),
-                            .text(".")
-                        )
-                    )
+                    .if(Environment.current != .production,
+                        .div(
+                            .class("filter_suggestions"),
+                            .text("Add filters to narrow search results. "),
+                            .span(
+                                .class("learn_more"),
+                                .a(
+                                    .href(SiteURL.faq.relativeURL(anchor: "search-filters")),
+                                    .text("Learn more")
+                                ),
+                                .text(".")
+                            )
+                        ))
                 ),
                 .if(model.query.count > 0, resultsSection())
             )
