bf124bc8043643f8cea19f0ac0167a9db785b25d
Sven A. Schmidt <sas@finestructure.co>
2022-02-02 11:19:46 +0100

    Drop awkwards defaults on non-test Version.init

---
+++ Tests/AppTests/Helpers/Version+init.swift
@@ -0,0 +1,43 @@
+@testable import App
+import Foundation
+import DependencyResolution
+
+
+#if DEBUG
+extension Version {
+    // Convenience initializer to set defaults for some required values to
+    // make instantiation in tests simpler.
+    convenience init(id: Id? = nil,
+                     package: Package,
+                     commit: CommitHash = "",
+                     commitDate: Date = .distantPast,
+                     latest: Kind? = nil,
+                     packageName: String? = nil,
+                     publishedAt: Date? = nil,
+                     reference: Reference = .branch("main"),
+                     releaseNotes: String? = nil,
+                     releaseNotesHTML: String? = nil,
+                     resolvedDependencies: [ResolvedDependency] = [],
+                     supportedPlatforms: [Platform] = [],
+                     swiftVersions: [SwiftVersion] = [],
+                     toolsVersion: String? = nil,
+                     url: String? = nil) throws {
+        self.init()
+        self.id = id
+        self.$package.id = try package.requireID()
+        self.commit = commit
+        self.commitDate = commitDate
+        self.latest = latest
+        self.packageName = packageName
+        self.publishedAt = publishedAt
+        self.reference = reference
+        self.releaseNotes = releaseNotes
+        self.releaseNotesHTML = releaseNotesHTML
+        self.resolvedDependencies = resolvedDependencies
+        self.supportedPlatforms = supportedPlatforms
+        self.swiftVersions = swiftVersions
+        self.toolsVersion = toolsVersion
+        self.url = url
+    }
+}
+#endif
