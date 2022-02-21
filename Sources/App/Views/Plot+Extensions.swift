135740a2cdefee0c810b4983374040961761b258
Dave Verwer <dave.verwer@gmail.com>
2022-02-21 16:03:02 +0000

    Added a very basic display of matching keywords into the package list helper.

--- Sources/App/Views/Plot+Extensions.swift
+++ Sources/App/Views/Plot+Extensions.swift
@@ -120,12 +120,24 @@ extension Node where Context == HTML.ListContext {
         }
     }
 
-    static func packageListItem(linkUrl: String, packageName: String, summary: String?, repositoryOwner: String, repositoryName: String, stars: Int?, lastActivityAt: Date?) -> Self {
+    static func packageListItem(linkUrl: String,
+                                packageName: String,
+                                summary: String?,
+                                matchingKeywords: [String]? = nil,
+                                repositoryOwner: String,
+                                repositoryName: String,
+                                stars: Int?,
+                                lastActivityAt: Date?) -> Self {
         .li(
             .a(
                 .href(linkUrl),
                 .h4(.text(packageName)),
                 .unwrap(summary) { .p(.text($0)) },
+                .unwrap(matchingKeywords) { keywords in
+                .ul(
+                    .class("keywords")
+                )
+                },
                 .ul(
                     .class("metadata"),
                     .li(
@@ -146,6 +158,19 @@ extension Node where Context == HTML.ListContext {
                         .starsListItem(numberOfStars: $0)
                     }
                 )
+            )
+        )
+    }
+
+    private static func packageListItemKeywordList(keywords: [String]) -> Node<HTML.BodyContext> {
+        .ul(
+            .class("keywords"),
+            .group(
+                keywords.map { keyword in
+                    .li(
+                        .text(keyword)
+                    )
+                }
             )
         )
     }
