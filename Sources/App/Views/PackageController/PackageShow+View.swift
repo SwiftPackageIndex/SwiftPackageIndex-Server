b4aecc81a4f09cdce38e865ba74211ad42502b8d
Dave Verwer <dave.verwer@gmail.com>
2022-03-08 14:44:15 +0000

    Working around the Safari layout issue.

--- Sources/App/Views/PackageController/PackageShow+View.swift
+++ Sources/App/Views/PackageController/PackageShow+View.swift
@@ -140,7 +140,7 @@ enum PackageShow {
             .section(
                 .class("main_compatibility"),
                 .div(
-                    .class("two_column v_end"),
+                    .class("two_column even v_end"),
                     .h3("Compatibility"),
                     .a(
                         .href(SiteURL.package(.value(model.repositoryOwner), .value(model.repositoryName), .builds).relativeURL()),
