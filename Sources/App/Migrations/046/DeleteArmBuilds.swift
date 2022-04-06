6e81e8c895fba919c69b8019f01ca147612eb572
Sven A. Schmidt <sas@finestructure.co>
2022-04-06 09:40:41 +0200

    Add arm build deletion migration + test

---
+++ Sources/App/Migrations/046/DeleteArmBuilds.swift
@@ -0,0 +1,32 @@
+// Copyright 2020-2021 Dave Verwer, Sven A. Schmidt, and other contributors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import Fluent
+import SQLKit
+
+
+struct DeleteArmBuilds: AsyncMigration {
+    func prepare(on database: Database) async throws {
+        guard let db = database as? SQLDatabase else {
+            fatalError("Database must be an SQLDatabase ('as? SQLDatabase' must succeed)")
+        }
+
+        // required for levenshtein distance in search
+        try await db.raw(#"DELETE FROM "builds" WHERE "platform" like '%-arm'"#).run()
+    }
+
+    func revert(on database: Database) async throws {
+        // There's nothing we can do to restore the previous state
+    }
+}
