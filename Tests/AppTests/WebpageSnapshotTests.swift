c6a476ed300d345729241647ffc96628e44c8ce2
Dave Verwer <dave.verwer@gmail.com>
2022-05-03 19:30:00 +0100

    Added DocC snapshot test.

--- Tests/AppTests/WebpageSnapshotTests.swift
+++ Tests/AppTests/WebpageSnapshotTests.swift
@@ -332,4 +332,12 @@ class WebpageSnapshotTests: SnapshotTestCase {
 
         assertSnapshot(matching: page, as: .html)
     }
+
+    func test_DocC_Template() throws {
+        let doccTemplatePath = fixturesDirectory().appendingPathComponent("docc-template.html").path
+        let doccHtml = try String(contentsOfFile: doccTemplatePath)
+        let processor = try XCTUnwrap(DocumentationPageProcessor(rawHtml: doccHtml))
+
+        assertSnapshot(matching: processor.processedPage, as: .html)
+    }
 }
