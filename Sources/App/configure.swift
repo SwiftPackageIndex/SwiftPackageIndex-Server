0fc368a86a99405d4351052b2797d4c9c03f3810
Sven A. Schmidt <sas@finestructure.co>
2022-06-02 17:55:43 +0200

    Add migration

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -238,6 +238,12 @@ public func configure(_ app: Application) throws -> String {
     do {  // Migration 049 - add versions.spi_manifest
         app.migrations.add(UpdateVersionAddSPIManifest())
     }
+    #warning("migration temporarily excluded from production")
+    if Environment.current != .production {
+        do {  // Migration 050 - add versions.doc_archives
+            app.migrations.add(UpdateVersionAddDocArchives())
+        }
+    }
 
     app.commands.use(Analyze.Command(), as: "analyze")
     app.commands.use(CreateRestfileCommand(), as: "create-restfile")
