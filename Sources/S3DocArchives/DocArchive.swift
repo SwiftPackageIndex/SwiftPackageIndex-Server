c6eb4fa1d76913605ba476614889b0836b059db6
Sven A. Schmidt <sas@finestructure.co>
2022-06-13 12:44:17 +0200

    Add S3DocArchives

---
+++ Sources/S3DocArchives/DocArchive.swift
@@ -0,0 +1,130 @@
+// Copyright 2020-2022 Dave Verwer, Sven A. Schmidt, and other contributors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import Foundation
+import Parsing
+import SotoS3
+
+
+public struct DocArchive: Codable, Equatable {
+    public var path: Path
+    public var title: String
+
+    init(s3: S3, in bucket: String, path: Path) async {
+        self.path = path
+        self.title = (try? await s3.getDocArchiveTitle(in: bucket, path: path)) ?? path.product
+    }
+
+#if DEBUG
+    // for unit testing purposes only
+    init(path: Path, title: String) {
+        self.path = path
+        self.title = title
+    }
+
+    static func mock(_ owner: String = "foo",
+                     _ repository: String = "bar",
+                     _ ref: String = "ref",
+                     _ product: String = "product",
+                     _ title: String = "Product") -> Self {
+        .init(path: .init(owner: owner, repository: repository, ref: ref, product: product),
+              title: title)
+    }
+#endif
+}
+
+
+public extension DocArchive {
+    static func fetchAll(prefix: String,
+                         awsBucketName: String,
+                         awsAccessKeyId: String,
+                         awsSecretAccessKey: String) async throws -> [DocArchive] {
+        let key = S3.StoreKey(bucket: awsBucketName, path: prefix)
+        let client = AWSClient(credentialProvider: .static(accessKeyId: awsAccessKeyId,
+                                                           secretAccessKey: awsSecretAccessKey),
+                               httpClientProvider: .createNew)
+        defer { try? client.syncShutdown() }
+
+        let s3 = S3(client: client, region: .useast2)
+
+        // filter this down somewhat by eliminating `.json` files
+        let paths = try await s3.listFiles(key: key, delimiter: ".json")
+            .compactMap { try? path.parse($0.file.key) }
+
+        var archives = [DocArchive]()
+        for path in paths {
+            archives.append(await DocArchive(s3: s3, in: awsBucketName, path: path))
+        }
+
+        return archives
+    }
+}
+
+
+extension DocArchive {
+    public struct Path: Codable, Equatable {
+        public var owner: String
+        public var repository: String
+        public var ref: String
+        public var product: String
+
+        var s3path: String { "\(owner)/\(repository)/\(ref)" }
+    }
+}
+
+
+extension DocArchive {
+    struct DocumentationData: Codable, Equatable {
+        var metadata: Metadata
+
+        struct Metadata: Codable, Equatable {
+            var title: String
+        }
+    }
+}
+
+
+// MARK: CustomStringConvertible
+
+extension DocArchive: CustomStringConvertible {
+    public var description: String {
+        "\(path) - \(title)"
+    }
+}
+
+
+extension DocArchive.Path: CustomStringConvertible {
+    public var description: String {
+        "\(owner)/\(repository) @ \(ref) - \(product)"
+    }
+}
+
+
+// MARK: - Parsers
+
+extension DocArchive {
+    static let pathSegment = Parse {
+        PrefixUpTo("/").map(.string)
+        "/"
+    }
+
+    static let path = Parse(DocArchive.Path.init) {
+        pathSegment
+        pathSegment
+        pathSegment
+        "documentation/"
+        pathSegment
+        "index.html"
+    }
+}
---
+++ Sources/S3DocArchives/S3+ext.swift
@@ -0,0 +1,77 @@
+// Copyright 2020-2022 Dave Verwer, Sven A. Schmidt, and other contributors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import Foundation
+
+import SotoS3
+
+
+extension S3 {
+
+    struct File {
+        var bucket: String
+        var key: String
+    }
+
+    struct FileDescriptor {
+        let file: File
+        let modificationDate: Date
+        let size: Int
+    }
+
+    struct StoreKey {
+        let bucket: String
+        var path: String
+
+        var url: String { "s3://\(bucket)/\(path)" }
+    }
+
+    func getFileContent(key: StoreKey) async throws -> Data? {
+        let getObjectRequest = S3.GetObjectRequest(bucket: key.bucket, key: key.path)
+        return try await self.getObject(getObjectRequest)
+            .body?.asData()
+    }
+
+    func listFiles(key: StoreKey, delimiter: String? = nil) async throws -> [FileDescriptor] {
+        try await listFiles(key: key, delimiter: delimiter).get()
+    }
+
+    func listFiles(key: StoreKey, delimiter: String? = nil) -> EventLoopFuture<[FileDescriptor]> {
+        let bucket = key.bucket
+        let request = S3.ListObjectsV2Request(bucket: bucket, delimiter: delimiter, prefix: key.path)
+        return listObjectsV2Paginator(request, []) { accumulator, response, eventLoop in
+            let files: [FileDescriptor] = response.contents?.compactMap {
+                guard let key = $0.key,
+                      let lastModified = $0.lastModified,
+                      let fileSize = $0.size else { return nil }
+                return FileDescriptor(
+                    file: File(bucket: bucket, key: key),
+                    modificationDate: lastModified,
+                    size: Int(fileSize)
+                )
+            } ?? []
+            return eventLoop.makeSucceededFuture((true, accumulator + files))
+        }
+    }
+
+    func getDocArchiveTitle(in bucket: String,
+                            path: DocArchive.Path) async throws -> String? {
+        let key = S3.StoreKey(bucket: bucket,
+                              path: path.s3path + "/data/documentation/\(path.product).json")
+        guard let data = try await getFileContent(key: key) else { return nil }
+        return try JSONDecoder().decode(DocArchive.DocumentationData.self, from: data)
+            .metadata.title
+    }
+
+}
