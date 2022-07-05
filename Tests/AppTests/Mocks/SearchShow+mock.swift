ac410b035f7def6904efbe7afdf819679d59f276
Jeroen Leenarts <jeroen.leenarts@getstream.io>
2022-07-05 13:31:35 +0200

    Fix unit tests.

--- Tests/AppTests/Mocks/SearchShow+mock.swift
+++ Tests/AppTests/Mocks/SearchShow+mock.swift
@@ -17,10 +17,19 @@
 import Foundation
 
 extension SearchShow.Model {
+    
+    private static func mockedWeightedKeywords(results: [Search.Result]) -> [PackageShow.Model.WeightedKeyword] {
+        let keywords = results.compactMap { $0.keywordResult?.keyword }
+        
+        let counts:[Int] = Array(1...keywords.count)
+        return zip(keywords, counts).map {
+            PackageShow.Model.WeightedKeyword(keyword: $0, weight: $1)
+        }
+    }
     static func mock(results: [Search.Result] = .mock()) -> Self {
         return .init(page: 3,
                      query: "query",
-                     response: .init(hasMoreResults: true, searchTerm: "query", searchFilters: [], results: results), weightedKeywords: [])
+                     response: .init(hasMoreResults: true, searchTerm: "query", searchFilters: [], results: results), weightedKeywords: mockedWeightedKeywords(results: results))
     }
     
     static func mockWithFilter(results: [Search.Result] = .mock()) -> Self {
@@ -31,6 +40,6 @@ extension SearchShow.Model {
                                      searchFilters: [
                                         .init(key: "license", operator: "is", value: "mit")
                                      ],
-                                     results: results), weightedKeywords: [])
+                                     results: results), weightedKeywords: mockedWeightedKeywords(results: results))
     }
 }
