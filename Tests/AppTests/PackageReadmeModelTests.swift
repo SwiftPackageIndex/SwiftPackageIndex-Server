6239c393c6813ddbb7f3b6045d45ae45cfd2c354
Dave Verwer <dave.verwer@gmail.com>
2022-07-25 16:02:07 +0100

    Added one more test case with multiple unencoded values.

--- Tests/AppTests/PackageReadmeModelTests.swift
+++ Tests/AppTests/PackageReadmeModelTests.swift
@@ -103,11 +103,14 @@ class PackageReadmeModelTests: SnapshotTestCase {
     }
 
     func test_url_initWithPotentiallyPercentEncodedPath() throws {
+        // Relative URLs
         XCTAssertEqual(try XCTUnwrap(URL(withPotentiallyPercentEncodedPath: "/root/relative/url")).absoluteString, "/root/relative/url")
         XCTAssertEqual(try XCTUnwrap(URL(withPotentiallyPercentEncodedPath: "relative/url")).absoluteString, "relative/url")
-        XCTAssertEqual(try XCTUnwrap(URL(withPotentiallyPercentEncodedPath: "/relative/encoded%20spaces")).absoluteString, "/relative/encoded%20spaces")
-        XCTAssertEqual(try XCTUnwrap(URL(withPotentiallyPercentEncodedPath: "/relative/unencoded spaces")).absoluteString, "/relative/unencoded%20spaces")
+        XCTAssertEqual(try XCTUnwrap(URL(withPotentiallyPercentEncodedPath: "/encoded%20spaces")).absoluteString, "/encoded%20spaces")
+        XCTAssertEqual(try XCTUnwrap(URL(withPotentiallyPercentEncodedPath: "/unencoded spaces")).absoluteString, "/unencoded%20spaces")
+        XCTAssertEqual(try XCTUnwrap(URL(withPotentiallyPercentEncodedPath: "/multiple {unencoded}")).absoluteString, "/multiple%20%7Bunencoded%7D")
 
+        // Absolute URLs
         XCTAssertEqual(try XCTUnwrap(URL(withPotentiallyPercentEncodedPath: "https://full.host/and/path")).absoluteString, "https://full.host/and/path")
         XCTAssertEqual(try XCTUnwrap(URL(withPotentiallyPercentEncodedPath: "https://full.host/encoded%20spaces")).absoluteString, "https://full.host/encoded%20spaces")
         XCTAssertEqual(try XCTUnwrap(URL(withPotentiallyPercentEncodedPath: "https://full.host/unencoded spaces")).absoluteString, "https://full.host/unencoded%20spaces")
