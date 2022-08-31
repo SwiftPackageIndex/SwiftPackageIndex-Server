84a788c370ac232fb1ad983666cfb9777a50e343
Dave Verwer <dave.verwer@gmail.com>
2022-08-31 12:31:51 +0100

    First try at the community sponsors file.

---
+++ Sources/App/Core/CommunitySponsors.swift
@@ -0,0 +1,446 @@
+// Copyright 2020-2021 Dave Verwer, Sven A. Schmidt, and other contributors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This file is auto-generated. Do not modify. For more information,
+// see: https://github.com/SwiftPackageIndex/UpdateCommunitySponsors
+
+enum CommunitySponsors {
+    static var sponsors = [
+        CommunitySponsor(
+            name: "Abizer Nasir",
+            avatarUrl: "https://avatars.githubusercontent.com/u/6902?u=380b013b423d7a09844031dfc4ab40581e096d87&v=4"
+        ),
+        CommunitySponsor(
+            name: "Naoki Hiroshima",
+            avatarUrl: "https://avatars.githubusercontent.com/u/10003?v=4"
+        ),
+        CommunitySponsor(
+            name: "Paul Morganthall",
+            avatarUrl: "https://avatars.githubusercontent.com/u/13285?u=d771a4b3d64f88c4b968ab1c152997c38ef351c7&v=4"
+        ),
+        CommunitySponsor(
+            name: "Ben Chatelain",
+            avatarUrl: "https://avatars.githubusercontent.com/u/28851?v=4"
+        ),
+        CommunitySponsor(
+            name: "Eneko Alonso",
+            avatarUrl: "https://avatars.githubusercontent.com/u/32922?v=4"
+        ),
+        CommunitySponsor(
+            name: "Joseph Heck",
+            avatarUrl: "https://avatars.githubusercontent.com/u/43388?u=a7ee72fd086f74f732941f971cd530350142ec3e&v=4"
+        ),
+        CommunitySponsor(
+            name: "Peter Steinberger",
+            avatarUrl: "https://avatars.githubusercontent.com/u/58493?u=95a4fccef3d341039661c427695691a664e333ba&v=4"
+        ),
+        CommunitySponsor(
+            name: "Andrew Tetlaw",
+            avatarUrl: "https://avatars.githubusercontent.com/u/64091?u=625a2167479210b6ec0426fc3c8e82c4fbaf1a79&v=4"
+        ),
+        CommunitySponsor(
+            name: "Thomas Dohmke",
+            avatarUrl: "https://avatars.githubusercontent.com/u/70720?u=5210ac874456eed768fe8b537322c1798acb9917&v=4"
+        ),
+        CommunitySponsor(
+            name: "Drew McCormack",
+            avatarUrl: "https://avatars.githubusercontent.com/u/77312?u=f078a71b06088b642f47748ac1a0d0d7a9068e93&v=4"
+        ),
+        CommunitySponsor(
+            name: "Joshua Nozzi",
+            avatarUrl: "https://avatars.githubusercontent.com/u/81273?u=13863d1ff1800b2319e77bb06f9cd02f25bc7aa0&v=4"
+        ),
+        CommunitySponsor(
+            name: "Matt Massicotte",
+            avatarUrl: "https://avatars.githubusercontent.com/u/85322?u=686384227a6f3796b5c80ab6f310da95bdce973c&v=4"
+        ),
+        CommunitySponsor(
+            name: "Max Desiatov",
+            avatarUrl: "https://avatars.githubusercontent.com/u/112310?u=3c86a5baad3413ec9df4c30c094d1fe89ca411b6&v=4"
+        ),
+        CommunitySponsor(
+            name: "Keith Harrison",
+            avatarUrl: "https://avatars.githubusercontent.com/u/118883?u=7fcbd7356369770046a3b1ca96e84eafd8d4282f&v=4"
+        ),
+        CommunitySponsor(
+            name: "Boris Bügling",
+            avatarUrl: "https://avatars.githubusercontent.com/u/118992?u=544f64efa9cbdbf2adaa9c205b1a1394832c2d84&v=4"
+        ),
+        CommunitySponsor(
+            name: "Ray Fix",
+            avatarUrl: "https://avatars.githubusercontent.com/u/137963?v=4"
+        ),
+        CommunitySponsor(
+            name: "Ben Dodson",
+            avatarUrl: "https://avatars.githubusercontent.com/u/157243?v=4"
+        ),
+        CommunitySponsor(
+            name: "Sam Symons",
+            avatarUrl: "https://avatars.githubusercontent.com/u/183774?u=f30f655ab3ddba4bf1ceff3aaf8669e1101e53c0&v=4"
+        ),
+        CommunitySponsor(
+            name: nil,
+            avatarUrl: "https://avatars.githubusercontent.com/u/187675?u=c1780a7bd4d210abaefcee5f0fdf841a883bd815&v=4"
+        ),
+        CommunitySponsor(
+            name: "Paul Hudson",
+            avatarUrl: "https://avatars.githubusercontent.com/u/190200?u=912a2a2f534026a4e445cd5dc3c5641d9825a6f4&v=4"
+        ),
+        CommunitySponsor(
+            name: "Jason Bruder",
+            avatarUrl: "https://avatars.githubusercontent.com/u/222564?u=00277f32dd48021c85b093b7172f613c5c94b31e&v=4"
+        ),
+        CommunitySponsor(
+            name: "Cihan",
+            avatarUrl: "https://avatars.githubusercontent.com/u/228203?u=70726bbd971683209cb3e2773112cab36f103250&v=4"
+        ),
+        CommunitySponsor(
+            name: "Massimo Biolcati",
+            avatarUrl: "https://avatars.githubusercontent.com/u/240599?v=4"
+        ),
+        CommunitySponsor(
+            name: "Oliver Drobnik",
+            avatarUrl: "https://avatars.githubusercontent.com/u/333270?v=4"
+        ),
+        CommunitySponsor(
+            name: "Evan K. Stone",
+            avatarUrl: "https://avatars.githubusercontent.com/u/377022?u=e7f60c60001651eddeebefd2d320d761ad421130&v=4"
+        ),
+        CommunitySponsor(
+            name: "Íker Karam",
+            avatarUrl: "https://avatars.githubusercontent.com/u/476819?u=35671e80faa8da7f01aa3b5530b2f3c4b0ac8055&v=4"
+        ),
+        CommunitySponsor(
+            name: "Chris",
+            avatarUrl: "https://avatars.githubusercontent.com/u/637026?u=95adf59c4fab9321e9f7caacf711eea283d27e91&v=4"
+        ),
+        CommunitySponsor(
+            name: "Marc Prud'hommeaux",
+            avatarUrl: "https://avatars.githubusercontent.com/u/659086?u=652e7f668806a424936e6ae31b6e7d5c9857383a&v=4"
+        ),
+        CommunitySponsor(
+            name: "Jeffrey Macko",
+            avatarUrl: "https://avatars.githubusercontent.com/u/661647?u=81081703e576c7211f21e1470994ac713f176c68&v=4"
+        ),
+        CommunitySponsor(
+            name: "Theodor Müller",
+            avatarUrl: "https://avatars.githubusercontent.com/u/668256?v=4"
+        ),
+        CommunitySponsor(
+            name: "Georg Tuparev",
+            avatarUrl: "https://avatars.githubusercontent.com/u/693171?u=219818bce486228f3a7099db56ed078241b597c5&v=4"
+        ),
+        CommunitySponsor(
+            name: "Riley Testut",
+            avatarUrl: "https://avatars.githubusercontent.com/u/705880?v=4"
+        ),
+        CommunitySponsor(
+            name: "Kyle Newsome",
+            avatarUrl: "https://avatars.githubusercontent.com/u/707507?u=3640b922b6c35ce0b05630b1da85527a42cf1f1a&v=4"
+        ),
+        CommunitySponsor(
+            name: "Alexandre Colucci",
+            avatarUrl: "https://avatars.githubusercontent.com/u/738543?u=eb968502ed1ce5a2761bb7ca64847bbd258ef3f6&v=4"
+        ),
+        CommunitySponsor(
+            name: "Alejandro Martínez",
+            avatarUrl: "https://avatars.githubusercontent.com/u/750912?v=4"
+        ),
+        CommunitySponsor(
+            name: "Marcin Krzyzanowski",
+            avatarUrl: "https://avatars.githubusercontent.com/u/758033?v=4"
+        ),
+        CommunitySponsor(
+            name: "Raymund Beyer",
+            avatarUrl: "https://avatars.githubusercontent.com/u/817177?v=4"
+        ),
+        CommunitySponsor(
+            name: nil,
+            avatarUrl: "https://avatars.githubusercontent.com/u/842678?u=3459b6ce8224d2c0cad06b05283a8579fb32f7de&v=4"
+        ),
+        CommunitySponsor(
+            name: "Joey Jarosz",
+            avatarUrl: "https://avatars.githubusercontent.com/u/856919?u=191c3a7883c794c5ab824e0b10dccb2abd045c90&v=4"
+        ),
+        CommunitySponsor(
+            name: "Tim Triemstra",
+            avatarUrl: "https://avatars.githubusercontent.com/u/923721?u=862992293d005a514df60ae4ab50101336e2717e&v=4"
+        ),
+        CommunitySponsor(
+            name: "Wei Wang",
+            avatarUrl: "https://avatars.githubusercontent.com/u/1019875?u=34477b070af505986e26066dfa6a5d2d64f319b6&v=4"
+        ),
+        CommunitySponsor(
+            name: nil,
+            avatarUrl: "https://avatars.githubusercontent.com/u/1046635?v=4"
+        ),
+        CommunitySponsor(
+            name: "Joshua Kaden",
+            avatarUrl: "https://avatars.githubusercontent.com/u/1244926?v=4"
+        ),
+        CommunitySponsor(
+            name: "Juanpe Catalán",
+            avatarUrl: "https://avatars.githubusercontent.com/u/1409041?u=cf6a1ead5e8c9fb31852e60fbaf2cefc9377e82e&v=4"
+        ),
+        CommunitySponsor(
+            name: "PSPDFKit GmbH",
+            avatarUrl: "https://avatars.githubusercontent.com/u/1527679?v=4"
+        ),
+        CommunitySponsor(
+            name: "Jules Coynel",
+            avatarUrl: "https://avatars.githubusercontent.com/u/1749664?u=8739e6ed416a43a1a4e943c80ba96a2205175185&v=4"
+        ),
+        CommunitySponsor(
+            name: "Hiroshi Kimura",
+            avatarUrl: "https://avatars.githubusercontent.com/u/1888355?u=ad57f4522c12230428c401223f21b902cef4f6e5&v=4"
+        ),
+        CommunitySponsor(
+            name: "Chris Mash",
+            avatarUrl: "https://avatars.githubusercontent.com/u/2419640?v=4"
+        ),
+        CommunitySponsor(
+            name: "David Roman",
+            avatarUrl: "https://avatars.githubusercontent.com/u/2538074?u=1390da8466bd4d8c0f1223a3c41efdedcfefdf7d&v=4"
+        ),
+        CommunitySponsor(
+            name: "David Okun",
+            avatarUrl: "https://avatars.githubusercontent.com/u/2584705?u=775c6272e145b256cb41057716e1b8ba8677742c&v=4"
+        ),
+        CommunitySponsor(
+            name: "Brian Drelling",
+            avatarUrl: "https://avatars.githubusercontent.com/u/3022693?u=2c13a2938360ecd59ba05b4aeadb2e972071091d&v=4"
+        ),
+        CommunitySponsor(
+            name: "Bob Wakefield",
+            avatarUrl: "https://avatars.githubusercontent.com/u/3050944?u=9335a5f808a4fcc5a49a16d02b6361dd3833cf6c&v=4"
+        ),
+        CommunitySponsor(
+            name: "Peter Molnar",
+            avatarUrl: "https://avatars.githubusercontent.com/u/3090905?u=05cb19a5130243b89c90d8415821299e20df9bb3&v=4"
+        ),
+        CommunitySponsor(
+            name: "Robin Kunde",
+            avatarUrl: "https://avatars.githubusercontent.com/u/3142400?u=1b5e6c270b12b50f94f4b57955cafaa6798cd76f&v=4"
+        ),
+        CommunitySponsor(
+            name: nil,
+            avatarUrl: "https://avatars.githubusercontent.com/u/3152038?v=4"
+        ),
+        CommunitySponsor(
+            name: "Gaige B Paulsen",
+            avatarUrl: "https://avatars.githubusercontent.com/u/3351751?u=c27bedf869c3dbf75fe6e8857169d68b12e0678e&v=4"
+        ),
+        CommunitySponsor(
+            name: "Nathan Harris",
+            avatarUrl: "https://avatars.githubusercontent.com/u/3506776?u=48101ab271f57ccb3de67e4ac7d2dc52ec79c19b&v=4"
+        ),
+        CommunitySponsor(
+            name: "Silverpine Software",
+            avatarUrl: "https://avatars.githubusercontent.com/u/3859102?v=4"
+        ),
+        CommunitySponsor(
+            name: "Varun Santhanam",
+            avatarUrl: "https://avatars.githubusercontent.com/u/3914230?u=1437d22b9ada924bb0abd7c6e5e9323b53cade93&v=4"
+        ),
+        CommunitySponsor(
+            name: "Umar Haroon",
+            avatarUrl: "https://avatars.githubusercontent.com/u/3974613?u=c4bdfa3d156f30ddbc1150284dab338c6de79ec0&v=4"
+        ),
+        CommunitySponsor(
+            name: "Marco Eidinger",
+            avatarUrl: "https://avatars.githubusercontent.com/u/4176826?u=4a5f9707743bfbb665352ab84be7feb7942a9920&v=4"
+        ),
+        CommunitySponsor(
+            name: "Jeremy Saklad",
+            avatarUrl: "https://avatars.githubusercontent.com/u/4258823?v=4"
+        ),
+        CommunitySponsor(
+            name: "Leo Liberman",
+            avatarUrl: "https://avatars.githubusercontent.com/u/4295007?u=706f9311903b12c42366a9bd09dfe2f10b8df9aa&v=4"
+        ),
+        CommunitySponsor(
+            name: "objc.io",
+            avatarUrl: "https://avatars.githubusercontent.com/u/4409139?v=4"
+        ),
+        CommunitySponsor(
+            name: "Christopher Rex",
+            avatarUrl: "https://avatars.githubusercontent.com/u/4457170?v=4"
+        ),
+        CommunitySponsor(
+            name: "Victor Sigler",
+            avatarUrl: "https://avatars.githubusercontent.com/u/4573453?u=ecc5faaa658a54d2c705c24c891d870a3993cce7&v=4"
+        ),
+        CommunitySponsor(
+            name: "Federico Zanetello",
+            avatarUrl: "https://avatars.githubusercontent.com/u/5277837?u=18315a30c7633bbb3fba55097f4cdf1a9e913a3e&v=4"
+        ),
+        CommunitySponsor(
+            name: "Arpad Zalan",
+            avatarUrl: "https://avatars.githubusercontent.com/u/5535925?v=4"
+        ),
+        CommunitySponsor(
+            name: "Karolina Samorek",
+            avatarUrl: "https://avatars.githubusercontent.com/u/5767416?u=5b1f75e68c1f993b5013091c33b1cd283221ae53&v=4"
+        ),
+        CommunitySponsor(
+            name: "Scott Robbins",
+            avatarUrl: "https://avatars.githubusercontent.com/u/6894359?u=f30a5959b161ebb09ec81a74bb4b66ff080fe9e8&v=4"
+        ),
+        CommunitySponsor(
+            name: "Chris Martin",
+            avatarUrl: "https://avatars.githubusercontent.com/u/7174994?v=4"
+        ),
+        CommunitySponsor(
+            name: "Idelfonso Gutierrez",
+            avatarUrl: "https://avatars.githubusercontent.com/u/7195235?u=c4b79d6cd67449f75b23afef971a2a6c1a6de77d&v=4"
+        ),
+        CommunitySponsor(
+            name: "Omar Albeik",
+            avatarUrl: "https://avatars.githubusercontent.com/u/8127757?u=2acfa470d8238d828b861505607e162500c9a0c9&v=4"
+        ),
+        CommunitySponsor(
+            name: "Joakim Hassila",
+            avatarUrl: "https://avatars.githubusercontent.com/u/8501048?u=0b4161d646a78badc952ed14fd0ac9f25a7d5a20&v=4"
+        ),
+        CommunitySponsor(
+            name: "Adam Fowler",
+            avatarUrl: "https://avatars.githubusercontent.com/u/9382567?u=519b4f0d165f180f80f8c03f3d0dc45faae88259&v=4"
+        ),
+        CommunitySponsor(
+            name: "Thomas Prezioso Jr.",
+            avatarUrl: "https://avatars.githubusercontent.com/u/9522197?u=5077d8ddad9b1f2694c3af75d3dc5f2ca52406b9&v=4"
+        ),
+        CommunitySponsor(
+            name: "Kyle Zawacki",
+            avatarUrl: "https://avatars.githubusercontent.com/u/9993442?u=208f39b88bc630bc7d0dc7f906a9ec4aed9ed37a&v=4"
+        ),
+        CommunitySponsor(
+            name: "Frank",
+            avatarUrl: "https://avatars.githubusercontent.com/u/10637868?u=ae73f0a44ef110c3375bb5b03985c78602031ac3&v=4"
+        ),
+        CommunitySponsor(
+            name: nil,
+            avatarUrl: "https://avatars.githubusercontent.com/u/11362913?u=4af3d048fb1678f198a4c9fe9a246017bed75052&v=4"
+        ),
+        CommunitySponsor(
+            name: "Marius Felkner",
+            avatarUrl: "https://avatars.githubusercontent.com/u/11645567?u=54066dd6578c7170742cb00acae0bdf56cfe9ca9&v=4"
+        ),
+        CommunitySponsor(
+            name: "Tristan Biesecker",
+            avatarUrl: "https://avatars.githubusercontent.com/u/12146879?u=1ae4c5828a3c133bd139a85da265a20e9f7a0f90&v=4"
+        ),
+        CommunitySponsor(
+            name: "Kyle Lee",
+            avatarUrl: "https://avatars.githubusercontent.com/u/13292981?u=849cd0ce3da6b708a8b05fe17c04fe11f5f1dea1&v=4"
+        ),
+        CommunitySponsor(
+            name: nil,
+            avatarUrl: "https://avatars.githubusercontent.com/u/14205867?v=4"
+        ),
+        CommunitySponsor(
+            name: "Marcos Griselli",
+            avatarUrl: "https://avatars.githubusercontent.com/u/14804033?u=43370bf3b74f17be2fe12c9a80b87b16029512fb&v=4"
+        ),
+        CommunitySponsor(
+            name: "James Sherlock",
+            avatarUrl: "https://avatars.githubusercontent.com/u/15193942?u=f789c13c13098bf6f7e96aca9116d6d8dbf1a73b&v=4"
+        ),
+        CommunitySponsor(
+            name: "Manu Rodríguez",
+            avatarUrl: "https://avatars.githubusercontent.com/u/15260744?u=6bae0835af37b2320f46a07a1d9a1611ba67ae83&v=4"
+        ),
+        CommunitySponsor(
+            name: "Loopwerk",
+            avatarUrl: "https://avatars.githubusercontent.com/u/16154378?v=4"
+        ),
+        CommunitySponsor(
+            name: "Xaver Lohmüller",
+            avatarUrl: "https://avatars.githubusercontent.com/u/16212751?v=4"
+        ),
+        CommunitySponsor(
+            name: "Arnold Rozon",
+            avatarUrl: "https://avatars.githubusercontent.com/u/16907089?u=58c29bc8fbe3092512f93764e01b352b4bd78571&v=4"
+        ),
+        CommunitySponsor(
+            name: "Witek Bobrowski",
+            avatarUrl: "https://avatars.githubusercontent.com/u/18266391?u=871aff7b95834f35da5c4a2a89b1b2a4015bb5c6&v=4"
+        ),
+        CommunitySponsor(
+            name: "emin",
+            avatarUrl: "https://avatars.githubusercontent.com/u/21968377?u=19b33db70d1f53634b30b06902c163dfd93b1327&v=4"
+        ),
+        CommunitySponsor(
+            name: "Richard L Zarth III",
+            avatarUrl: "https://avatars.githubusercontent.com/u/22179660?u=bbba7685433c8ae7d9977219157674f1118ac275&v=4"
+        ),
+        CommunitySponsor(
+            name: "B.T. Franklin",
+            avatarUrl: "https://avatars.githubusercontent.com/u/23176608?v=4"
+        ),
+        CommunitySponsor(
+            name: "Broken Hands",
+            avatarUrl: "https://avatars.githubusercontent.com/u/23179490?v=4"
+        ),
+        CommunitySponsor(
+            name: "Feli Bernutz",
+            avatarUrl: "https://avatars.githubusercontent.com/u/26111180?v=4"
+        ),
+        CommunitySponsor(
+            name: "Joohee Kim",
+            avatarUrl: "https://avatars.githubusercontent.com/u/28954046?u=775af2ab5b835fd9f9489ce1717fed085f1d6e74&v=4"
+        ),
+        CommunitySponsor(
+            name: "Point-Free",
+            avatarUrl: "https://avatars.githubusercontent.com/u/29466629?v=4"
+        ),
+        CommunitySponsor(
+            name: "Alexander Sandberg",
+            avatarUrl: "https://avatars.githubusercontent.com/u/35671299?u=2f44069a09e1088091ef0cef4d410fbdbd5ad663&v=4"
+        ),
+        CommunitySponsor(
+            name: "Timo",
+            avatarUrl: "https://avatars.githubusercontent.com/u/38291523?u=ee9baa49406d7fadb78f02d4e25ca27d70266ff0&v=4"
+        ),
+        CommunitySponsor(
+            name: "Paul Farnell",
+            avatarUrl: "https://avatars.githubusercontent.com/u/48354391?v=4"
+        ),
+        CommunitySponsor(
+            name: "Fleuronic",
+            avatarUrl: "https://avatars.githubusercontent.com/u/51964521?v=4"
+        ),
+        CommunitySponsor(
+            name: "Dev Agani",
+            avatarUrl: "https://avatars.githubusercontent.com/u/64806625?u=e7985c9a4c87a845557e1bfdf70841d579936411&v=4"
+        ),
+        CommunitySponsor(
+            name: "Emerge Tools",
+            avatarUrl: "https://avatars.githubusercontent.com/u/74033486?v=4"
+        ),
+        CommunitySponsor(
+            name: "TapEnvy.us, LLC",
+            avatarUrl: "https://avatars.githubusercontent.com/u/88908726?v=4"
+        ),
+        CommunitySponsor(
+            name: "Sparrow Code",
+            avatarUrl: "https://avatars.githubusercontent.com/u/98487302?v=4"
+        ),
+    ]
+}
+
+struct CommunitySponsor {
+    let name: String?
+    let avatarUrl: String
+}
