9117476e2f6da4f8fbad058921ca21be7659a3c4
jcubit <jav.cuesta@gmail.com>
2022-10-01 10:47:54 +0200

    change return type to PackageAuthors

--- Sources/App/Views/PackageController/PackageResult+ModelSupport.swift
+++ Sources/App/Views/PackageController/PackageResult+ModelSupport.swift
@@ -1,4 +1,4 @@
-// Copyright 2020-2021 Dave Verwer, Sven A. Schmidt, and other contributors.
+// Copyright 2020-2022 Dave Verwer, Sven A. Schmidt, and other contributors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,10 +17,10 @@ import Foundation
 
 extension PackageController.PackageResult {
 
-    func authors() -> [Link]? {
+    func authors() -> PackageAuthors? {
         // TODO: fill in
         // https://github.com/SwiftPackageIndex/SwiftPackageIndex-Server/issues/175
-        return nil
+        return repository.authors
     }
 
     func activity() -> PackageShow.Model.Activity? {
