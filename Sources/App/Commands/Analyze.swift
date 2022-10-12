841c242e4b36efda077dd265436f381fa4ac5a2d
jcubit <jav.cuesta@gmail.com>
2022-10-12 18:22:21 +0200

    fixed rename of placeholder

--- Sources/App/Commands/Analyze.swift
+++ Sources/App/Commands/Analyze.swift
@@ -360,7 +360,7 @@ extension Analyze {
         repo.commitCount = (try? Current.git.commitCount(gitDirectory)) ?? 0
         repo.firstCommitDate = try? Current.git.firstCommitDate(gitDirectory)
         repo.lastCommitDate = try? Current.git.lastCommitDate(gitDirectory)
-        repo.authors = try PackageContributors.extract(cacheDirPath: gitDirectory, packageID: package.model.id)
+        repo.authors = try PackageContributors.extract(gitCacheDirectoryPath: gitDirectory, packageID: package.model.id)
 
         try await repo.update(on: database)
     }
