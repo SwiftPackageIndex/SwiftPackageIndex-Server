5d8b72be1132f9e95f9c3918b73188a328d73c69
Sven A. Schmidt <sas@finestructure.co>
2022-10-12 11:20:57 +0200

    Remove unused variants, they're easy enough to add back

--- Sources/App/Core/Extensions/Comparable+clamped.swift
+++ Sources/App/Core/Extensions/Comparable+clamped.swift
@@ -13,18 +13,8 @@
 // limitations under the License.
 
 
-// periphery:ignore
 extension Comparable {
-    func clamped(to limits: ClosedRange<Self>) -> Self {
-        clamped(to: limits.lowerBound...)
-            .clamped(to: ...limits.upperBound)
-    }
-
     func clamped(to limit: PartialRangeFrom<Self>) -> Self {
         max(self, limit.lowerBound)
-    }
-
-    func clamped(to limit: PartialRangeThrough<Self>) -> Self {
-        min(self, limit.upperBound)
     }
 }
