38c9ad6a13ebe94fdd96344e3f87644fc4022716
Sven A. Schmidt <sas@finestructure.co>
2023-04-21 10:11:41 +0200

    Revert try?

--- Sources/App/Commands/Common.swift
+++ Sources/App/Commands/Common.swift
@@ -68,7 +68,7 @@ func updatePackage(client: Client,
                 try await pkg.update(on: database)
             } catch {
                 logger.report(error: error)
-                try? await Current.reportError(client, .critical, error)
+                try await Current.reportError(client, .critical, error)
             }
 
         case .failure(let error) where error as? PostgresNIO.PostgresError != nil:
