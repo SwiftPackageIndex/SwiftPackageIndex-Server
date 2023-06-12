3f06cc0085db689a873a0db14bf67e7ff3b4e93f
Sven A. Schmidt <sas@finestructure.co>
2023-06-12 12:36:17 +0200

    Fix build on Xcode 15

--- Package.swift
+++ Package.swift
@@ -16,6 +16,7 @@
 
 import PackageDescription
 
+#warning("see FIXME below")
 let package = Package(
     name: "SPI-Server",
     platforms: [
@@ -34,7 +35,8 @@ let package = Package(
         .package(url: "https://github.com/dankinsoid/VaporToOpenAPI.git", from: "4.0.0"),
         .package(url: "https://github.com/pointfreeco/swift-custom-dump.git", from: "0.10.0"),
         .package(url: "https://github.com/pointfreeco/swift-parsing.git", from: "0.12.0"),
-        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.7.2"),
+        // FIXME: change this back to origin repo
+        .package(url: "https://github.com/finestructure/swift-snapshot-testing.git", revision: "d47aff583470146b355108f08a74f958d3b0b111"),
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.3.2"),
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", exact: "2.6.2"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
