7add0a1ff01c60b70e9df0e44cfe13ed46c080cf
Sven A. Schmidt <sas@finestructure.co>
2023-06-20 09:42:26 +0200

    Refactor test_siteMapForPackage_noDocs

--- Tests/AppTests/SitemapTests.swift
+++ Tests/AppTests/SitemapTests.swift
@@ -20,7 +20,7 @@ import XCTVapor
 
 
 class SitemapTests: SnapshotTestCase {
-
+    
     @MainActor
     func test_siteMapIndex() async throws {
         // Setup
@@ -32,7 +32,7 @@ class SitemapTests: SnapshotTestCase {
         try await packages.map { try Version(package: $0, packageName: "foo",
                                              reference: .branch("default")) }.save(on: app.db)
         try await Search.refresh(on: app.db).get()
-
+        
         // MUT
         try app.test(.GET, "/sitemap.xml") { res in
             // Validation
@@ -40,7 +40,7 @@ class SitemapTests: SnapshotTestCase {
             assertSnapshot(matching: res.body.asString(), as: .init(pathExtension: "xml", diffing: .lines))
         }
     }
-
+    
     @MainActor
     func test_siteMapStaticPages() async throws {
         // MUT
@@ -50,7 +50,11 @@ class SitemapTests: SnapshotTestCase {
             assertSnapshot(matching: res.body.asString(), as: .init(pathExtension: "xml", diffing: .lines))
         }
     }
-
+    
+    func test_linkableEntityUrls() async throws {
+        throw XCTSkip()
+    }
+    
     @MainActor
     func test_siteMapForPackage_noDocs() async throws {
         let package = Package(url: URL(stringLiteral: "https://example.com/owner/repo0"))
@@ -60,16 +64,17 @@ class SitemapTests: SnapshotTestCase {
                              name: "Repo0", owner: "Owner").save(on: app.db)
         try await Version(package: package, latest: .defaultBranch, packageName: "SomePackage",
                           reference: .branch("default")).save(on: app.db)
-
+        let packageResult = try await PackageController.PackageResult
+            .query(on: app.db, owner: "owner", repository: "repo0")
+        
         // MUT
-        let req = Vapor.Request(application: app, url: "/owner/repo0/sitemap.xml", on: app.eventLoopGroup.next())
-        let response = try await PackageController.siteMap(req: req, owner: "owner", repository: "repo0")
-        let body = try XCTUnwrap(response.body.string)
+        let sitemap = try await PackageController.siteMap(packageResult: packageResult, linkableEntityUrls: [])
+        let xml = sitemap.render(indentedBy: .spaces(2))
 
         // Validation
-        assertSnapshot(matching: body, as: .init(pathExtension: "xml", diffing: .lines))
+        assertSnapshot(matching: xml, as: .init(pathExtension: "xml", diffing: .lines))
     }
-
+    
     @MainActor
     func test_siteMapForPackage_withDocs() async throws {
         throw XCTSkip()
@@ -80,7 +85,7 @@ class SitemapTests: SnapshotTestCase {
             { "path": "/documentation/semanticversion/semanticversion/'...(_:)-40b95" }
         ]
         """
-
+        
         let package = Package(url: URL(stringLiteral: "https://example.com/owner/repo0"))
         try await package.save(on: app.db)
         try await Repository(package: package, defaultBranch: "default",
@@ -88,14 +93,18 @@ class SitemapTests: SnapshotTestCase {
                              name: "Repo0", owner: "Owner").save(on: app.db)
         try await Version(package: package, latest: .defaultBranch, packageName: "SomePackage",
                           reference: .branch("default")).save(on: app.db)
-
+        
         // MUT
         let req = Vapor.Request(application: app, url: "/owner/repo0/sitemap.xml", on: app.eventLoopGroup.next())
         let response = try await PackageController.siteMap(req: req)
         let body = try XCTUnwrap(response.body.string)
-
+        
         // Validation
         assertSnapshot(matching: body, as: .init(pathExtension: "xml", diffing: .lines))
+    }
+    
+    func test_siteMap_request() async throws {
+        XCTFail("implement me")
     }
 
 }
