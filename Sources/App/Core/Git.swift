6f832aa6720683ffed8d1d7eff4f36dc7d83a692
Sven A. Schmidt <sas@finestructure.co>
2023-08-28 10:39:32 +0200

    Fix log format

--- Sources/App/Core/Git.swift
+++ Sources/App/Core/Git.swift
@@ -71,7 +71,7 @@ extension Git {
                                                                   separator: separator), at: path)
         let parts = res.components(separatedBy: separator)
         guard parts.count == 2 else {
-            Current.logger().warning(#"Git.invalidRevisionInfo: \(res) for "\#(ShellOutCommand.gitRevisionInfo(reference: reference, separator: separator))" at: \#(path)"#)
+            Current.logger().warning(#"Git.invalidRevisionInfo: \#(res) for '\#(ShellOutCommand.gitRevisionInfo(reference: reference, separator: separator).string)' at: \#(path)"#)
             throw GitError.invalidRevisionInfo(res)
         }
         let hash = parts[0]
