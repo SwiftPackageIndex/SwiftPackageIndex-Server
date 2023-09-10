03865dc26683fece0fd1cdd2bcbc7b9949fab206
cyndichin <cgc95@cornell.edu>
2023-09-10 18:46:13 -0400

    move fetching targets into its own function

--- Sources/App/Commands/Analyze.swift
+++ Sources/App/Commands/Analyze.swift
@@ -238,23 +238,28 @@ extension Analyze {
             }
 
             let versions = try await updateLatestVersions(on: tx, package: package)
-
-            let targets: [(String, TargetType)]? = await {
-                guard let repo = package.repository, let owner = repo.owner, let repository = repo.name else {
-                    return nil
-                }
-                do {
-                    return try? await API.PackageController.Target.query(on: tx, owner: owner, repository: repository)
-                }
-            }()
+            
+            let targets = await fetchTargets(on: tx, package: package)
 
             updateScore(package: package, versions: versions, targets: targets)
             
             await onNewVersions(client: client, logger: logger, package: package, versions: newVersions)
         }
     }
+    
+    /// Fetch targets for a given `Package`
+    /// - Parameters:
+    ///   - database: `Database` object
+    ///   - package: `Package` object
+    /// - Returns: targets associated with package
+    private static func fetchTargets(on database: Database, package: Joined<Package, Repository>) async -> [(String, TargetType)]? {
+        guard let repo = package.repository, let owner = repo.owner, let repository = repo.name else {
+            return nil
+        }
+        return try? await API.PackageController.Target.query(on: database, owner: owner, repository: repository)
+    }
 
-
+    
     static func createCheckoutsDirectory(client: Client,
                                          logger: Logger,
                                          path: String) async throws {
