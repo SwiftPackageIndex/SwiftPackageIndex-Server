6c41d07dcc6504c05d2b17d68867a110338b79ab
Sven A. Schmidt <sas@finestructure.co>
2023-09-25 14:33:12 +0200

    Use SwiftPM 5.9 release

--- Package.swift
+++ Package.swift
@@ -33,7 +33,7 @@ let package = Package(
         .package(url: "https://github.com/SwiftPackageIndex/SPIManifest.git", from: "1.1.0"),
         .package(url: "https://github.com/SwiftPackageIndex/SemanticVersion", from: "0.3.0"),
         .package(url: "https://github.com/SwiftPackageIndex/ShellOut.git", from: "3.1.0"),
-        .package(url: "https://github.com/apple/swift-package-manager.git", branch: "release/5.9"),
+        .package(url: "https://github.com/apple/swift-package-manager.git", revision: "swift-5.9-RELEASE"),
         .package(url: "https://github.com/dankinsoid/VaporToOpenAPI.git", from: "4.0.0"),
         .package(url: "https://github.com/pointfreeco/swift-custom-dump.git", from: "0.10.0"),
         .package(url: "https://github.com/pointfreeco/swift-parsing.git", from: "0.12.0"),
