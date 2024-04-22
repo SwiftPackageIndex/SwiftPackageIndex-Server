b0ce4f716689c3231eb5f3d338a6709ad379eadd
Dave Verwer <dave.verwer@gmail.com>
2024-04-22 10:58:11 +0100

    Belt and braces in `esbuild`.

--- FrontEnd/esbuild.mjs
+++ FrontEnd/esbuild.mjs
@@ -24,6 +24,7 @@ try {
         minify: true,
         plugins: [sassPlugin()],
         external: ['/images/*'],
+        platform: 'browser',
     })
 
     if (process.argv.includes('--watch')) {
