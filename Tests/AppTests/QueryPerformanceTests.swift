8807749d91492ccedbf5b46416ca8d354620fd65
Sven A. Schmidt <sas@finestructure.co>
2024-05-05 12:49:17 +0200

    Update query perf tests

--- Tests/AppTests/QueryPerformanceTests.swift
+++ Tests/AppTests/QueryPerformanceTests.swift
@@ -41,12 +41,12 @@ class QueryPerformanceTests: XCTestCase {
 
     func test_01_Search_packageMatchQuery() async throws {
         let query = Search.packageMatchQueryBuilder(on: app.db, terms: ["a"], filters: [])
-        try await assertQueryPerformance(query, expectedCost: 1400, variation: 150)
+        try await assertQueryPerformance(query, expectedCost: 1500, variation: 150)
     }
 
     func test_02_Search_keywordMatchQuery() async throws {
         let query = Search.keywordMatchQueryBuilder(on: app.db, terms: ["a"])
-        try await assertQueryPerformance(query, expectedCost: 5400, variation: 200)
+        try await assertQueryPerformance(query, expectedCost: 5600, variation: 200)
     }
 
     func test_03_Search_authorMatchQuery() async throws {
@@ -57,56 +57,56 @@ class QueryPerformanceTests: XCTestCase {
     func test_04_Search_query_noFilter() async throws {
         let query = try Search.query(app.db, ["a"], page: 1)
             .unwrap()
-        try await assertQueryPerformance(query, expectedCost: 7400, variation: 200)
+        try await assertQueryPerformance(query, expectedCost: 7700, variation: 200)
     }
 
     func test_05_Search_query_authorFilter() async throws {
         let filter = try AuthorSearchFilter(expression: .init(operator: .is, value: "apple"))
         let query = try Search.query(app.db, ["a"], filters: [filter], page: 1)
             .unwrap()
-        try await assertQueryPerformance(query, expectedCost: 7100, variation: 200)
+        try await assertQueryPerformance(query, expectedCost: 7300, variation: 200)
     }
 
     func test_06_Search_query_keywordFilter() async throws {
         let filter = try KeywordSearchFilter(expression: .init(operator: .is, value: "apple"))
         let query = try Search.query(app.db, ["a"], filters: [filter], page: 1)
             .unwrap()
-        try await assertQueryPerformance(query, expectedCost: 7200, variation: 200)
+        try await assertQueryPerformance(query, expectedCost: 7400, variation: 200)
     }
 
     func test_07_Search_query_lastActicityFilter() async throws {
         let filter = try LastActivitySearchFilter(expression: .init(operator: .greaterThan, value: "2000-01-01"))
         let query = try Search.query(app.db, ["a"], filters: [filter], page: 1)
             .unwrap()
-        try await assertQueryPerformance(query, expectedCost: 7400, variation: 200)
+        try await assertQueryPerformance(query, expectedCost: 7700, variation: 200)
     }
 
     func test_08_Search_query_licenseFilter() async throws {
         let filter = try LicenseSearchFilter(expression: .init(operator: .is, value: "mit"))
         let query = try Search.query(app.db, ["a"], filters: [filter], page: 1)
             .unwrap()
-        try await assertQueryPerformance(query, expectedCost: 7300, variation: 200)
+        try await assertQueryPerformance(query, expectedCost: 7600, variation: 200)
     }
 
     func test_09_Search_query_platformFilter() async throws {
         let filter = try PlatformSearchFilter(expression: .init(operator: .is, value: "macos,ios"))
         let query = try Search.query(app.db, ["a"], filters: [filter], page: 1)
             .unwrap()
-        try await assertQueryPerformance(query, expectedCost: 7100, variation: 200)
+        try await assertQueryPerformance(query, expectedCost: 7500, variation: 200)
     }
 
     func test_10_Search_query_productTypeFilter() async throws {
         let filter = try ProductTypeSearchFilter(expression: .init(operator: .is, value: "plugin"))
         let query = try Search.query(app.db, ["a"], filters: [filter], page: 1)
             .unwrap()
-        try await assertQueryPerformance(query, expectedCost: 7100, variation: 200)
+        try await assertQueryPerformance(query, expectedCost: 7300, variation: 200)
     }
 
     func test_11_Search_query_starsFilter() async throws {
         let filter = try StarsSearchFilter(expression: .init(operator: .greaterThan, value: "5"))
         let query = try Search.query(app.db, ["a"], filters: [filter], page: 1)
             .unwrap()
-        try await assertQueryPerformance(query, expectedCost: 7300, variation: 300)
+        try await assertQueryPerformance(query, expectedCost: 7500, variation: 300)
     }
 
     func test_12_Search_refresh() async throws {
