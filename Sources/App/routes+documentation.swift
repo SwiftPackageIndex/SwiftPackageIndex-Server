ac29018b15579b314103bebc7ef5747ea30c7b94
Sven A. Schmidt <sas@finestructure.co>
2024-05-30 13:16:38 +0200

    Update crash route

--- Sources/App/routes+documentation.swift
+++ Sources/App/routes+documentation.swift
@@ -34,15 +34,9 @@ func docRoutes(_ app: Application) throws {
     }.excludeFromOpenAPI()
 
     if Current.environment() == .development {
-        // https://github.com/SwiftPackageIndex/SwiftPackageIndex-Server/issues/3021
-        // Trigger via
-        // curl -sL -w "%{http_code}" -o /dev/null http://localhost:8080/crash/1234567890
-        // NB: length of the archive parameter is relevant, it MUST be >= 10 characters long
         let crashRoute = Environment.get("CRASH_ROUTE") ?? "crash"
-        app.get(.init(stringLiteral: crashRoute), ":archive") { req in
-            let archive = req.parameters.get("archive")
-            let _ = ([archive].compacted() + req.parameters.getCatchall()).map { $0.lowercased() }
-            return ""
+        app.get(.init(stringLiteral: crashRoute)) { req in
+            fatalError("crash route triggered: crash expected")
         }
     }
 
