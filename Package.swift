b51571f0308c5f75b2aa6e5c10a8434cc9bfd706
Sven A. Schmidt <sas@finestructure.co>
2024-06-10 09:39:57 +0200

    Update SwiftPM to swift-5.10.1-RELEASE

--- Package.swift
+++ Package.swift
@@ -36,7 +36,7 @@ let package = Package(
         .package(url: "https://github.com/SwiftPackageIndex/SPIManifest.git", from: "1.2.0"),
         .package(url: "https://github.com/SwiftPackageIndex/SemanticVersion.git", from: "0.3.0"),
         .package(url: "https://github.com/SwiftPackageIndex/ShellOut.git", from: "3.1.4"),
-        .package(url: "https://github.com/apple/swift-package-manager.git", revision: "swift-5.9-RELEASE"),
+        .package(url: "https://github.com/apple/swift-package-manager.git", revision: "swift-5.10.1-RELEASE"),
         .package(url: "https://github.com/dankinsoid/VaporToOpenAPI.git", from: "4.4.4"),
         .package(url: "https://github.com/pointfreeco/swift-custom-dump.git", from: "1.0.0"),
         .package(url: "https://github.com/pointfreeco/swift-parsing.git", from: "0.12.0"),
