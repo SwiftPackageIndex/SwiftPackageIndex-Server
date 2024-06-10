310729fd046ad961d81fb9072d0a050cb053dd1c
Dave Verwer <dave.verwer@gmail.com>
2024-06-10 21:24:48 +0100

    Enable the Ready for Swift 6 CTA on the homepage.

--- Sources/App/Views/Home/HomeIndex+View.swift
+++ Sources/App/Views/Home/HomeIndex+View.swift
@@ -70,24 +70,17 @@ enum HomeIndex {
 
         func homePageCTA() -> Node<HTML.BodyContext> {
             .section(
-                .if(Current.environment() == .production,
-                    .panelButton(cssClass: "podcast",
-                                 linkUrl: ExternalURL.podcast,
-                                 bodyNode: .podcastPanelBody(includeHeading: false),
-                                 cta: "Listen Now",
-                                 analyticsEvent: "Home - Podcast CTA"),
-                    else: .panelButton(cssClass: "rfs6",
-                                       linkUrl: SiteURL.readyForSwift6.relativeURL(),
-                                       bodyNode: .group(
-                                        .p(
-                                            .strong("Are the packages you use safe from data races?"),
-                                            .text(" "),
-                                            .text("We’re tracking progress towards data race safety this summer. Find out which packages are “Ready for Swift 6” and check for packages marked as “Safe from data races” when browsing the index.")
-                                        )
-                                       ),
-                                       cta: "Track Progress",
-                                       analyticsEvent: "Home - Ready for Swift 6 CTA")
-                )
+                .panelButton(cssClass: "rfs6",
+                             linkUrl: SiteURL.readyForSwift6.relativeURL(),
+                             bodyNode: .group(
+                                .p(
+                                    .strong("Are the packages you use safe from data races?"),
+                                    .text(" "),
+                                    .text("We’re tracking progress towards data race safety this summer. Find out which packages are “Ready for Swift 6” and check for packages marked as “Safe from data races” when browsing the index.")
+                                )
+                             ),
+                             cta: "Track Progress",
+                             analyticsEvent: "Home - Ready for Swift 6 CTA")
             )
         }
 
