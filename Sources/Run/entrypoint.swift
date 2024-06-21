6d9a6d28cef73472011ce2040d5c509866640273
Gwynne Raskind <gwynne@vapor.codes>
2024-06-21 08:10:10 -0500

    Update Vapor entrypoint

--- Sources/Run/entrypoint.swift
+++ Sources/Run/entrypoint.swift
@@ -22,15 +22,16 @@ enum Entrypoint {
         var env = try Environment.detect()
         try LoggingSystem.bootstrap(from: &env)
 
-        let app = Application(env)
-        defer { app.shutdown() }
+        let app = try await Application.make(env)
 
         do {
             try await configure(app)
         } catch {
             app.logger.report(error: error)
+            try? await app.asyncShutdown()
             throw error
         }
         try await app.execute()
+        try await app.asyncShutdown()
     }
 }
