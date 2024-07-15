48c3212556b2ecac9eba6e221b43a523ab56ab40
Sven A. Schmidt <sas@finestructure.co>
2024-07-15 14:24:56 +0200

    Fix Cache warning

--- Sources/App/Core/CurrentReferenceCache.swift
+++ Sources/App/Core/CurrentReferenceCache.swift
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import Cache
+@preconcurrency import Cache
 
 typealias CurrentReferenceCache = ExpiringCache<String, String>
 
