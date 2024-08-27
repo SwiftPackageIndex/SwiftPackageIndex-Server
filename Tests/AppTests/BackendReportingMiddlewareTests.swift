b15efb6cd953b4a7fc271f5ae905ebe20f59285e
msuzoagu <msuzoagu@users.noreply.github.com>
2024-08-26 20:15:19 -0400

    Add route for maintainer page and corresponding test for Plausible event tracking
    - Added `maintainerPage` case to `Path` enum to handle URLs for package
    maintainer information.
    - Added new route in `routes.swift` for the maintainer information page, wrapped
    with `BackendReportingMiddleware` for Plausible event tracking.
    - Added unit test in `BackendReportingMiddlewareTests` to ensure the maintainer
    page view triggers a Plausible event, using `MockClient` to verify interception.

--- Tests/AppTests/BackendReportingMiddlewareTests.swift
+++ Tests/AppTests/BackendReportingMiddlewareTests.swift
@@ -1,35 +1,53 @@
+// Copyright Dave Verwer, Sven A. Schmidt, and other contributors.
 //
-//  BackendReportingMiddlewareTests.swift
-//  
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  Created by msuzoagu on 8/25/24.
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-import XCTest
+import XCTVapor
+import NIOConcurrencyHelpers
+@testable import App
 
 final class BackendReportingMiddlewareTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func testMaintainerPageViewTriggersPlausibleEvent() async throws {
+        let app = Application(.testing)
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        /// Defer to shutdown the app asynchronously after the test completes
+        defer {
+            Task {
+                await app.shutdown()
+            }
         }
-    }
 
+        try await configure(app)
+
+        var intercepted = false
+        let mockClient = MockClient { req, res in
+            intercepted = true
+            res.status = .ok
+        }
+
+        /// Ensure Current is using the MockClient
+        Current.setHTTPClient(mockClient)
+
+        /// Trigger a request
+        try app.test(.GET, "/schwa/Compute/information-for-package-maintainers") { res in
+            XCTAssertEqual(res.status, .ok)
+            XCTAssertTrue(res.body.string.contains("Information for Compute Maintainers"))
+        }
+
+        /// Await to give time for async tasks
+        try await Task.sleep(nanoseconds: 5_000_000_000)
+
+        XCTAssertTrue(intercepted, "The request was not intercepted by MockClient")
+    }
 }
