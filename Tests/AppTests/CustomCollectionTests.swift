c8e7ff732f1685c77814c70e64096393aff69d8f
Sven A. Schmidt <sas@finestructure.co>
2024-10-08 15:02:42 +0200

    Add CustomCollectionPackage pivot

--- Tests/AppTests/CustomCollectionTests.swift
+++ Tests/AppTests/CustomCollectionTests.swift
@@ -20,6 +20,7 @@ import Dependencies
 
 
 class CustomCollectionTests: AppTestCase {
+
     func test_CustomCollection_save() async throws {
         // MUT
         try await CustomCollection(id: .id0, name: "List", url: "https://github.com/foo/bar/list.json")
@@ -51,4 +52,36 @@ class CustomCollectionTests: AppTestCase {
                       "was: \(msg)")
         }
     }
+
+    func test_CustomCollectionPackage_attach() async throws {
+        // setup
+        let pkg = Package(id: .id0, url: "1".asGithubUrl.url)
+        try await pkg.save(on: app.db)
+        let collection = CustomCollection(id: .id1, name: "List", url: "https://github.com/foo/bar/list.json")
+        try await collection.save(on: app.db)
+
+        // MUT
+        try await collection.$packages.attach(pkg, on: app.db)
+
+        do { // validate
+            let count = try await CustomCollectionPackage.query(on: app.db).count()
+            XCTAssertEqual(count, 1)
+            let pivot = try await CustomCollectionPackage.query(on: app.db).first().unwrap()
+            try await pivot.$package.load(on: app.db)
+            XCTAssertEqual(pivot.package.id, .id0)
+            XCTAssertEqual(pivot.package.url, "1".asGithubUrl)
+            try await pivot.$customCollection.load(on: app.db)
+            XCTAssertEqual(pivot.customCollection.id, .id1)
+            XCTAssertEqual(pivot.customCollection.name, "List")
+        }
+
+        do { // ensure package is unique per list
+            try await collection.$packages.attach(pkg, on: app.db)
+        } catch {
+            let msg = String(reflecting: error)
+            XCTAssert(msg.contains(#"duplicate key value violates unique constraint "uq:custom_collections+packages.custom_collection_id+custom_coll""#),
+                      "was: \(msg)")
+        }
+    }
+
 }
