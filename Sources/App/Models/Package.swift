542d96e302eec9caad726a417ec63b5dfbba2180
Sven A. Schmidt <sas@finestructure.co>
2024-10-09 11:53:48 +0200

    Add Package filter by urls

--- Sources/App/Models/Package.swift
+++ Sources/App/Models/Package.swift
@@ -271,6 +271,10 @@ extension QueryBuilder where Model == Package {
     func filter(by url: URL) -> Self {
         filter(\.$url == url.absoluteString)
     }
+
+    func filter(by urls: some Collection<URL>) -> Self {
+        filter(\.$url ~~ urls.map(\.absoluteString))
+    }
 }
 
 
