0700d20d2765e207a1dc04832c50bdb448a62ad5
Rahaf Aljerwi <aljerwi.rahaf@yahoo.com>
2024-12-31 16:31:29 -0600

    seperate more cognito functions

--- Sources/App/Controllers/Manage/Cognito.swift
+++ Sources/App/Controllers/Manage/Cognito.swift
@@ -5,7 +5,7 @@ import SotoCognitoIdentity
 
 struct Cognito {
     @Sendable
-    static func authenticate(req: Request, username: String, password: String) async throws {
+    static func authenticate(req: Request, username: String, password: String) async throws -> CognitoAuthenticateResponse {
         let awsClient = AWSClient(httpClientProvider: .shared(req.application.http.client.shared))
         let awsCognitoConfiguration = CognitoConfiguration(
             userPoolId: Environment.get("AWS_COGNITO_POOL_ID")!,
@@ -16,13 +16,22 @@ struct Cognito {
         )
         req.application.cognito.authenticatable = CognitoAuthenticatable(configuration: awsCognitoConfiguration)
         let response = try await req.application.cognito.authenticatable.authenticate(username: username, password: password)
-        switch response {
-        case .authenticated(let authenticatedResponse):
-            let user = AuthenticatedUser(accessToken: authenticatedResponse.accessToken!, refreshToken: authenticatedResponse.refreshToken!)
-            req.auth.login(user)
-        case .challenged(let challengedResponse): // TODO: handle challenge
-            break
-        }
+        try awsClient.syncShutdown()
+        return response
+    }
+    
+    @Sendable
+    static func authenticateToken(req: Request, sessionID: String, accessToken: String, on eventLoop: EventLoop) async throws -> Void {
+        let awsClient = AWSClient(httpClientProvider: .shared(req.application.http.client.shared))
+        let awsCognitoConfiguration = CognitoConfiguration(
+            userPoolId: Environment.get("AWS_COGNITO_POOL_ID")!,
+            clientId: Environment.get("AWS_COGNITO_CLIENT_ID")!,
+            clientSecret: Environment.get("AWS_COGNITO_CLIENT_SECRET")!,
+            cognitoIDP: CognitoIdentityProvider(client: awsClient, region: .useast2),
+            adminClient: true
+        )
+        req.application.cognito.authenticatable = CognitoAuthenticatable(configuration: awsCognitoConfiguration)
+        let _ = try await req.application.cognito.authenticatable.authenticate(accessToken: sessionID, on: req.eventLoop)
         try awsClient.syncShutdown()
     }
     
@@ -38,6 +47,36 @@ struct Cognito {
         )
         req.application.cognito.authenticatable = CognitoAuthenticatable(configuration: awsCognitoConfiguration)
         try await req.application.cognito.authenticatable.signUp(username: username, password: password, attributes: [:], on:req.eventLoop)
+        try awsClient.syncShutdown()
+    }
+    
+    @Sendable
+    static func forgotPassword(req: Request, username: String) async throws {
+        let awsClient = AWSClient(httpClientProvider: .shared(req.application.http.client.shared))
+        let awsCognitoConfiguration = CognitoConfiguration(
+            userPoolId: Environment.get("AWS_COGNITO_POOL_ID")!,
+            clientId: Environment.get("AWS_COGNITO_CLIENT_ID")!,
+            clientSecret: Environment.get("AWS_COGNITO_CLIENT_SECRET")!,
+            cognitoIDP: CognitoIdentityProvider(client: awsClient, region: .useast2),
+            adminClient: true
+        )
+        req.application.cognito.authenticatable = CognitoAuthenticatable(configuration: awsCognitoConfiguration)
+        try await req.application.cognito.authenticatable.forgotPassword(username: username)
+        try awsClient.syncShutdown()
+    }
+    
+    @Sendable
+    static func resetPassword(req: Request, username: String, password: String, confirmationCode: String) async throws {
+        let awsClient = AWSClient(httpClientProvider: .shared(req.application.http.client.shared))
+        let awsCognitoConfiguration = CognitoConfiguration(
+            userPoolId: Environment.get("AWS_COGNITO_POOL_ID")!,
+            clientId: Environment.get("AWS_COGNITO_CLIENT_ID")!,
+            clientSecret: Environment.get("AWS_COGNITO_CLIENT_SECRET")!,
+            cognitoIDP: CognitoIdentityProvider(client: awsClient, region: .useast2),
+            adminClient: true
+        )
+        req.application.cognito.authenticatable = CognitoAuthenticatable(configuration: awsCognitoConfiguration)
+        try await req.application.cognito.authenticatable.confirmForgotPassword(username: username, newPassword: password, confirmationCode: confirmationCode)
         try awsClient.syncShutdown()
     }
 }
