7b6d6029a3635f93777ba5561622868dba30bd3e
Sven A. Schmidt <sas@finestructure.co>
2025-01-02 16:11:48 +0100

    Toggle logging according to env variable

--- Sources/App/Core/CFRayRouteLoggingMiddleware.swift
+++ Sources/App/Core/CFRayRouteLoggingMiddleware.swift
@@ -12,20 +12,28 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import Dependencies
 import Vapor
+
 
 // Replica of the Vapor RouteLoggingMiddleware that's tweaked to explicitly expose the cf-ray header in the logger metadata for the request.
 public final class CFRayRouteLoggingMiddleware: Middleware {
     public let logLevel: Logger.Level
-    
+
     public init(logLevel: Logger.Level = .info) {
         self.logLevel = logLevel
     }
-    
+
     public func respond(to request: Request, chainingTo next: Responder) -> EventLoopFuture<Response> {
+        @Dependency(\.environment) var environment
+
+        guard environment.enableCFRayLogging() else {
+            return next.respond(to: request)
+        }
         guard let cfray = request.headers.first(name: "cf-ray") else {
             return next.respond(to: request)
         }
+
         request.logger[metadataKey: "cf-ray"] = .string(cfray)
         request.logger.log(level: self.logLevel, "\(request.method) \(request.url.path.removingPercentEncoding ?? request.url.path)")
         return next.respond(to: request)
