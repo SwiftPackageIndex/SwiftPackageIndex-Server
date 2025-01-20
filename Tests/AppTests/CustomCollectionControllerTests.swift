09feefb1a7b9a1f357951090184ee0beb7696654
Sven A. Schmidt <sas@finestructure.co>
2025-01-20 14:03:39 +0100

    Add test_query_pagination

--- Tests/AppTests/CustomCollectionControllerTests.swift
+++ Tests/AppTests/CustomCollectionControllerTests.swift
@@ -17,6 +17,7 @@ import XCTest
 @testable import App
 
 import Dependencies
+import Fluent
 import Vapor
 
 
@@ -24,15 +25,13 @@ class CustomCollectionControllerTests: AppTestCase {
 
     func test_query() async throws {
         // setup
-        let pkg = Package(id: .id0, url: "1".asGithubUrl.url)
-        try await pkg.save(on: app.db)
-        try await Repository(package: pkg, name: "1", owner: "owner").save(on: app.db)
-        try await Version(package: pkg, latest: .defaultBranch).save(on: app.db)
-        let collection = CustomCollection(id: .id1, .init(key: "list",
-                                                          name: "List",
-                                                          url: "https://github.com/foo/bar/list.json"))
-        try await collection.save(on: app.db)
-        try await collection.$packages.attach([pkg], on: app.db)
+        try await CustomCollection.save(
+            on: app.db,
+            key: "list",
+            name: "List",
+            url: "https://github.com/foo/bar/list.json",
+            packages: [( id: .id0, url: "https://github.com/foo/1", owner: "foo", name: "1" )]
+        )
 
         // MUT
         let page = try await CustomCollectionsController.query(on: app.db,
@@ -45,4 +44,69 @@ class CustomCollectionControllerTests: AppTestCase {
         XCTAssertEqual(page.hasMoreResults, false)
     }
 
+    func test_query_pagination() async throws {
+        // setup
+        let pkgInfo = [UUID.id0, .id1, .id2, .id3, .id4].enumerated().shuffled().map { (idx, id) in
+            (id, URL(string: "https://github.com/foo/\(idx)")!, "foo", "\(idx)")
+        }
+        try await CustomCollection.save(
+            on: app.db,
+            key: "list",
+            name: "List",
+            url: "https://github.com/foo/bar/list.json",
+            packages: pkgInfo
+        )
+
+        do {  // first page
+              // MUT
+            let page = try await CustomCollectionsController.query(on: app.db,
+                                                                   key: "list",
+                                                                   page: 1,
+                                                                   pageSize: 2)
+            // validate
+            XCTAssertEqual(page.results.map(\.repository.name), ["0", "1"])
+            XCTAssertEqual(page.hasMoreResults, true)
+        }
+
+        do {  // second page
+              // MUT
+            let page = try await CustomCollectionsController.query(on: app.db,
+                                                                   key: "list",
+                                                                   page: 2,
+                                                                   pageSize: 2)
+            // validate
+            XCTAssertEqual(page.results.map(\.repository.name), ["2", "3"])
+            XCTAssertEqual(page.hasMoreResults, true)
+        }
+
+        do {  // first page
+              // MUT
+            let page = try await CustomCollectionsController.query(on: app.db,
+                                                                   key: "list",
+                                                                   page: 3,
+                                                                   pageSize: 2)
+            // validate
+            XCTAssertEqual(page.results.map(\.repository.name), ["4"])
+            XCTAssertEqual(page.hasMoreResults, false)
+        }
+    }
+
+}
+
+
+private extension CustomCollection {
+    @discardableResult
+    static func save(on database: Database, key: String, name: String, url: URL, packages: [(id: Package.Id, url: URL, owner: String, name: String)]) async throws -> CustomCollection {
+        let packages = try await packages.mapAsync {
+            let pkg = Package(id: $0.id, url: $0.url)
+            try await pkg.save(on: database)
+            try await Repository(package: pkg, name: $0.name, owner: $0.owner).save(on: database)
+            try await Version(package: pkg, latest: .defaultBranch).save(on: database)
+            return pkg
+        }
+        let collection = CustomCollection(id: .id1, .init(key: key, name: name, url: url))
+        try await collection.save(on: database)
+        try await collection.$packages.attach(packages, on: database)
+        return collection
+    }
 }
