b8fcb3b59e9b5cf5457d4e485f80a7c5a88a384b
Sven A. Schmidt <sas@finestructure.co>
2025-03-07 12:01:44 +0100

    Move top level databasePool property to DatabasePool.shared

--- Tests/AppTests/DatabasePoolTests.swift
+++ Tests/AppTests/DatabasePoolTests.swift
@@ -6,16 +6,11 @@ import ShellOut
 import Testing
 
 
-
-
-private let databasePool = DatabasePool(maxCount: 4)
-
-
 @Suite(.setupDatabasePool)
 struct DatabasePoolTests {
     @Test(arguments: (0..<10))
     func dbPoolTest(_ id: Int) async throws {
-        try await databasePool.withDatabase { id in
+        try await DatabasePool.shared.withDatabase { id in
             print("⚠️", id)
             try await Task.sleep(for: .milliseconds(500))
         }
@@ -25,10 +20,10 @@ struct DatabasePoolTests {
 struct DatabaseSetupTrait: SuiteTrait, TestScoping {
     func provideScope(for test: Test, testCase: Test.Case?, performing function: @Sendable () async throws -> Void) async throws {
         print("⚠️ setup")
-        try await databasePool.setUp()
+        try await DatabasePool.shared.setUp()
         try await function()
         print("⚠️ tear down")
-        try await databasePool.tearDown()
+        try await DatabasePool.shared.tearDown()
     }
 }
 
