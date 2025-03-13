1a66bf626eb8323b9e21614291f99c5df8905f5e
Dave Verwer <dave.verwer@gmail.com>
2025-03-13 17:52:06 +0000

    Remove Ready for Swift 6 CTA and put the podcast CTA back.

--- Sources/App/Views/DocumentationPageProcessor.swift
+++ Sources/App/Views/DocumentationPageProcessor.swift
@@ -280,19 +280,37 @@ struct DocumentationPageProcessor {
                         .li(
                             .a(
                                 .href(SiteURL.privacy.relativeURL()),
-                                "Privacy and Cookies"
+                                "Privacy"
                             )
                         ),
                         .li(
                             .a(
                                 .href("https://swiftpackageindex.statuspage.io"),
-                                "Uptime and System Status"
+                                "System Status"
+                            )
+                        ),
+                        .li(
+                            .a(
+                                .href(SiteURL.buildMonitor.relativeURL()),
+                                "Build System Monitor"
                             )
                         ),
                         .li(
                             .a(
                                 .href(ExternalURL.mastodon),
                                 "Mastodon"
+                            )
+                        ),
+                        .li(
+                            .a(
+                                .href(ExternalURL.podcast),
+                                "Podcast"
+                            )
+                        ),
+                        .li(
+                            .a(
+                                .href(SiteURL.readyForSwift6.relativeURL()),
+                                "Ready for Swift 6"
                             )
                         )
                     )
--- Sources/App/Views/Home/HomeIndex+View.swift
+++ Sources/App/Views/Home/HomeIndex+View.swift
@@ -70,17 +70,11 @@ enum HomeIndex {
 
         func homePageCTA() -> Node<HTML.BodyContext> {
             .section(
-                .panelButton(cssClass: "rfs6",
-                             linkUrl: SiteURL.readyForSwift6.relativeURL(),
-                             bodyNode: .group(
-                                .p(
-                                    .strong("Are the packages you use safe from data races?"),
-                                    .text(" "),
-                                    .text("We’re tracking progress towards data race safety with Swift 6. Find out which packages are “Ready for Swift 6” and check for packages marked as “Safe from data races” when browsing the index.")
-                                )
-                             ),
-                             cta: "Track Progress",
-                             analyticsEvent: "Home - Ready for Swift 6 CTA")
+                .panelButton(cssClass: "podcast",
+                             linkUrl: ExternalURL.podcast,
+                             bodyNode: .podcastPanelBody(includeHeading: false),
+                             cta: "Listen Now",
+                             analyticsEvent: "Home - Podcast CTA")
             )
         }
 
