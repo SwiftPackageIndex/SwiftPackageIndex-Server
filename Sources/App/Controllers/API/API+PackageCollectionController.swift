46fc0ac397b6751eb9385da2a91ac681721706d7
Sven A. Schmidt <sas@finestructure.co>
2025-05-02 12:25:59 +0200

    Fix Sendable error

--- Sources/App/Controllers/API/API+PackageCollectionController.swift
+++ Sources/App/Controllers/API/API+PackageCollectionController.swift
@@ -61,7 +61,7 @@ extension API {
 }
 
 
-extension PackageCollectionSigning.Model.SignedCollection: Vapor.Content {}
+extension PackageCollectionSigning.Model.SignedCollection: @retroactive @unchecked Sendable, Vapor.Content {}
 
 
 extension API {
