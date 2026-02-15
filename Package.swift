daa0e3d035b3c8f55e2302d90f6e50cda05f5149
Sven A. Schmidt <sas@finestructure.co>
2026-02-15 09:16:36 +0100

    Use SemanticVersion from branch accept-leading-zeroes
    # Conflicts:
    #	Package.resolved
    #	Package.swift

--- Package.swift
+++ Package.swift
@@ -33,7 +33,7 @@ let package = Package(
         .package(url: "https://github.com/SwiftPackageIndex/CanonicalPackageURL.git", from: "1.0.0"),
         .package(url: "https://github.com/SwiftPackageIndex/DependencyResolution.git", from: "1.1.2"),
         .package(url: "https://github.com/SwiftPackageIndex/SPIManifest.git", from: "1.10.0"),
-        .package(url: "https://github.com/SwiftPackageIndex/SemanticVersion.git", from: "0.3.0"),
+        .package(url: "https://github.com/SwiftPackageIndex/SemanticVersion.git", branch: "accept-leading-zeroes"),
         .package(url: "https://github.com/SwiftPackageIndex/ShellOut.git", from: "3.3.0"),
         .package(url: "https://github.com/SwiftPackageIndex/swift-package-manager.git", branch: "release/6.1"),
         .package(url: "https://github.com/pointfreeco/swift-custom-dump.git", from: "1.0.0"),
