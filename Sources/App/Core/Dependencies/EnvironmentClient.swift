9a47351b6728896382279fec1ce4df8e7175fcc1
Dave Verwer <daveverwer@apple.com>
2026-07-30 18:52:02 -0400

    Fix up the mock.

--- Sources/App/Core/Dependencies/EnvironmentClient.swift
+++ Sources/App/Core/Dependencies/EnvironmentClient.swift
@@ -218,6 +218,7 @@ extension EnvironmentClient: TestDependencyKey {
         var mock = Self()
         mock.appVersion = { "test" }
         mock.current = { .development }
+        mock.hideLatestSwiftVersionBuildData = { false }
         mock.hideStagingBanner = { false }
         mock.siteURL = { "http://localhost:8080" }
         mock.shouldFail = { @Sendable _ in false }
