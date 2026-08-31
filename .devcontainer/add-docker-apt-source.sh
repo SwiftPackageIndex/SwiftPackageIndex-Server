06e39264ef7dcd5e2d4eac084b4320f8f792bd3e
Sven A. Schmidt <sas@finestructure.co>
2026-08-31 05:42:28 +0200

    Set up docker inside dev container

---
+++ .devcontainer/add-docker-apt-source.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
